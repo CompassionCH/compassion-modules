cb3f630534da12edb8c9828c52467deb605af054
ecino <ecino@compassion.ch>
2014-08-26 13:34:54 +0200

    PEP8 and small code cleaning.

--- message_center_compassion/model/contracts.py
+++ message_center_compassion/model/contracts.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Author: Cyril Sester. Copyright Compassion Suisse
+#    Author: Emanuel Cino. Copyright Compassion Suisse
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -18,34 +18,31 @@
 #
 ##############################################################################
 
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
-from openerp.osv import orm, fields
-from openerp.tools import mod10r
-from openerp.tools.translate import _
-from openerp.tools import DEFAULT_SERVER_DATE_FORMAT
-import pdb
+from openerp.osv import orm
 import logging
-import time
 
 logger = logging.getLogger(__name__)
 
+
 class simple_recurring_contract(orm.Model):
+
     """ We add here creation of messages concerning commitments. """
     _inherit = "simple.recurring.contract"
-    
+
     def _on_contract_active(self, cr, uid, ids, context=None):
         """ Create messages to GMC when new sponsorship is activated. """
-        super(simple_recurring_contract, self)._on_contract_active(cr, uid, ids, context=context)
+        super(simple_recurring_contract, self)._on_contract_active(
+            cr, uid, ids, context=context)
         message_obj = self.pool.get('gmc.message.pool')
         action_obj = self.pool.get('gmc.action')
         action_id = 0
         message_vals = {}
-        
+
         for contract in self.browse(cr, uid, ids, context=context):
             if contract.child_id:
                 # UpsertConstituent Message
-                action_id = action_obj.search(cr, uid, [('name','=','UpsertConstituent')], limit=1, context=context)[0]
+                action_id = action_obj.search(
+                    cr, uid, [('name', '=', 'UpsertConstituent')], limit=1, context=context)[0]
                 message_vals = {
                     'action_id': action_id,
                     'object_id': contract.partner_id.id,
@@ -53,25 +50,28 @@ class simple_recurring_contract(orm.Model):
                     'date': contract.first_payment_date,
                 }
                 message_obj.create(cr, uid, message_vals, context=context)
-                
+
                 # CreateCommitment Message
-                action_id = action_obj.search(cr, uid, [('name','=','CreateCommitment')], limit=1, context=context)[0]
+                action_id = action_obj.search(
+                    cr, uid, [('name', '=', 'CreateCommitment')], limit=1, context=context)[0]
                 message_vals.update({
                     'action_id': action_id,
                     'object_id': contract.id,
                     'child_id': contract.child_id.id,
                 })
                 message_obj.create(cr, uid, message_vals, context=context)
-        
+
     def contract_terminated(self, cr, uid, ids, context=None):
         """ Inform GMC when sponsorship is terminated. """
-        res = super(simple_recurring_contract, self).contract_terminated(cr, uid, ids)
+        res = super(simple_recurring_contract, self).contract_terminated(
+            cr, uid, ids)
         if res:
             message_obj = self.pool.get('gmc.message.pool')
             action_obj = self.pool.get('gmc.action')
-            action_id = action_obj.search(cr, uid, [('name','=','CancelCommitment')], limit=1, context=context)[0]
+            action_id = action_obj.search(
+                cr, uid, [('name', '=', 'CancelCommitment')], limit=1, context=context)[0]
             message_vals = {'action_id': action_id}
-            
+
             for contract in self.browse(cr, uid, ids, context=context):
                 if contract.child_id:
                     message_vals.update({
@@ -80,9 +80,9 @@ class simple_recurring_contract(orm.Model):
                         'child_id': contract.child_id.id,
                     })
                     message_obj.create(cr, uid, message_vals)
-        
+
         return res
-        
+
     def _invoice_paid(self, cr, uid, invoice, context=None):
         """ Check if invoice paid contains
             a child gift and creates a message to GMC. """
@@ -92,12 +92,14 @@ class simple_recurring_contract(orm.Model):
         ]
         message_obj = self.pool.get('gmc.message.pool')
         action_obj = self.pool.get('gmc.action')
-        action_id = action_obj.search(cr, uid, [('name','=','CreateGift')], limit=1, context=context)[0]
+        action_id = action_obj.search(
+            cr, uid, [('name', '=', 'CreateGift')], limit=1, context=context)[0]
         message_vals = {
             'action_id': action_id,
             'date': invoice.date_invoice,
         }
-        gift_ids = self.pool.get('product.product').search(cr, uid, [('name_template','in',gift_product_names)], context={'lang':'en_US'})
+        gift_ids = self.pool.get('product.product').search(
+            cr, uid, [('name_template', 'in', gift_product_names)], context={'lang': 'en_US'})
 
         for invoice_line in invoice.invoice_line:
             if invoice_line.product_id.id in gift_ids:
