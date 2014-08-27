0b9b9aece913df33006776c600e1dedf35cfefd6
ecino <ecino@compassion.ch>
2014-08-27 12:24:20 +0200

    Adds a default date to the next first of months in contracts.

--- message_center_compassion/model/partner_compassion.py
+++ message_center_compassion/model/partner_compassion.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
@@ -27,3 +28,35 @@ class ResPartner(Model):
 
 
 ResPartner()
+=======
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Emanuel Cino. Copyright Compassion Suisse
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp.osv.orm import Model
+
+
+class ResPartner(Model):
+
+    """ Adds triggers to creation of GMC messages."""
+
+    _inherit = 'res.partner'
+
+
+ResPartner()
+>>>>>>> Adds a default date to the next first of months in contracts.
