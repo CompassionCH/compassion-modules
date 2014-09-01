3372af12b3a2c663697390a8a025423fbf1c85a2
Cyril Sester <cyril.sester@outlook.com>
2014-09-01 11:28:34 +0200

    Fixed methods privacy and add some type checks

--- child_compassion/wizard/child_description_wizard.py
+++ child_compassion/wizard/child_description_wizard.py
@@ -25,8 +25,8 @@ from openerp.tools.translate import _
 class child_description_wizard(orm.TransientModel):
     _name = 'child.description.wizard'
     
-    def get_value_ids(self, cr, uid, ids, fieldname, args, context=None):
-        property_id = context.get('property_id', False)
+    def _get_value_ids(self, cr, uid, ids, fieldname, args, context=None):
+        property_id = context.get('property_id')
         if not property_id:
             return dict([(id, []) for id in ids])
 
@@ -46,10 +46,10 @@ class child_description_wizard(orm.TransientModel):
         value_ids = [x[0] for x in cr.fetchall()]
         return dict([(id, value_ids) for id in ids])
 
-    def get_default_ids(self, cr, uid, context=None):
-        return self.get_value_ids(cr, uid, [0], '', '', context)[0]
+    def _get_default_ids(self, cr, uid, context=None):
+        return self._get_value_ids(cr, uid, [0], '', '', context)[0]
 
-    def write_values(self, cr, uid, id, name, value, inv_arg, context=None):
+    def _write_values(self, cr, uid, id, name, value, inv_arg, context=None):
         value_obj = self.pool.get('compassion.child.property.value')
         for line in value:
             if line[0] == 1: # on2many update
@@ -66,9 +66,9 @@ class child_description_wizard(orm.TransientModel):
         'desc_it': fields.text(_('Italian description')),
         'keep_desc_en': fields.boolean(_('Keep english description')),
         'desc_en': fields.text(_('English description')),
-        'child_property_value_ids': fields.function(get_value_ids, type='one2many',
+        'child_property_value_ids': fields.function(_get_value_ids, type='one2many',
                                                     relation='compassion.child.property.value',
-                                                    fnct_inv=write_values),
+                                                    fnct_inv=_write_values),
         'state': fields.selection(
             [('values', _('Values completion')),
              ('descriptions', _('Descriptions correction'))],
@@ -82,7 +82,7 @@ class child_description_wizard(orm.TransientModel):
         'keep_desc_it': False,
         'keep_desc_en': False,
         'child_property_value_ids': lambda self, cr, uid, context: \
-                                        self.get_default_ids(cr, uid, context),
+                                        self._get_default_ids(cr, uid, context),
     }
 
     def generate_descriptions(self, cr, uid, ids, context=None):
@@ -90,7 +90,8 @@ class child_description_wizard(orm.TransientModel):
         child = child_obj.browse(cr, uid, context.get('child_id'), context)
         if not child:
             raise orm.except_orm('ObjectError', _('No valid child id given !'))
-        child = child[0]
+        if isinstance(child, list):
+            child = child[0]
         case_study = child.case_study_ids[-1]
         self.write(cr, uid, ids, {
             'state': 'descriptions',
@@ -132,23 +133,28 @@ class child_description_wizard(orm.TransientModel):
             'tag': 'reload',
         }
 
-    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
+    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None,
+                        toolbar=False, submenu=False):
         ret = super(child_description_wizard, self).fields_view_get(cr, uid, view_id, view_type, 
                                                                     context, toolbar, submenu)
-        fr, de, it, en = self.get_helps(cr, uid, context)
+        fr, de, it, en = self._get_helps(cr, uid, context)
         ret['fields']['desc_fr']['help'] = fr
         ret['fields']['desc_de']['help'] = de
         ret['fields']['desc_it']['help'] = it
         ret['fields']['desc_en']['help'] = en
         return ret
 
-    def get_helps(self, cr, uid, context=None):
+    def _get_helps(self, cr, uid, context=None):
         if not context or not context.get('child_id'):
             return '', '', '', ''
         child_obj = self.pool.get('compassion.child')
         child = child_obj.browse(cr, uid, context['child_id'], context=context)[0]
         return child.desc_fr, child.desc_de, child.desc_it, child.desc_en
 
+    ##################################################
+    #              Common description                #
+    #                     tools                      #
+    ##################################################
     def _gen_list_string(self, list, separator, last_separator):
         string = separator.join(list[:-1])
         if len(list) > 1:
