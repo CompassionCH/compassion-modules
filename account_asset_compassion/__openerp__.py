e3f13d9bc8d3f3776c3378cb1188c63739402f18
Cyril Sester <cyril.sester@outlook.com>
2014-09-08 11:39:56 +0200

    [ADD]Asset rectification module (prorata temporis).

---
+++ account_asset_compassion/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Compassion asset customizing module for openERP
+#    Copyright (C) 2014 Compassion CH (http://www.compassion.ch)
+#    @author: Cyril Sester <csester@compassion.ch>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Assets Compassion',
+    'summary': 'Modifications to fit Compassion needs',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'author': 'Compassion CH',
+    'website': 'http://www.compassion.ch',
+    'category': 'Accounting & Finance',
+    'depends': ['account_asset'],
+    'external_dependencies': {},
+    'data': [
+    ],
+    'demo': [],
+    'description': '''
+        This module overrides the prorata temporis asset process.
+    ''',
+    'active': False,
+    'installable': True,
+}
