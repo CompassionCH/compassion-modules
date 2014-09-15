0ee99ddf91586172306dc356a568417f0543edb2
Cyril Sester <cyril.sester@outlook.com>
2014-09-15 09:47:03 +0200

    [ADD] Community prototypes for value retrieval for description generation.

--- child_compassion/model/project_compassion.py
+++ child_compassion/model/project_compassion.py
@@ -58,6 +58,15 @@ class compassion_project(orm.Model):
         'country_denomination': fields.char(_('Local denomination')),
         'western_denomination': fields.char(_('Western denomination')),
         'community_name': fields.char(_('Community name')),
+        # Example of a field used for description generation. It should refer
+        # to a value in the child.property.value model. This way, we can set
+        # dynamic translations and reuse them if multiple projects refer
+        # to a same value. Property_name filter is important to give a context
+        # to words.
+        'primary_diet_ids': fields.many2many(
+            'compassion.child.property.value', 'project_property_to_value',
+            'property_id', 'value_id', _('Primary diet'),
+            domain=[('property_name', '=', 'primary_diet')]),
         #'country_id': fields.many2one('compassion.country', _('Country')),
     }
 
@@ -68,9 +77,14 @@ class compassion_project(orm.Model):
 
         ret = {}
         for project in self.browse(cr, uid, ids, context):
-            coutry, type = self._update_program_info(cr, uid, project, context)
+            values, coutry, type, community_id = self._update_program_info(
+                cr, uid, project, context)
+            values.extend(
+                self._update_community_info(cr, uid, community_id, context))
             if type == 'CDSP':
-                self._update_cdsp_info(cr, uid, project, context)
+                values.extend(self._update_cdsp_info(cr, uid,
+                                                     project, context))
+            self.write(cr, uid, [project.id], values, context=context)
 
         #TODO: Update related country informations
         return True
@@ -82,7 +96,47 @@ class compassion_project(orm.Model):
             return None
 
         prog_impl = json.loads(r.text)
+        
+        values = self._get_program_values(cr, uid, prog_impl, context)
+
+        coutry_code = prog_impl.get('ISOCountryCode')
+        type = prog_impl.get('ProgramImplementorTypeCode')
+        community_id = prog_impl.get('CommunityId')
+        return values, coutry_code, type, community_id
+
+    def _update_community_info(self, cr, uid, community_id, context=None):
+        url = self._get_url(community_id, 'community')
+        r = requests.get(url)
+        if not r.status_code/100 == 2:
+            return None
+
+        prog_impl = json.loads(r.text)
+        return self._get_community_values(cr, uid, cdsp_impl, context)
+
+    def _update_cdsp_info(self, cr, uid, project, context=None):
+        url = self._get_url(project.code, 'cdspimplementor')
+        r = requests.get(url)
+        if not r.status_code/100 == 2:
+            return None
+
+        cdsp_impl = json.loads(r.text)
+        return self._get_cdsp_values(cr, uid, cdsp_impl, context)
+
+    def _get_program_values(self, cr, uid, json_values, context=None):
+        ''' Map JSON values to openERP fields.
+            @param self: Python equivalent for "this"
+            @param cr: OpenERP database cursor. Used for object browsing
+            @param uid: Current user id. Standard parameter.
+            @param json_values: json parsed values retrieved from 
+                https://api2.compassion.com/ci/v1/programimplementor/ . Please
+                look at http://bit.ly/YKrD4d to see full description
+            @param context: Standard strategy used in OpenERP to share and set
+                default values in the views. This should be part of most
+                function signatures
+        '''
         values = {}
+
+        # For example, we map a bunch of fields
         json_field_mapping = {
             'ProgramImplementorTypeCode': 'type',
             'OrganizationName': 'local_church_name',
@@ -93,21 +147,50 @@ class compassion_project(orm.Model):
             'CommunityName': 'community_name',
             }
 
+        # Setup the values dict. In OpenERP object creation/update is made
+        # this way: you should give a dict with field_name:value to create or
+        # write function. Setting my_object.field_name = value won't work in
+        #OpenERP 7
         for json_name, field in json_field_mapping.iteritems():
-            if prog_impl.get(json_name):
-                values[field] = prog_impl[json_name]
-        self.write(cr, uid, [project.id], values, context=context)
-        coutry_code = prog_impl.get('ISOCountryCode')
-        type = prog_impl.get('ProgramImplementorTypeCode')
-        return coutry_code, type
+            if json_values.get(json_name):
+                values[field] = json_values[json_name]
 
-    def _update_cdsp_info(self, cr, uid, project, context=None):
-        url = self._get_url(project.code, 'cdspimplementor')
-        r = requests.get(url)
-        if not r.status_code/100 == 2:
-            return None
+        # TODO: look at a project description (in web-client). All the
+        # informations needed to generate this kind of description comes from
+        # one of this web-service : 
+        # - REST_Get_Program_Implementor
+        # - REST_Get_Community
+        # - Get_CDSP_Implementor
+        # Your job is to map this infos in the appropriate function:
+        # - _get_program_values
+        # - _get_community_values
+        # - _get_cdsp_values
 
-        cdsp_impl = json.loads(r.text)
+        return values
+
+    def _get_community_values(self, cr, uid, json_values, context=None):
+        ''' Map JSON values to fields.
+            REST_Get_Community
+        '''
+        values = {}
+
+        # Example of a field used for description generation. It should refer
+        # to a value in the child.property.value model. This way, we can set
+        # dynamic translations and reuse them if multiple projects refer
+        # to a same value. Property_name filter is important to give a context
+        # to words.
+        multi_values = self._get_values(cr, uid,
+                                        json_values['PrimaryDiet'].split(', '),
+                                        'primary_diet', context)
+        # Set Many2One or Many2Many values
+        values['primary_diet_ids'] = [(6, 0, multi_values)]
+
+        return values
+
+    def _get_cdsp_values(self, cr, uid, json_values, context=None):
+        ''' Map JSON values to fields.
+            Get_CDSP_Implementor method.
+        '''
         values = {}
         json_field_mapping = {
             'Name': 'name',
@@ -126,7 +209,32 @@ class compassion_project(orm.Model):
             if cdsp_impl.get(json_name):
                 values[field] = cdsp_impl[json_name]
 
-        self.write(cr, uid, [project.id], values, context=context)
+        return values
+
+    # Same as in child. Should we create a tool class handing this use-case ?
+    def _get_values(self, cr, uid, _list, property_name, context):
+        value_ids = []
+        if isinstance(_list, list):
+            for elem in _list:
+                value_ids.append(self._get_value_id(cr, uid, elem,
+                                                    property_name, context))
+        elif isinstance(_list, basestring):
+            value_ids.append(self._get_value_id(cr, uid, _list, property_name,
+                                                context))
+        return value_ids
+
+    def _get_value_id(self, cr, uid, value, property_name, context=None):
+        ''' Get id for value having property_name, else create one '''
+        prop_val_obj = self.pool.get('compassion.child.property.value')
+        value = value.lower()
+        val_ids = prop_val_obj.search(cr, uid, [('value_en', '=like', value),
+                                                ('property_name', '=', property_name)],
+                                      context=context)
+        if val_ids:
+            return val_ids[0]
+        prop_id = prop_val_obj.create(cr, uid, {'property_name': property_name,
+                                                'value_en': value})
+        return prop_id
 
     def _get_url(self, project_code, api_mess):
         url = config.get('compass_url')
