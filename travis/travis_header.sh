e81e738dc2c6ce99255e3937a13925e130cbde1a
Cyril Sester <cyril.sester@outlook.com>
2014-09-16 15:16:21 +0200

    [FIX] <CR> and BOM charater causing diff failure

--- travis/travis_header.sh
+++ travis/travis_header.sh
@@ -4,14 +4,14 @@ status=0
 for f in $(find . -name '*.py');
 do
   if [[ "$f" == *__openerp__.py ]]; then
-    dummy=$(diff <(head -n 12 travis/compassion_openerp_header.txt) <(head -n 12 $f))
+    dummy=$(diff <(sed 's/^\xef\xbb\xbf//' <(sed 's/\r$//' <(head -n 12 travis/compassion_openerp_header.txt))) <(sed 's/^\xef\xbb\xbf//' <(sed 's/^\xef\xbb\xbf//' <(sed 's/\r$//' <(head -n 12 $f)))))
     isOk=$?
-    dummy=$(diff <(sed -n '14,28p;28q' travis/compassion_openerp_header.txt) <(sed -n '14,28p;28q' $f))
+    dummy=$(diff <(sed 's/^\xef\xbb\xbf//' <(sed 's/\r$//' <(sed -n '14,28p;28q' travis/compassion_openerp_header.txt))) <(sed 's/\r$//' <(sed 's/^\xef\xbb\xbf//' <(sed -n '14,28p;28q' $f))))
     isOk2=$?
   else
-    dummy=$(diff <(head -n 5 travis/compassion_py_header.txt) <(head -n 5 $f))
+    dummy=$(diff <(sed 's/^\xef\xbb\xbf//' <(sed 's/\r$//' <(head -n 5 travis/compassion_py_header.txt))) <(sed 's/^\xef\xbb\xbf//' <(sed 's/\r$//' <(head -n 5 $f))))
     isOk=$?
-    dummy=$(diff <(sed -n '7,10p;10q' travis/compassion_py_header.txt) <(sed -n '7,10p;10q' $f))
+    dummy=$(diff <(sed 's/^\xef\xbb\xbf//' <(sed 's/\r$//' <(sed -n '7,10p;10q' travis/compassion_py_header.txt))) <(sed 's/^\xef\xbb\xbf//' <(sed 's/\r$//' <(sed -n '7,10p;10q' $f))))
     isOk2=$?
   fi
   if [[ $isOk -ne 0 || $isOK2 -ne 0 ]]; then
