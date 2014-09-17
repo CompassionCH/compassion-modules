7d824f3014c273c5c311d6084dd862ff74f2b76e
ecino <ecino@compassion.ch>
2014-09-17 17:05:12 +0200

    Improved the reconciliation so that it can handle multiple payments that cover one big invoice and big payments that cover multiple invoices.

--- account_advanced_reconcile_bvr_ref_compassion/advanced_reconciliation.py
+++ account_advanced_reconcile_bvr_ref_compassion/advanced_reconciliation.py
@@ -22,10 +22,20 @@ from openerp.osv import orm
 import datetime
 import pdb
 
+""" We keep track of reconciled debit lines so that we don't
+    try to reconcile them with multiple credit lines. """
+rec_debit_ids = {}
+
+# We keep track of reconciled credit lines
+rec_credit_ids = []
+
 class easy_reconcile_advanced_bvr_ref(orm.TransientModel):
 
     _name = 'easy.reconcile.advanced.bvr_ref'
     _inherit = 'easy.reconcile.advanced'
+    
+
+
 
     def _base_columns(self, rec):
         """ Mandatory columns for move lines queries
@@ -85,11 +95,21 @@ class easy_reconcile_advanced_bvr_ref(orm.TransientModel):
             future_invoice_ids = invoice_obj.search(cr, uid, [('bvr_reference','=',move_line['ref']),('state','=','open'),('date_due','>', datetime.date.today())], order='date_due asc', context=context)
             invoices = invoice_obj.browse(cr, uid, present_invoice_ids + future_invoice_ids, context=context)
             total_due = 0.0
+            credit_amount = move_line['credit']
             for invoice in invoices:
                 total_due += invoice.amount_total
-                if total_due == move_line['credit']:
+                if total_due == credit_amount:
                     # The credit line can fully reconcile an integer number of open invoices, it can be reconciled automatically.
                     return False
+            
+            if credit_amount < total_due:
+                # Check for other unreconciled credit lines that could complete the payment and reconcile all invoices.
+                credit_lines = self._query_credit(cr, uid, rec, context=context)
+                for other_credit in credit_lines:
+                    if other_credit['ref'] == move_line['ref'] and not other_credit['id'] in rec_credit_ids:
+                        credit_amount += other_credit['credit']
+                        if total_due == credit_amount:
+                            return False
                     
         # Skip reconciliation for this credit line.
         return True
@@ -105,7 +125,7 @@ class easy_reconcile_advanced_bvr_ref(orm.TransientModel):
 
     def _search_opposites(self, cr, uid, rec, move_line, opposite_move_lines, context=None):
         """
-        Search the opposite move lines for a move line
+        Search the opposite move lines for a credit move line
 
         We limit the search to the number of move_lines that can be fully reconciled to avoid partial reconciliation.
         """
@@ -114,9 +134,25 @@ class easy_reconcile_advanced_bvr_ref(orm.TransientModel):
         amount_reconciled = 0.0
         for op in opposite_move_lines:
             if amount_reconciled < move_line['credit']:
-                if self._compare_opposite(cr, uid, rec, move_line, op, matchers, context=context):
+                if self._compare_opposite(cr, uid, rec, move_line, op, matchers, context=context) and not self._debit_reconciled(move_line, op):
                     opposite_matchers.append(op)
                     amount_reconciled += op['debit']
             else:
                 break
         return opposite_matchers
+        
+    def _debit_reconciled(self, credit_line, debit_line):
+        """ Test if a debit line is already fully reconciled. If not, update the amount reconciled by credit_line. """
+        debit_line_id = debit_line['id']
+        amount_to_reconcile = min(debit_line['debit'], credit_line['credit'])
+        
+        if debit_line_id in rec_debit_ids.keys():
+            if rec_debit_ids[debit_line_id] == debit_line['debit']:
+                return True
+            amount_to_reconcile += rec_debit_ids[debit_line_id]
+        
+        rec_debit_ids.update({
+            debit_line_id: amount_to_reconcile
+        })
+        
+        return False
