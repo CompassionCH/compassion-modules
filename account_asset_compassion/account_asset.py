45c0f1afeeb09475690e96fadac588973813a0c2
ecino <ecino@compassion.ch>
2014-10-23 16:13:08 +0200

    Update to use the patch of module asset_management

--- account_asset_compassion/account_asset.py
+++ account_asset_compassion/account_asset.py
@@ -25,32 +25,44 @@ class asset_category(orm.Model):
 class account_asset_depreciation_line(orm.Model):
     _inherit = 'account.asset.depreciation.line'
 
-    def create_move(self, cr, uid, ids, context=None):
-        """ Put the analytics_ids in context for retrieving it in create
-        method of account_move_line."""
-        if context is None:
-            context = {}
-        # Dict containing {asset_id : analytic_distribution_id}
-        analytics_ids = {}
-        for line in self.browse(cr, uid, ids, context):
-            analytics_ids.update({
-                line.asset_id.id: line.asset_id.category_id.analytics_id.id})
-        context['analytics_ids'] = analytics_ids
+    def _setup_move_line_data(self, depreciation_line, depreciation_date,
+                              period_ids, account_id, type, context):
+        """ Add analytic distribution to move_line """
+        move_line_data = super(account_asset_depreciation_line, 
+                               self)._setup_move_line_data(
+                depreciation_line, depreciation_date, period_ids, account_id,
+                type, context)
+        if type == 'expense':
+            move_line_data.update({
+                'analytics_id': line.asset_id.category_id.analytics_id.id})
+        return move_line_data
+    
+    # def create_move(self, cr, uid, ids, context=None):
+        # """ Put the analytics_ids in context for retrieving it in create
+        # method of account_move_line."""
+        # if context is None:
+            # context = {}
+        # # Dict containing {asset_id : analytic_distribution_id}
+        # analytics_ids = {}
+        # for line in self.browse(cr, uid, ids, context):
+            # analytics_ids.update({
+                # line.asset_id.id: line.asset_id.category_id.analytics_id.id})
+        # context['analytics_ids'] = analytics_ids
 
-        return super(account_asset_depreciation_line,
-                     self).create_move(cr, uid, ids, context)
+        # return super(account_asset_depreciation_line,
+                     # self).create_move(cr, uid, ids, context)
 
 
-class account_move_line(orm.Model):
-    _inherit = 'account.move.line'
+# class account_move_line(orm.Model):
+    # _inherit = 'account.move.line'
 
-    def create(self, cr, uid, vals, context=None, check=True):
-        """ Retrieve the analytics ids from context, if defined,
-        and create move line."""
-        analytics_ids = context.get('analytics_ids')
-        asset_id = vals.get('asset_id')
-        if analytics_ids and asset_id:
-            vals['analytics_id'] = analytics_ids[asset_id]
+    # def create(self, cr, uid, vals, context=None, check=True):
+        # """ Retrieve the analytics ids from context, if defined,
+        # and create move line."""
+        # analytics_ids = context.get('analytics_ids')
+        # asset_id = vals.get('asset_id')
+        # if analytics_ids and asset_id:
+            # vals['analytics_id'] = analytics_ids[asset_id]
 
-        return super(account_move_line,
-                     self).create(cr, uid, vals, context, check)
+        # return super(account_move_line,
+                     # self).create(cr, uid, vals, context, check)
