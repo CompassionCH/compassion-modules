4db7c6bc7e0fe5bc6c10f5394346a6cd8ee8f667
ecino <ecino@compassion.ch>
2015-01-19 13:15:17 +0100

    Add gift details to message views
    - New field for gift instructions
    - Add related fields in view of messages for retrieving gift info

--- message_center_compassion/model/invoice_line.py
+++ message_center_compassion/model/invoice_line.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2014 Compassion CH (http://www.compassion.ch)
+#    Copyright (C) 2014-2015 Compassion CH (http://www.compassion.ch)
 #    Releasing children from poverty in Jesus' name
 #    @author: Emanuel Cino <ecino@compassion.ch>
 #
@@ -12,8 +12,9 @@
 from openerp.osv import orm, fields
 
 
-class invoice_line(orm.Model):
-    _inherit = 'account.invoice.line'
+class account_invoice_line(orm.Model):
+    """ We add a field for gift instructions. """
+    _inherit = "account.invoice.line"
 
     def _get_gift_info(self, cr, uid, ids, field_name, args, context=None):
         res = dict()
@@ -26,6 +27,7 @@ class invoice_line(orm.Model):
         return res
 
     def get_gift_details(self, cr, uid, line_id, context=None):
+        # TODO : See with Stuart if we can remove this method
         inv_line = self.browse(cr, uid, line_id, context)
         need_key = inv_line.contract_id.child_code
         if inv_line.product_id.gmc_name == 'ProjectGift':
@@ -35,11 +37,27 @@ class invoice_line(orm.Model):
             'NeedKey': inv_line.contract_id.child_code,
             'GiftType': inv_line.product_id.gmc_name,
             'GiftAmount': inv_line.price_subtotal,
-            'GiftInstructions': inv_line.name,
+            'GiftInstructions': inv_line.gift_instructions,
             'TransactionDate': inv_line.invoice_id.date_invoice
         }
         return res
 
+    def _get_instructions(self, cr, uid, ids, field, args, context=None):
+        res = dict()
+        ctx = context.copy()
+        for inv_line in self.browse(cr, uid, ids, context):
+            ctx['lang'] = inv_line.partner_id.lang
+            product = self.pool.get('product.product').browse(
+                cr, uid, inv_line.product_id.id, ctx)
+            if inv_line.product_id.name == inv_line.name or \
+                    product.name == inv_line.name:
+                res[inv_line.id] = ''
+            else:
+                res[inv_line.id] = inv_line.name
+        return res
+
     _columns = {
+        'gift_instructions': fields.function(
+            _get_instructions, type='char'),
         'need_key': fields.function(_get_gift_info, type='char')
     }
