0c3b2fd0aa7ec34fcbae5ad259fb181b035712a7
David Coninckx <davconinckx@gmail.com>
2015-02-10 16:20:47 +0100

    Add paragraph to explain procedure
    Add a delegation date and handle reset of delegation fields
    
    Add a list view for the child being modified
    
    Merge _sel_request_to_typo3 and _update_request_to_typo3 into _request_to_typo3
    
    Add few little comments
    
    Move pictures and descriptions check before creating request

--- child_compassion/wizard/child_on_internet_wizard.py
+++ child_compassion/wizard/child_on_internet_wizard.py
@@ -9,26 +9,55 @@
 #
 ##############################################################################
 
-from openerp.osv import orm
+from openerp.osv import orm, fields
+from openerp.tools.translate import _
 
 
 class child_on_internet_wizard(orm.TransientModel):
     _name = 'child.on.internet.wizard'
 
-    def put_child_on_internet(self, cr, uid, ids, context=None):
+    def _get_active_ids(self, cr, uid, ids, field_name, arg, context):
+        child_ids = list()
+
         child_obj = self.pool.get('compassion.child')
         childrens = child_obj.browse(
-            cr, uid, context.get('active_ids', list()), context)
-        child_ids = []
+            cr, uid, context.get('active_ids'), context)
 
         for child in childrens:
             possible_states = ['N', 'R']
             if child.state in possible_states:
                 child_ids.append(child.id)
 
-                if(not(child.desc_de and child.desc_fr)):
-                    raise orm.except_orm(
-                        _('Warning'), _('Missing descriptions'))
+        return {id: child_ids for id in ids}
 
+    def _default_child_ids(self, cr, uid, context):
+        return self._get_active_ids(cr, uid, [0], None, None, context)[0]
 
-        child_obj.child_add_to_typo3(cr, uid, child_ids, context=None)
+    _columns = {
+        'child_ids': fields.function(
+            _get_active_ids, type='one2many',
+            obj='compassion.child',
+            string=_('Selected childs')),
+    }
+
+    _defaults = {
+        'child_ids': (_default_child_ids)
+    }
+
+    def put_child_on_internet(self, cr, uid, ids, context=None):
+        child_ids = self._default_child_ids(cr, uid, context)
+
+        for child in self.pool.get('compassion.child').browse(
+                cr, uid, child_ids, context):
+            # Check for descriptions
+            if(not(child.desc_de) or not(child.desc_fr)):
+                raise orm.except_orm(
+                    _('Warning'), _('Missing descriptions'))
+
+            # Check for pictures
+            if not(child.portrait and child.fullshot):
+                raise orm.except_orm(
+                    _('Warning'), _('Child has no picture'))
+
+        self.pool.get('compassion.child').child_add_to_typo3(
+            cr, uid, child_ids, context=None)
