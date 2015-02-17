11f5b2a82da1fad974c4ed505b9cd4938414b859
ecino <ecino@compassion.ch>
2015-02-17 12:33:53 +0100

    Remove children of a suspended project from internet.

--- sponsorship_compassion/model/project_compassion.py
+++ sponsorship_compassion/model/project_compassion.py
@@ -20,12 +20,22 @@ class project_compassion(orm.Model):
         """ When a project is suspended, We update all contracts of
         sponsored children in the project, so that we don't create invoices
         during the period of suspension.
+        We also remove the children on internet.
         """
         project = self.browse(cr, uid, project_id, context)
         contract_obj = self.pool.get('recurring.contract')
-        contract_ids = contract_obj.search(
-            cr, uid, [('child_code', 'like', project.code),
-                      ('state', 'in', ('active', 'waiting'))], context=context)
+        contract_ids = contract_obj.search(cr, uid, [
+            ('child_code', 'like', project.code),
+            ('state', 'in', ('active', 'waiting'))], context=context)
         # For now, suspend the contract for 3 months
-        contract_obj.suspend_contract(cr, uid, contract_ids, start, 3, context)
+        contract_obj.suspend_contract(cr, uid, contract_ids, start, 3,
+                                      context)
+
+        # Children to remove from internet
+        child_obj = self.pool.get('compassion.child')
+        child_ids = child_obj.search(cr, uid, [
+            ('code', 'like', project.code),
+            ('state', '=', 'I')], context=context)
+        if child_ids:
+            child_obj.child_remove_from_typo3(cr, uid, child_ids, context)
         return True
