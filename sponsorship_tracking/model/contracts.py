0304bf91e7e52e77312804f2980a9132e3c7779a
David Coninckx <davconinckx@gmail.com>
2015-03-10 11:26:41 +0100

    Fix PEP8

--- sponsorship_tracking/model/contracts.py
+++ sponsorship_tracking/model/contracts.py
@@ -8,7 +8,6 @@
 #    The licence is in the file __openerp__.py
 #
 ##############################################################################
-
 from openerp.osv import orm, fields
 from openerp import netsvc
 from openerp.tools import DEFAULT_SERVER_DATE_FORMAT as DF
@@ -18,7 +17,6 @@ from datetime import datetime, date, timedelta
 import logging
 
 logger = logging.getLogger(__name__)
-import pdb
 
 
 class recurring_contract(orm.Model):
@@ -64,35 +62,39 @@ class recurring_contract(orm.Model):
             wkf_id, project_suspended_contracts_ids, context)
 
     def _ins_wkf_items(self, cr, uid, act_id, wkf_id, cont_ids, context=None):
-            ir_model_data = self.pool.get('ir.model.data')
-            wkf_activity_id = ir_model_data.get_object_reference(
-                cr, uid, 'sponsorship_tracking',
-                act_id)[1]
+        ir_model_data = self.pool.get('ir.model.data')
+        wkf_activity_id = ir_model_data.get_object_reference(
+            cr, uid, 'sponsorship_tracking',
+            act_id)[1]
 
-            wkf_instance_ids = list()
-            for contract_id in cont_ids:
-                cr.execute(
-                    '''
+        wkf_instance_ids = list()
+        for contract_id in cont_ids:
+            cr.execute(
+                '''
                     SELECT id FROM wkf_instance
                     WHERE wkf_id = {} AND res_id = {}
                     '''.format(wkf_id, contract_id))
-                res = cr.fetchall()
-                if res: wkf_instance_ids.append(res[0][0])
+            res = cr.fetchall()
+            if res:
+                wkf_instance_ids.append(res[0][0])
 
-            for wkf_instance_id in wkf_instance_ids:
-                cr.execute(
-                    '''
+        for wkf_instance_id in wkf_instance_ids:
+            cr.execute(
+                '''
                     INSERT INTO wkf_workitem(act_id, inst_id, state)
                     VALUES ('{}', '{}', '{}')
                     '''.format(wkf_activity_id, wkf_instance_id, 'complete')
-                )
+            )
+
     def _get_contract_ids(prop_obj, cr, uid, ids, context=None):
         contract_ids_to_update = list()
         for project in prop_obj.browse(cr, uid, ids, context):
             contract_obj = prop_obj.pool.get('recurring.contract')
             child_obj = prop_obj.pool.get('compassion.child')
-            child_ids = child_obj.search(cr, uid, [('code', 'like', project.code)], context=context)
-            contract_ids = contract_obj.search(cr, uid, [('child_id', 'in', child_ids)], context=context)
+            child_ids = child_obj.search(
+                cr, uid, [('code', 'like', project.code)], context=context)
+            contract_ids = contract_obj.search(
+                cr, uid, [('child_id', 'in', child_ids)], context=context)
             contract_ids_to_update.extend(contract_ids)
         return contract_ids_to_update
 
@@ -114,7 +116,7 @@ class recurring_contract(orm.Model):
                 SET sds_state = 'active'
                 WHERE id = '{}'
                 '''.format(active_contract_id)
-                )
+            )
         for draft_contract_id in draft_contract_ids:
             cr.execute(
                 '''
@@ -122,21 +124,24 @@ class recurring_contract(orm.Model):
                 SET sds_state = 'draft'
                 WHERE id = '{}'
                 '''.format(draft_contract_id)
-                )
-    # Only at module installation            
+            )
+    # Only at module installation
+
     def _set_project_state(self, cr, uid, ids=None, context=None):
-        suspended_project_ids = self.pool.get('compassion.project').search(cr, uid, [('status', '=', 'S')], context=context)
+        suspended_project_ids = self.pool.get('compassion.project').search(
+            cr, uid, [('status', '=', 'S')], context=context)
         suspended_project_contract_ids = self.search(
             cr, uid,
             [('project_id', 'in', suspended_project_ids),
-            ('state', 'not in', ['terminated', 'cancelled'])],
+             ('state', 'not in', ['terminated', 'cancelled'])],
             context)
 
-        active_project_ids = self.pool.get('compassion.project').search(cr, uid, [('status', '=', 'A')], context=context)
+        active_project_ids = self.pool.get('compassion.project').search(
+            cr, uid, [('status', '=', 'A')], context=context)
         active_project_contract_ids = self.search(
             cr, uid,
             [('project_id', 'in', active_project_ids),
-            ('state', 'not in', ['terminated', 'cancelled'])],
+             ('state', 'not in', ['terminated', 'cancelled'])],
             context)
 
         for suspended_project_contract_id in suspended_project_contract_ids:
@@ -146,7 +151,7 @@ class recurring_contract(orm.Model):
                 SET project_state = 'suspended'
                 WHERE id = '{}'
                 '''.format(suspended_project_contract_id)
-                )
+            )
 
         for active_project_contract_id in active_project_contract_ids:
             cr.execute(
@@ -155,7 +160,7 @@ class recurring_contract(orm.Model):
                 SET project_state = 'active'
                 WHERE id = '{}'
                 '''.format(active_project_contract_id)
-                )
+            )
 
     _columns = {
         'sds_state': fields.selection([
@@ -178,21 +183,25 @@ class recurring_contract(orm.Model):
             type='date', string=_('Date sub'),
             store=True, readonly=True),
         'project_id': fields.related(
-            'child_id', 'project_id', type='many2one', string=_('Project'), readonly=True
-            ),
+            'child_id', 'project_id',
+            type='many2one', string=_('Project'),
+            readonly=True
+        ),
         'project_state': fields.selection([
             ('active', _('Active')),
             ('inform_suspended', _('Inform suspended')),
             ('suspended', _('Suspended')),
             ('inform_reactivation', _('Inform reactivation')),
             ('waiting_attribution', _('Waiting attribution')),
-            ('inform_suspended_reactivation', _('Inform suspended and reactivation')),
+            ('inform_suspended_reactivation',
+             _('Inform suspended and reactivation')),
             ('inform_project_terminated', _('Inform project terminated')),
             ('phase_out', _('Phase out')),
             ('terminated', _('Terminated'))], _('Project Status'), select=True,
             readonly=True, track_visibility='onchange',
             help=_('')),
     }
+
     def copy(self, cr, uid, id, default=None, context=None):
         if not default:
             default = dict()
@@ -251,27 +260,32 @@ class recurring_contract(orm.Model):
 
     def sub_reject(self, cr, uid, ids, context=None):
         self.write(cr, uid, ids, {'sds_state': 'sub_reject'}, context)
-        
+
     def inform_project_suspended(self, cr, uid, ids, context=None):
-        self.write(cr, uid, ids, {'project_state': 'inform_suspended'}, context)
-        
+        self.write(
+            cr, uid, ids, {'project_state': 'inform_suspended'}, context)
+
     def inform_project_terminated(self, cr, uid, ids, context=None):
-        self.write(cr, uid, ids, {'project_state': 'inform_project_terminated'}, context)
-        
+        self.write(
+            cr, uid, ids,
+            {'project_state': 'inform_project_terminated'},
+            context)
+
     def inform_project_reactivation(self, cr, uid, ids, context=None):
-        self.write(cr, uid, ids, {'project_state': 'inform_reactivation'}, context)
-        
+        self.write(
+            cr, uid, ids, {'project_state': 'inform_reactivation'}, context)
+
     def suspended(self, cr, uid, ids, context=None):
         self.write(cr, uid, ids, {'project_state': 'suspended'}, context)
-    
+
     def project_active(self, cr, uid, ids, context=None):
         self.write(cr, uid, ids, {'project_state': 'project_active'}, context)
-    
+
     def project_terminated(self, cr, uid, ids, context=None):
-        self.write(cr, uid, ids, {'project_state': 'terminated'}, context)  
-        
+        self.write(cr, uid, ids, {'project_state': 'terminated'}, context)
+
     def project_phase_out(self, cr, uid, ids, context=None):
-        self.write(cr, uid, ids, {'project_state': 'phase_out'}, context)  
+        self.write(cr, uid, ids, {'project_state': 'phase_out'}, context)
 
     def welcome_sent(self, cr, uid, contract_id, context=None):
         wf_service = netsvc.LocalService('workflow')
@@ -309,7 +323,7 @@ class recurring_contract(orm.Model):
         wf_service = netsvc.LocalService('workflow')
         logger.info("Contract " + str(contract_id) + " project suspended.")
         wf_service.trg_validate(uid, 'recurring.contract', contract_id,
-                                'project_suspended', cr)     
+                                'project_suspended', cr)
 
     def check_sub_waiting_duration(self, cr, uid, context=None):
         contract_ids = self.search(
@@ -391,7 +405,7 @@ class recurring_contract(orm.Model):
                         cr, uid,
                         [('parent_id', '=', same_partner_contract.id)],
                         context=context)):
-                        return same_partner_contract.id
+                    return same_partner_contract.id
         return False
 
     def switch_contract_view(self, cr, uid, contract_id, context=None):
