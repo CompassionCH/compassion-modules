885255a52ca533f04cd9c8edb605633fb69e5528
David Coninckx <davconinckx@gmail.com>
2015-04-07 09:22:49 +0200

    Add to prefilter views for the buttons "Unreconciled Transaction Items" and "1050 Transaction Items" in partner view

--- sponsorship_compassion/model/res_partner.py
+++ sponsorship_compassion/model/res_partner.py
@@ -80,7 +80,7 @@ class res_partner(orm.Model):
                 'view_invoice_line_partner_tree')[1]
         except ValueError:
             invoice_line_id = False
-
+        context['search_default_partner_id'] = ids
         action = {
             'name': 'Related invoice lines',
             'type': 'ir.actions.act_window',
@@ -90,7 +90,30 @@ class res_partner(orm.Model):
             'view_id': invoice_line_id,
             'views': [(invoice_line_id, 'tree'), (False, 'form')],
             'target': 'current',
-            'context': {'search_default_partner_id': ids},
+            'context': context,
+        }
+
+        return action
+
+    def show_move_lines(self, cr, uid, ids, context=None):
+        try:
+            ir_model_data = self.pool.get('ir.model.data')
+            move_line_id = ir_model_data.get_object_reference(
+                cr, uid, 'account',
+                'view_move_line_tree')[1]
+        except ValueError:
+            move_line_id = False
+        context['search_default_partner_id'] = ids
+        action = {
+            'name': 'Related invoice lines',
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'tree',
+            'res_model': 'account.move.line',
+            'view_id': move_line_id,
+            'views': [(move_line_id, 'tree')],
+            'target': 'current',
+            'context': context,
         }
 
         return action
@@ -109,3 +132,15 @@ class res_partner(orm.Model):
             'target': 'current',
             'context': context
         }
+
+    def unreconciled_transaction_items(self, cr, uid, ids, context=None):
+        context['search_default_unreconciled'] = 1
+        return self.show_move_lines(cr, uid, ids, context)
+
+    def receivable_transaction_items(self, cr, uid, ids, context=None):
+        account_ids = self.pool.get('account.account').search(
+            cr, uid,
+            [('code', '=', '1050')],
+            context=context)
+        context['search_default_account_id'] = account_ids[0]
+        return self.show_move_lines(cr, uid, ids, context)
