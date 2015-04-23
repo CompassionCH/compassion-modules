e69118abfda2229f31de1639cd11bd52402f7b67
ecino <ecino@compassion.ch>
2015-04-23 12:12:05 +0200

    GMC state field gets more generic to be inherited.

--- message_center_compassion/model/child_properties.py
+++ message_center_compassion/model/child_properties.py
@@ -24,5 +24,5 @@ class child_properties(orm.Model):
         property = self.browse(cr, uid, ids, context)[0]
         for contract in property.child_id.contract_ids:
             if contract.state in ('waiting', 'active', 'mandate'):
-                contract.write({'gmc_state': 'biennial'})
+                contract.new_biennial()
         return res
