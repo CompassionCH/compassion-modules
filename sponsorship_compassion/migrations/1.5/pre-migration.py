427393302c31ff68110458862881f4519b7629bf
David Coninckx <davconinckx@gmail.com>
2015-04-27 13:42:42 +0200

    Fix PEP8

--- sponsorship_compassion/migrations/1.5/pre-migration.py
+++ sponsorship_compassion/migrations/1.5/pre-migration.py
@@ -19,43 +19,43 @@ def migrate(cr, version):
     if not version:
         return
 
-    activities_to_migrate = ['banking_validation', 'validation', 'mandate', 'cancelled', 'waiting']
-    
+    activities_to_migrate = [
+        'banking_validation', 'validation', 'mandate', 'cancelled', 'waiting']
+
     for activity in activities_to_migrate:
         cr.execute(
-        '''
+            '''
         SELECT id,create_date FROM wkf_activity
         WHERE name = '{}'
         '''.format(activity))
-        
+
         activity_ids = cr.fetchall()
 
-        new_activity_id = max(activity_ids,key=itemgetter(1))[0]
-        old_activity_id = min(activity_ids,key=itemgetter(1))[0]
-        
+        new_activity_id = max(activity_ids, key=itemgetter(1))[0]
+        old_activity_id = min(activity_ids, key=itemgetter(1))[0]
+
         # Update workitems
         cr.execute(
-        '''
-        UPDATE wkf_workitem 
+            '''
+        UPDATE wkf_workitem
         SET act_id = {0}
         WHERE act_id = {1}
         '''.format(new_activity_id, old_activity_id)
         )
-        
+
         # Update transitions
         cr.execute(
-        '''
-        UPDATE wkf_transition 
+            '''
+        UPDATE wkf_transition
         SET act_to = {0}
         WHERE act_to = {1}
         '''.format(new_activity_id, old_activity_id)
         )
 
         cr.execute(
-        '''
-        UPDATE wkf_transition 
+            '''
+        UPDATE wkf_transition
         SET act_from = {0}
         WHERE act_from = {1}
         '''.format(new_activity_id, old_activity_id)
         )
-    
