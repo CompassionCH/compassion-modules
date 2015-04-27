b877fbde84693cb1e02954f5c899915f45b12ff5
David Coninckx <davconinckx@gmail.com>
2015-04-27 16:04:17 +0200

    Remove product type (use product.category instead)

--- contract_compassion/model/product.py
+++ contract_compassion/model/product.py
@@ -16,10 +16,3 @@ from openerp.tools.translate import _
 class product(orm.Model):
     _inherit = 'product.product'
 
-    _columns = {
-        'type': fields.selection([
-            ('S', _('Sponsorship')),
-            ('B', _('Both')),
-            ('G', _('Gifts'))], _('Type'), select=True,
-            readonly=True)
-    }
