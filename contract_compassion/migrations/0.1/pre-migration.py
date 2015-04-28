cbde55bb5abc9d47ed0f7a98d9903503464905cc
David Coninckx <davconinckx@gmail.com>
2015-04-28 15:29:44 +0200

    Fix PEP8 and remove unused migration script

--- contract_compassion/migrations/0.1/pre-migration.py
+++ contract_compassion/migrations/0.1/pre-migration.py
@@ -9,21 +9,18 @@
 #
 ##############################################################################
 import sys
-from operator import itemgetter
-import pdb
-
 
 
 def migrate(cr, version):
     reload(sys)
     sys.setdefaultencoding('UTF8')
-    pdb.set_trace()
+
     if not version:
         return
-    
+
     # Remove new ir_model_data for contract_compassion product
     cr.execute(
-    """
+        """
     DELETE FROM ir_model_data
     WHERE module='contract_compassion' AND
     model='product.product'
@@ -31,17 +28,17 @@ def migrate(cr, version):
     )
     # Modify old ir_model_data to change module name
     cr.execute(
-    """
+        """
     UPDATE ir_model_data
     SET module='contract_compassion'
     WHERE module='sponsorship_compassion' AND
     model='product.product'
     """
     )
-    
+
     # Remove new ir_model_data for contract_compassion workflow activities
     cr.execute(
-    """
+        """
     DELETE FROM ir_model_data
     WHERE module='contract_compassion' AND
     model IN ('workflow.activity','workflow.transition')
@@ -49,11 +46,10 @@ def migrate(cr, version):
     )
     # Modify old ir_model_data to change module name
     cr.execute(
-    """
+        """
     UPDATE ir_model_data
     SET module='contract_compassion'
     WHERE module='sponsorship_compassion' AND
     model IN ('workflow.activity','workflow.transition')
     """
     )
-    pdb.set_trace()
