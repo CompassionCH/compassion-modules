febb13f9fd228634376ada1b20b8e3becf79024a
ecino <ecino@compassion.ch>
2015-05-12 13:27:18 +0200

    Correct colors in some fields. Store categ_name of products always in english

--- contract_compassion/model/product.py
+++ contract_compassion/model/product.py
@@ -16,9 +16,14 @@ from openerp.tools.translate import _
 class product(orm.Model):
     _inherit = 'product.product'
 
+    def _get_categ_name(self, cr, uid, ids, name, args, context=None):
+        return {p.id: p.product_tmpl_id.categ_id.name for p in
+                self.browse(cr, uid, ids, {'lang': 'en_US'})}
+
     _columns = {
         'gp_fund_id': fields.integer("GP Fund id", size=4),
-        'categ_name': fields.related(
-            'product_tmpl_id', 'categ_id', 'name',
-            type="char", string=_('Product category')),
+        'categ_name': fields.function(
+            _get_categ_name,
+            type="char", string=_('Product category'),
+            store=True)
     }
