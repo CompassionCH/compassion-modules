62ba815980b5f10e35328b55be479690858a4a9c
steveferry <steve.ferry1992@gmail.com>
2015-05-27 01:48:14 +0200

    Bank account traking on delete Override unlink function to notify the removal  account by a message

--- partner_compassion/model/partner_bank_compassion.py
+++ partner_compassion/model/partner_bank_compassion.py
@@ -12,12 +12,14 @@ from openerp.osv import orm
 
 
 class ResPartnerBank(orm.Model):
-    """ This class upgrade the partners to match Compassion needs.
+    """ This class upgrade the partners.bank to match Compassion needs.
     """
 
     _inherit = 'res.partner.bank'
 
     def create(self, cr, uid, data, context):
+        """Override function to notify creation in a message
+        """
         obj_partner = self.pool['res.partner']
         part = obj_partner.browse(cr, uid, data['partner_id'], context=context)
         part.pool.get('mail.thread').message_post(cr, uid, part.id,
@@ -30,4 +32,24 @@ class ResPartnerBank(orm.Model):
         result = super(ResPartnerBank, self).create(cr, uid, data,
                                                     context=context)
         self.post_write(cr, uid, [result], context=context)
+        return result
+
+    def unlink(self, cr, uid, ids, context):
+        """Override function to notify delte in a message
+        """
+
+        obj_bank = self.pool['res.partner.bank']
+        account = None
+
+        for account in obj_bank.browse(cr, uid, ids, context=context):
+            account.partner_id.pool.get('mail.thread'). \
+                message_post(cr, uid, account.partner_id.id,
+                             "<b>Account number: </b>" +
+                             account.acc_number,
+                             "Account deleted",
+                             'comment',
+                             context={'thread_model':
+                                      account.partner_id._name})
+        result = super(ResPartnerBank, self).unlink(cr, uid, ids,
+                                                    context=context)
         return result
