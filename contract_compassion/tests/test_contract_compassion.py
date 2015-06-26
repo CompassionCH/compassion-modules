49f149588c2ad26c9d9b79a4ea1f8999560b516c
Albert Shenouda <albert.shenouda@efrei.net>
2015-06-26 10:50:15 +0200

    Fix review issues

--- contract_compassion/tests/test_contract_compassion.py
+++ contract_compassion/tests/test_contract_compassion.py
@@ -30,7 +30,7 @@ class test_contract_compassion(common.TransactionCase):
 
     def setUp(self):
         super(test_contract_compassion, self).setUp()
-        # Creation of an account
+        # Retrieve receivable and payable accounts
         account_type = self.registry('account.account.type').search(
             self.cr, self.uid, [
                 ('code', '=', 'receivable'),
@@ -69,7 +69,7 @@ class test_contract_compassion(common.TransactionCase):
             'notification_email_send': 'none',
             'ref': '00002222',
         })
-        # Creation of a payement term
+        # Retrieve a payement term
         payment_term_obj = self.registry('account.payment.term')
         self.payment_term_id = payment_term_obj.search(self.cr, self.uid, [
             ('name', '=', '15 Days')
@@ -80,7 +80,161 @@ class test_contract_compassion(common.TransactionCase):
             'property_account_income': property_account_income,
             })
 
-    def _create_contract(self, start_date, group_id, channel,
+    def test_contract_compassion_first_scenario(self):
+        """
+            In this test we are testing states changement of a contract and if
+            the old invoice are well cancelled when we pay one invoice.
+        """
+        # Testing area creation
+        contract_group = self._create_group_id(
+            'do_nothing', 1, 'month', self.partner_id, 5, self.payment_term_id)
+        contract_id = self._create_contract_id(
+            datetime.today().strftime(DF), contract_group,
+            'phone', datetime.today().strftime(DF))
+        self._create_contract_line_id(
+            contract_id, '2', '40.0')
+
+        contract_obj = self.registry('recurring.contract')
+        contract = contract_obj.browse(self.cr, self.uid, contract_id)
+        self.assertEqual(contract.state, 'draft')
+
+        # Switching to "waiting for payment" state
+        wf_service = netsvc.LocalService('workflow')
+        wf_service.trg_validate(
+            self.uid, 'recurring.contract',
+            contract_id, 'contract_validated', self.cr)
+        contract = contract_obj.browse(
+            self.cr, self.uid, contract_id)
+        self.assertEqual(contract.state, 'waiting')
+        invoicer_obj = self.registry('recurring.invoicer')
+        invoicer_id = contract.button_generate_invoices()
+        invoices = invoicer_obj.browse(
+            self.cr, self.uid, invoicer_id).invoice_ids
+        nb_invoices = len(invoices)
+        self.assertEqual(nb_invoices, 6)
+        self.assertEqual(invoices[3].state, 'open')
+        # Payment of the third invoice so the
+        # contract will be on the active state and the 2 first invoices should
+        # be canceled.
+        self._pay_invoice(invoices[3].id)
+        invoices = invoicer_obj.browse(
+            self.cr, self.uid, invoicer_id).invoice_ids
+        contract = contract_obj.browse(self.cr, self.uid, contract_id)
+        self.assertEqual(invoices[3].state, 'paid')
+        self.assertEqual(invoices[0].state, 'open')
+        self.assertEqual(invoices[1].state, 'open')
+        self.assertEqual(invoices[2].state, 'open')
+        self.assertEqual(invoices[4].state, 'cancel')
+        self.assertEqual(invoices[5].state, 'cancel')
+        self.assertEqual(contract.state, 'active')
+        wf_service.trg_validate(
+            self.uid, 'recurring.contract',
+            contract_id, 'contract_terminated', self.cr)
+        contract = contract_obj.browse(
+            self.cr, self.uid, contract_id)
+        self.assertEqual(contract.state, 'terminated')
+
+    def test_contract_compassion_second_scenario(self):
+        """
+            Testing if invoices are well concelled when we cancel the related
+            contract.
+        """
+        # Testing area creation
+        contract_group = self._create_group_id(
+            'do_nothing', 1, 'month', self.partner_id1, 1,
+            self.payment_term_id)
+        contract_id = self._create_contract_id(
+            datetime.today().strftime(DF), contract_group, 'postal',
+            datetime.today().strftime(DF))
+        self._create_contract_line_id(
+            contract_id, '3', '200.0')
+
+        # Switch to "waiting for payment" state
+        wf_service = netsvc.LocalService('workflow')
+        wf_service.trg_validate(
+            self.uid, 'recurring.contract',
+            contract_id, 'contract_validated', self.cr)
+        contract_obj = self.registry('recurring.contract')
+        contract = contract_obj.browse(
+            self.cr, self.uid, contract_id)
+        invoicer_obj = self.registry('recurring.invoicer')
+        invoicer_id = contract.button_generate_invoices()
+        invoices = invoicer_obj.browse(
+            self.cr, self.uid, invoicer_id).invoice_ids
+        self.assertEqual(invoices[0].state, 'open')
+        self.assertEqual(invoices[1].state, 'open')
+        self.assertEqual(len(invoices), 2)
+
+        # Cancelling of the contract
+        wf_service.trg_validate(
+            self.uid, 'recurring.contract',
+            contract_id, 'contract_terminated', self.cr)
+        contract = contract_obj.browse(
+            self.cr, self.uid, contract_id)
+        self.assertEqual(contract.state, 'cancelled')
+        invoices = invoicer_obj.browse(
+            self.cr, self.uid, invoicer_id).invoice_ids
+        self.assertEqual(invoices[0].state, 'cancel')
+        self.assertEqual(invoices[1].state, 'cancel')
+
+    def test_reset_open_invoices(self):
+        """
+            Testing of the method that update invoices when the contract
+            is updated.
+            THe invoice paid should not be updated, whereas the other one
+            should be upadted.
+        """
+        # Testing area creation
+        contract_group = self._create_group_id(
+            'do_nothing', 1, 'month', self.partner_id, 1,
+            self.payment_term_id)
+        contract_group2 = self._create_group_id(
+            'do_nothing', 1, 'month', self.partner_id1, 2,
+            self.payment_term_id)
+        contract_id = self._create_contract_id(
+            datetime.today().strftime(DF), contract_group,
+            'postal', datetime.today().strftime(DF))
+        contract_line_id = self._create_contract_line_id(
+            contract_id, '2', '60.0')
+
+        wf_service = netsvc.LocalService('workflow')
+        wf_service.trg_validate(
+            self.uid, 'recurring.contract',
+            contract_id, 'contract_validated', self.cr)
+        contract_obj = self.registry('recurring.contract')
+        contract = contract_obj.browse(
+            self.cr, self.uid, contract_id)
+        invoicer_obj = self.registry('recurring.invoicer')
+        invoicer_id = contract.button_generate_invoices()
+        invoicer = invoicer_obj.browse(self.cr, self.uid, invoicer_id)
+        invoices = invoicer_obj.browse(
+            self.cr, self.uid, invoicer_id).invoice_ids
+        self._pay_invoice(invoices[1].id)
+        self.assertEqual(len(invoices), 2)
+        
+        # Updating of the contract
+        contract_line_obj = self.registry('recurring.contract.line')
+        contract_line_obj.write(
+            self.cr, self.uid, contract_line_id, {
+                'quantity': '3',
+                'amount': '100.0',
+            })
+        contract_obj.write(
+            self.cr, self.uid, contract_id, {
+                'group_id': contract_group2,
+            })
+        ctr_line_id = contract_line_obj.browse(
+            self.cr, self.uid, contract_line_id)
+
+        # Check if the invoice unpaid is well updated
+        invoice_obj = self.registry('account.invoice')
+        invoice_upd = invoice_obj.browse(self.cr, self.uid, invoices[0].id)
+        invoice_line_up = self.registry('account.invoice.line').browse(
+            self.cr, self.uid, invoice_upd.invoice_line[0].id)
+        self.assertEqual(invoice_line_up.price_unit, ctr_line_id.amount)
+        self.assertEqual(invoice_line_up.price_subtotal, ctr_line_id.subtotal)
+
+    def _create_contract_id(self, start_date, group_id, channel,
                          next_invoice_date):
         """
             Create a contract. For that purpose we have created a partner
@@ -101,7 +255,7 @@ class test_contract_compassion(common.TransactionCase):
         })
         return contract_id
 
-    def _create_contract_line(self, contract_id, quantity, price):
+    def _create_contract_line_id(self, contract_id, quantity, price):
         """ Create contract's lines """
         contract_line_obj = self.registry('recurring.contract.line')
         contract_line_id = contract_line_obj.create(self.cr, self.uid, {
@@ -112,7 +266,7 @@ class test_contract_compassion(common.TransactionCase):
         })
         return contract_line_id
 
-    def _create_group(self, change_method, rec_value, rec_unit, partner_id,
+    def _create_group_id(self, change_method, rec_value, rec_unit, partner_id,
                       adv_biling_months, payment_term_id, ref=None):
         """
             Create a group with 2 possibilities :
@@ -175,165 +329,3 @@ class test_contract_compassion(common.TransactionCase):
             ('move_id', '=', invoice.move_id.id),
             ('account_id', '=', account_id)]) + [mv_line_id]
         move_line_obj.reconcile(self.cr, self.uid, to_reconcile)
-
-    def test_contract_compassion_first_scenario(self):
-        """
-            In this test we are testing states changement of a contract and if
-            the old invoice are well cancelled when we pay one invoice.
-        """
-        # Testing area creation
-        self.contract_group = self._create_group(
-            'do_nothing', 1, 'month', self.partner_id, 5, self.payment_term_id)
-        self.contract_id = self._create_contract(
-            datetime.today().strftime(DF), self.contract_group,
-            'phone', datetime.today().strftime(DF))
-        self.contract_line_id = self._create_contract_line(
-            self.contract_id, '2', '40.0')
-
-        contract_obj = self.registry('recurring.contract')
-        contract = contract_obj.browse(self.cr, self.uid, self.contract_id)
-        self.assertEqual(contract.state, 'draft')
-
-        # Switching to "waiting for payment" state
-        wf_service = netsvc.LocalService('workflow')
-        wf_service.trg_validate(
-            self.uid, 'recurring.contract',
-            self.contract_id, 'contract_validated', self.cr)
-        contract_wait = contract_obj.browse(
-            self.cr, self.uid, self.contract_id)
-        self.assertEqual(contract_wait.state, 'waiting')
-        invoice_obj = self.registry('account.invoice')
-        invoicer_obj = self.registry('recurring.invoicer')
-        invoicer_id = contract_wait.button_generate_invoices()
-        invoicer = invoicer_obj.browse(self.cr, self.uid, invoicer_id)
-        invoices = invoicer.invoice_ids
-        nb_invoices = len(invoices)
-        self.assertEqual(nb_invoices, 6)
-        invoice3 = invoices[3]
-        self.assertEqual(invoice3.state, 'open')
-        # Payment of the third invoice so the
-        # contract will be on the active state and the 2 first invoices should
-        # be canceled.
-        self._pay_invoice(invoice3.id)
-        invoice_paid = invoice_obj.browse(self.cr, self.uid, invoices[0].id)
-        invoice_paid1 = invoice_obj.browse(self.cr, self.uid, invoices[1].id)
-        invoice_paid2 = invoice_obj.browse(self.cr, self.uid, invoices[2].id)
-        invoice_paid3 = invoice_obj.browse(self.cr, self.uid, invoice3.id)
-        invoice_paid4 = invoice_obj.browse(self.cr, self.uid, invoices[4].id)
-        invoice_paid5 = invoice_obj.browse(self.cr, self.uid, invoices[5].id)
-        contract_act = contract_obj.browse(self.cr, self.uid, self.contract_id)
-        self.assertEqual(invoice_paid3.state, 'paid')
-        self.assertEqual(invoice_paid.state, 'open')
-        self.assertEqual(invoice_paid1.state, 'open')
-        self.assertEqual(invoice_paid2.state, 'open')
-        self.assertEqual(invoice_paid4.state, 'cancel')
-        self.assertEqual(invoice_paid5.state, 'cancel')
-        self.assertEqual(contract_act.state, 'active')
-        wf_service.trg_validate(
-            self.uid, 'recurring.contract',
-            self.contract_id, 'contract_terminated', self.cr)
-        contract_term = contract_obj.browse(
-            self.cr, self.uid, self.contract_id)
-        self.assertEqual(contract_term.state, 'terminated')
-
-    def test_contract_compassion_second_scenario(self):
-        """
-            Testing if invoices are well concelled when we cancel the related
-            contract.
-        """
-        # Testing area creation
-        self.contract_group = self._create_group(
-            'do_nothing', 1, 'month', self.partner_id1, 1,
-            self.payment_term_id)
-        self.contract_id = self._create_contract(
-            datetime.today().strftime(DF), self.contract_group, 'postal',
-            datetime.today().strftime(DF))
-        self.contract_line_id = self._create_contract_line(
-            self.contract_id, '3', '200.0')
-
-        # Switch to "waiting for payment" state
-        wf_service = netsvc.LocalService('workflow')
-        wf_service.trg_validate(
-            self.uid, 'recurring.contract',
-            self.contract_id, 'contract_validated', self.cr)
-        contract_obj = self.registry('recurring.contract')
-        contract_wait = contract_obj.browse(
-            self.cr, self.uid, self.contract_id)
-        invoicer_obj = self.registry('recurring.invoicer')
-        invoicer_id = contract_wait.button_generate_invoices()
-        invoicer = invoicer_obj.browse(self.cr, self.uid, invoicer_id)
-        invoices = invoicer.invoice_ids
-        invoice = invoices[0]
-        invoice1 = invoices[1]
-        self.assertEqual(invoice.state, 'open')
-        self.assertEqual(invoice1.state, 'open')
-
-        # Cancelling of the contract
-        wf_service.trg_validate(
-            self.uid, 'recurring.contract',
-            self.contract_id, 'contract_terminated', self.cr)
-        contract_act = contract_obj.browse(
-            self.cr, self.uid, self.contract_id)
-        self.assertEqual(contract_act.state, 'cancelled')
-        invoice_cancel = self.registry('account.invoice').browse(
-            self.cr, self.uid, invoice.id)
-        invoice1_cancel = self.registry('account.invoice').browse(
-            self.cr, self.uid, invoice1.id)
-        self.assertEqual(invoice_cancel.state, 'cancel')
-        self.assertEqual(invoice1_cancel.state, 'cancel')
-
-    def test_reset_open_invoices(self):
-        """
-            Testing of the method that update invoices when the contract
-            is updated.
-            THe invoice paid should not be updated, whereas the other one
-            should be upadted.
-        """
-        # Testing area creation
-        self.contract_group = self._create_group(
-            'do_nothing', 1, 'month', self.partner_id, 1,
-            self.payment_term_id)
-        self.contract_group2 = self._create_group(
-            'do_nothing', 1, 'month', self.partner_id1, 2,
-            self.payment_term_id)
-        self.contract_id = self._create_contract(
-            datetime.today().strftime(DF), self.contract_group,
-            'postal', datetime.today().strftime(DF))
-        self.contract_line_id = self._create_contract_line(
-            self.contract_id, '2', '60.0')
-
-        wf_service = netsvc.LocalService('workflow')
-        wf_service.trg_validate(
-            self.uid, 'recurring.contract',
-            self.contract_id, 'contract_validated', self.cr)
-        contract_obj = self.registry('recurring.contract')
-        contract_wait = contract_obj.browse(
-            self.cr, self.uid, self.contract_id)
-        invoicer_obj = self.registry('recurring.invoicer')
-        invoicer_id = contract_wait.button_generate_invoices()
-        invoicer = invoicer_obj.browse(self.cr, self.uid, invoicer_id)
-        invoices = invoicer.invoice_ids
-        invoice1 = invoices[1]
-        self._pay_invoice(invoice1.id)
-
-        # Updating of the contract
-        contract_line_obj = self.registry('recurring.contract.line')
-        contract_line_obj.write(
-            self.cr, self.uid, self.contract_line_id, {
-                'quantity': '3',
-                'amount': '100.0',
-            })
-        contract_obj.write(
-            self.cr, self.uid, self.contract_id, {
-                'group_id': self.contract_group2,
-            })
-        ctr_line_id = contract_line_obj.browse(
-            self.cr, self.uid, self.contract_line_id)
-
-        # Check if the invoice unpaid is well updated
-        invoice_obj = self.registry('account.invoice')
-        invoice_upd = invoice_obj.browse(self.cr, self.uid, invoices[0].id)
-        invoice_line_up = self.registry('account.invoice.line').browse(
-            self.cr, self.uid, invoice_upd.invoice_line[0].id)
-        self.assertEqual(invoice_line_up.price_unit, ctr_line_id.amount)
-        self.assertEqual(invoice_line_up.price_subtotal, ctr_line_id.subtotal)
