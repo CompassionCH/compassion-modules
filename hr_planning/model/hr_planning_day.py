fe62742f0d20383e3bc80aee1bdb342c131e72bb
steveferry <steve.ferry1992@gmail.com>
2015-07-13 15:25:58 +0200

    Porting hr_planning

--- hr_planning/model/hr_planning_day.py
+++ hr_planning/model/hr_planning_day.py
@@ -9,42 +9,23 @@
 #
 ##############################################################################
 
-import openerp
-from openerp import models
-from openerp.osv import orm, fields
-from openerp.tools.translate import _
+from openerp import fields, models, _
 
 
 class hr_planning_day(models.Model):
     _name = "hr.planning.day"
     '''
         Simple model for a working day for an employee.
-    # '''
-    # _columns = {
-        # 'employee_id': fields.many2one(
-            # 'hr.employee', 'Employee', readonly=True),
-        # 'start_date': fields.datetime('Start date', readonly=True),
-        # 'end_date': fields.datetime('End date', readonly=True),
-        # 'contract_id': fields.many2one(
-            # 'hr.contract', 'Contract', readonly=True),
-        # 'department_id': fields.related(
-            # 'employee_id', 'department_id',
-            # string=_('Department'), type="many2one",
-            # relation='hr.department', store=True, readonly=True),
-        # 'category_ids': fields.related(
-            # 'employee_id', 'category_ids',
-            # string=_('Category'), type='many2many',
-            # relation='hr.employee.category', store=False, readonly=True),
-    # }
-    employee_id = openerp.fields.Many2one(
+    '''
+    employee_id = fields.Many2one(
         'hr.employee', 'Employee', readonly=True)
-    start_date = openerp.fields.Datetime('Start date', readonly=True)
-    end_date = openerp.fields.Datetime('End date', readonly=True)
-    contract_id = openerp.fields.Many2one(
+    start_date = fields.Datetime('Start date', readonly=True)
+    end_date = fields.Datetime('End date', readonly=True)
+    contract_id = fields.Many2one(
         'hr.contract', 'Contract', readonly=True)
-    department_id = openerp.fields.Many2one(
-        reltated='employee_id.department_id', string=_('Department'),
-        store=True, readonly=True)
-    category_ids = openerp.fields.Many2many(
+    department_id = fields.Many2one(
+        'hr.department', reltated='employee_id.department_id',
+        string=_('Department'), store=True, readonly=True)
+    category_ids = fields.Many2many(
         related='employee_id.category_ids', string=_('Category'),
         store=False, readonly=True)
