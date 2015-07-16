64822b0c32f75da27f1efe38164312f0a74ae9fc
steveferry <steve.ferry1992@gmail.com>
2015-07-16 15:23:01 +0200

    Porting partner_compassion Porting minor classes account_banking_mandate_compassion and partner_bank_compassion. Correcting a field name in XML

--- partner_compassion/model/partner_bank_compassion.py
+++ partner_compassion/model/partner_bank_compassion.py
@@ -8,43 +8,36 @@
 #    The licence is in the file __openerp__.py
 #
 ##############################################################################
-from openerp.osv import orm
+
+from openerp import api, models
 
 
-class ResPartnerBank(orm.Model):
+class ResPartnerBank(models.Model):
     """ This class upgrade the partners.bank to match Compassion needs.
     """
 
     _inherit = 'res.partner.bank'
 
-    def create(self, cr, uid, data, context):
+    @api.model
+    def create(self, data):
         """Override function to notify creation in a message
         """
-        obj_partner = self.pool['res.partner']
-        part = obj_partner.browse(cr, uid, data['partner_id'], context=context)
-        part.pool.get('mail.thread').message_post(
-            cr, uid, part.id, "<b>Account number: </b>" + data['acc_number'],
-            "New account created", 'comment', context={'thread_model':
-                                                       part._name})
+        result = super(ResPartnerBank, self).create(data)
+        
+        part = result.partner_id
+        part.message_post( "<b>Account number: </b>" + result.acc_number,
+            "New account created", 'comment')
 
-        result = super(ResPartnerBank, self).create(cr, uid, data,
-                                                    context=context)
-        self.post_write(cr, uid, [result], context=context)
         return result
 
-    def unlink(self, cr, uid, ids, context):
+    @api.multi
+    def unlink(self):
         """Override function to notify delte in a message
         """
+        for account in self:
+            part = account.partner_id
+            part.message_post( "<b>Account number: </b>" + account.acc_number,
+                "Account deleted", 'comment')
 
-        obj_bank = self.pool['res.partner.bank']
-
-        for account in obj_bank.browse(cr, uid, ids, context=context):
-            account.partner_id.pool.get('mail.thread'). \
-                message_post(
-                    cr, uid, account.partner_id.id, "<b>Account number: </b>" +
-                    account.acc_number, "Account deleted", 'comment',
-                    context={'thread_model': account.partner_id._name})
-
-        result = super(ResPartnerBank, self).unlink(cr, uid, ids,
-                                                    context=context)
+        result = super(ResPartnerBank, self).unlink()
         return result
