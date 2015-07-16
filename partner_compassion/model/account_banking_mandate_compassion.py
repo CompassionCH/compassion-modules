aae174a8a87f763ae6ee27b03a1ce563be3e6abe
steveferry <steve.ferry1992@gmail.com>
2015-07-16 15:23:01 +0200

    Porting partner_compassion Porting minor classes account_banking_mandate_compassion and partner_bank_compassion. Correcting a field name in XML

--- partner_compassion/model/account_banking_mandate_compassion.py
+++ partner_compassion/model/account_banking_mandate_compassion.py
@@ -8,87 +8,87 @@
 #    The licence is in the file __openerp__.py
 #
 ##############################################################################
-from openerp.osv import orm
+
+from openerp import api, models
 
 
-class Account_Banking_Mandate(orm.Model):
+class Account_Banking_Mandate(models.Model):
     """ This class upgrade the partners.bank to match Compassion needs.
     """
     _inherit = 'account.banking.mandate'
 
-    def create(self, cr, uid, data, context):
+    MANDATE_STATE = {'create': 'created',
+                     'cancel': 'cancelled',
+                     'validate': 'validated',
+                     'back2draft': 'back to draft',
+                     'delete': 'deleted'}
+
+    def _update_mandate_status_partner(self, action):
+        """
+        Post a message on the partner's message feed with the new state
+        of the mandate
+        """
+        self.ensure_one()
+
+        if action in MANDATE_STATE:
+            self.partner.message_post(
+                    "For account: " + self.partner_bank_id.acc_number,
+                    "Mandate " + MANDATE_STATE[action], 'comment')
+
+    @api.model
+    def create(self, data):
         """Override function to notify creation in a message on partner feed
         """
-        obj_bank = self.pool['res.partner.bank']
-        account = obj_bank.browse(cr, uid, data['partner_bank_id'],
-                                  context=context)
-
-        account.partner_id.pool.get('mail.thread').message_post(
-            cr, uid, account.partner_id.id, "For account: " +
-            account.acc_number, "New mandate created", 'comment',
-            context={'thread_model': account.partner_id._name})
-
-        result = super(Account_Banking_Mandate, self).create(cr, uid, data,
-                                                             context=context)
+        result = super(Account_Banking_Mandate, self).create()
+        result._update_mandate_status_partner('create')
+        
         return result
 
-    def validate(self, cr, uid, ids, context=None):
-        """Override function to notify validation in a message on partner feed
+    @api.multi
+    def validate(self):
         """
-        mail_thread_obj = self.pool.get('mail.thread')
+        Override function to notify validation in a message on partner feed
+        """
 
-        for mandate in self.browse(cr, uid, ids, context=context):
-            mail_thread_obj.message_post(
-                cr, uid, mandate.partner_id.id, "For account: " +
-                mandate.partner_bank_id.acc_number, "Mandate validated",
-                'comment', context={'thread_model': mandate.partner_id._name})
+        for mandate in self:
+            mandate._update_mandate_status_partner('validate')
 
-        super(Account_Banking_Mandate, self).validate(cr, uid, ids, context)
+        super(Account_Banking_Mandate, self).validate()
+
         return True
 
-    def cancel(self, cr, uid, ids, context=None):
+    @api.multi
+    def cancel(self):
         """
         Override function to notify cancellation in a message on partner feed
         """
 
-        mail_thread_obj = self.pool.get('mail.thread')
+        for mandate in self:
+            mandate._update_mandate_status_partner('cancel')
 
-        for mandate in self.browse(cr, uid, ids, context=context):
-            mail_thread_obj.message_post(
-                cr, uid, mandate.partner_id.id, "For account: " +
-                mandate.partner_bank_id.acc_number, "Mandate cancelled",
-                'comment', context={'thread_model': mandate.partner_id._name})
-
-        super(Account_Banking_Mandate, self).cancel(cr, uid, ids, context)
+        super(Account_Banking_Mandate, self).cancel()
         return True
 
+    @api.multi
     def back2draft(self, cr, uid, ids, context=None):
         """
         Override function to notify cancellation in a message on partner feed
         """
 
-        mail_thread_obj = self.pool.get('mail.thread')
+        for mandate in self:
+            mandate._update_mandate_status_partner('back2draft')
 
-        for mandate in self.browse(cr, uid, ids, context=context):
-            mail_thread_obj.message_post(
-                cr, uid, mandate.partner_id.id, "For account: " +
-                mandate.partner_bank_id.acc_number, "Mandate back to draft",
-                'comment', context={'thread_model': mandate.partner_id._name})
-
-        super(Account_Banking_Mandate, self).back2draft(cr, uid, ids, context)
+        super(Account_Banking_Mandate, self).back2draft()
         return True
 
+    @api.multi
     def unlink(self, cr, uid, ids, context):
-        """Override function to notify removal in a message on partner feed
         """
-        mail_thread_obj = self.pool.get('mail.thread')
+        Override function to notify removal in a message on partner feed
+        """
 
-        for mandate in self.browse(cr, uid, ids, context=context):
-            mail_thread_obj.message_post(
-                cr, uid, mandate.partner_id.id, "For account: " +
-                mandate.partner_bank_id.acc_number, "Mandate deleted",
-                'comment', context={'thread_model': mandate.partner_id._name})
+        for mandate in self:
+            mandate._update_mandate_status_partner('delete')
 
-        result = super(Account_Banking_Mandate, self).unlink(cr, uid, ids,
-                                                             context=context)
+        result = super(Account_Banking_Mandate, self).unlink()
         return result
