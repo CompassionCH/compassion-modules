e312ef7125266094c319473d287db360788b38b4
Albert Shenouda <albert.shenouda@efrei.net>
2015-08-20 15:18:35 +0200

    Corrections

--- contract_compassion/model/product.py
+++ contract_compassion/model/product.py
@@ -15,7 +15,6 @@ from openerp import models, fields
 class product(models.Model):
     _inherit = 'product.product'
 
-    gp_fund_id = fields.Integer('GP Fund id', size=4)
     categ_name = fields.Char(
         'Product category', related='product_tmpl_id.categ_id.name',
         store=True)
