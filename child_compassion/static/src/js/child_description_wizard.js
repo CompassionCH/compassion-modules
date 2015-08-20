63c21c2462b699a3dc143f545c359a2cde8dafcd
ndtran <nicolas.tran@epfl.ch>
2015-08-20 15:55:57 +0200

    [fix] auto description and date of picture

--- child_compassion/static/src/js/child_description_wizard.js
+++ child_compassion/static/src/js/child_description_wizard.js
@@ -1,4 +1,40 @@
-openerp.child_compassion = function (instance) {
+openerp.child_compassion = function (instance, local) {
+    
+    /* 
+     * new autoDescription
+     * how: add widget="autoDescription" to the char field  
+     */
+    local.autoDescription = instance.web.form.FieldChar.extend({
+        init: function(field_manager, node) {
+            this._super.apply(this, arguments);
+        },
+        start: function() {
+            this._super();
+            this.on("change:value", this, this.values_changed)
+        },
+        update_value: function(value_lang, value, value_en) {
+            lang = value_lang.replace('value_', '');
+            var value_en_id = '[id="'+value_en+'"]';
+            if (value.newValue) {
+                $('.oe_desc_'+lang+' iframe').contents().find(value_en_id).text(value.newValue);
+                $('.oe_desc_'+lang+' iframe').contents().find(value_en_id).css('color', 'blue');
+            }
+            else {
+                $('.oe_desc_'+lang+' iframe').contents().find(value_en_id).text(value_en);
+                $('.oe_desc_'+lang+' iframe').contents().find(value_en_id).css('color', 'red');
+            }
+        },
+        values_changed: function(field, value) {
+            var value_en = this.$().parent().find('[data-fieldname="value_en"]').children().text();
+            this.update_value(field.name, value, value_en);
+        },
+    });
+    instance.web.form.widgets.add('autoDescription', 'instance.child_compassion.autoDescription');
+    
+    
+    /*
+     * buggy: the FormView cannot detect the change of the sub field of Many2many
+     */
     instance.web.child_compassion = instance.web.child_compassion || {};
     
     instance.web.views.add('auto_description_form', 'instance.web.child_compassion.AutoDescriptionFormView');
