eda0f46cb78f2d2ceac712ec6924ba201871dd22
ndtran <nicolas.tran@epfl.ch>
2015-08-24 15:10:07 +0200

    add readme

---
+++ partner_compassion/README.rst
@@ -0,0 +1,27 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :alt: License: AGPL-3
+
+Partner Compassion
+==================
+
+Upgrade Partners to Compassion standards :
+    1. Three lines for the address
+    2. Mandatory reference sequence (CODEGA)
+    3. Church members relation between Partners
+    4. Birthdate and Deathdate
+    5. Adds new contact titles
+    6. Adds Partner Categories
+    7. Notify on Partner Mandate changes
+
+Credits
+=======
+
+Contributors
+------------
+
+* Emanuel Cino <ecino@compassion.ch>
+
+Maintainer
+----------
+
+This module is maintained by `Compassion Switzerland <https://www.compassion.ch>`.
--- sponsorship_compassion/__openerp__.py
+++ sponsorship_compassion/__openerp__.py
@@ -32,56 +32,6 @@
     'name': 'Compassion Sponsorships',
     'version': '1.6',
     'category': 'Other',
-    'description': """
-Compassion Sponsorships
-=======================
-Sponsorship management module. This module makes the link between child and
-contracts. It also customize contracts to fit the child sponsorship context.
-
-Installation
-============
-This modules requires en_US, fr_CH, de_DE, it_IT and es_ES to be installed
-on the server.
-
-To check installed locales:
-
-* locale -a
-
-To add a new locale :
-
-* /usr/share/locales/install-language-pack <ISO-locale-name>
-* dpkg-reconfigure locales
-
-Configuration
-=============
-You can configure how to behave when projects are suspended. If you setup
-nothing, sponsorship invoices for a period during a supsension of a projects
-will be cancelled.
-
-If you want to change sponsorship with a a fund donation, you can add
-the following key-value in the System Parameters:
-    - sponsorship_compassion.suspend_product_id : product_id
-
-Usage
-=====
-To use this module, you need to:
-
-* go to Sponsorship -> Sponsorships
-
-Known issues / Roadmap
-======================
-
-* Contracts and Sponsorships will be better differenciated.
-
-Credits
-=======
-
-Contributors
-------------
-
-* Cyril Sester <cyril.sester@outlook.com>
-* Emanuel Cino <ecino@compassion.ch>
-    """,
     'author': 'Compassion CH',
     'website': 'http://www.compassion.ch',
     'depends': ['contract_compassion', 'crm',
