8fb7655b1ab800a91682d888fdd2d11bb1705407
loikki <loic_hausammann@hotmail.com>
2015-10-01 15:20:46 +0200

    Run travis

--- sbc_compassion/models/import_mail.py
+++ sbc_compassion/models/import_mail.py
@@ -1,3 +1,8 @@
+"""
+This file reads a zip file containing scans of mail and find the relation
+between the database and the mail.
+"""
+
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
@@ -8,34 +13,30 @@
 #    The licence is in the file __openerp__.py
 #
 ##############################################################################
-"""
-This file reads a zip file containing scans of mail and find the relation 
-between the database and the mail.
-"""
 import base64
 import zipfile
 import time
 import subprocess
 import os
-import copy
 import shutil
 import pdb
 
 from openerp import api, fields, models, _, exceptions
-import sponsorship_correspondence
+# import sponsorship_correspondence
 
 import zxing
+
 
 def check_file(name):
     """
     Check the name of a file.
     return 1 if it is a tiff or a pdf and 0 otherwise.
-    
+
     This function can be upgraded in order to include other
     format (1 for file, 2 for archive, 0 for not supported).
     In order to have a nice code, one should add the function
     is... when adding a new format
-    
+
     :param str name: Name of the file to check
     :return: 1 if pdf or tiff, 2 if zip, 0 otherwise
     :rtype: int
@@ -62,6 +63,7 @@ def isPDF(name):
     else:
         return False
 
+
 def isTIFF(name):
     """
     Check the extension of the name
@@ -75,7 +77,7 @@ def isTIFF(name):
         return True
     else:
         return False
-    
+
 
 def isZIP(name):
     """
@@ -105,6 +107,7 @@ def list2string(list_):
         tmp += i + ';'
     return tmp
 
+
 def addname(string, name):
     """
     Add name to string by separating them with ;
@@ -115,7 +118,8 @@ def addname(string, name):
     """
     return string + name + ';'
 
-def removename(string,name):
+
+def removename(string, name):
     """
     Find and remove name in string where string is a string separated with ;
 
@@ -124,10 +128,10 @@ def removename(string,name):
     :returns: string without name or False
     :rtype: string or bool
     """
-    print ';' + name +';'
-    tmp = string.replace(';'+name+';',';')
+    print ';' + name + ';'
+    tmp = string.replace(';' + name + ';', ';')
     if tmp == string:
-        tmp = string.replace(name+';',';')
+        tmp = string.replace(name + ';', ';')
         if tmp == string:
             return -1
         else:
@@ -137,11 +141,12 @@ def removename(string,name):
 
 
 class ImportMail(models.TransientModel):
+
     """
     Model for Import mail
-    This class allows the user to import some letters (individually or in a zip)
-    in the database by doing an automatic analysis.
-    The code is reading some barcodes (QR code) in order to do the analysis 
+    This class allows the user to import some letters (individually or in a
+    zip) in the database by doing an automatic analysis.
+    The code is reading some barcodes (QR code) in order to do the analysis
     (with the help of the library zxing)
     """
     _name = "import.mail"
@@ -150,24 +155,23 @@ class ImportMail(models.TransientModel):
     ##########################################################################
     #                                 FIELDS                                 #
     ##########################################################################
-    
-    nber_file = fields.Integer(_('Number of files: '),readonly=True,
+
+    nber_file = fields.Integer(_('Number of files: '), readonly=True,
                                compute="_count_nber_files")
-    title = fields.Text(_("Import Mail"),readonly=True)
-    debug = fields.Text("DEBUG",readonly=True)
+    title = fields.Text(_("Import Mail"), readonly=True)
+    debug = fields.Text("DEBUG", readonly=True)
     # path where the zipfile are store
     path = "/tmp/sbc_compassion/"
     # list zip in the many2many
     # use ';' in order to separate the files
-    list_zip = fields.Text("LIST_ZIP",readonly=True)
+    list_zip = fields.Text("LIST_ZIP", readonly=True)
 
     # link to _count_nber_files
-    data = fields.Many2many('ir.attachment') 
+    data = fields.Many2many('ir.attachment')
     import_mail_line_ids = fields.Many2many('import.mail.line')
-    
-    
-    #-------------------- _COUNT_NBER_FILES ------------------------------------
-    
+
+    # -------------------- _COUNT_NBER_FILES ---------------------------------
+
     @api.one
     @api.onchange("data")
     def _count_nber_files(self):
@@ -175,18 +179,18 @@ class ImportMail(models.TransientModel):
         Counts the number of scans (if a zip is given, count the number
         inside it)
         """
-        if self.list_zip == False:
+        if self.list_zip is False:
             self.list_zip = ""
-        if self.debug == False:
+        if self.debug is False:
             self.debug = "YOUPI"
-        #removes old files in the directory
+        # removes old files in the directory
         if os.path.exists(self.path):
-            onlyfiles = [ f for f in os.listdir(self.path)
-                          if os.path.isfile(os.path.join(self.path,f)) ]
+            onlyfiles = [f for f in os.listdir(self.path)
+                         if os.path.isfile(os.path.join(self.path, f))]
             # loop over files
             for f in onlyfiles:
                 t = time.time()
-                t -= os.path.getctime(self.path+f)
+                t -= os.path.getctime(self.path + f)
                 # if file older than 12h
                 if t > 43200:
                     os.remove(f)
@@ -202,25 +206,25 @@ class ImportMail(models.TransientModel):
                 tmp += 1
             # zip case
             elif check_file(attachment.name) == 2:
-                tmp_name_file = self.path+attachment.name
+                tmp_name_file = self.path + attachment.name
                 # save the zip file
                 if not os.path.exists(tmp_name_file):
-                    f = open(tmp_name_file,'w')
+                    f = open(tmp_name_file, 'w')
                     f.write(base64.b64decode(attachment.with_context(
                         bin_size=False).datas))
                     f.close()
                     if attachment.name not in self.list_zip:
-                        self.list_zip = addname(self.list_zip,attachment.name)
+                        self.list_zip = addname(self.list_zip, attachment.name)
                 # catch ALL the exceptions that can be raised by class
                 # zipfile
                 try:
-                    zip_ = zipfile.ZipFile(tmp_name_file,'r')
+                    zip_ = zipfile.ZipFile(tmp_name_file, 'r')
                 except zipfile.BadZipfile:
-                    raise exceptions.Warning(_('Zip file corrupted (' + 
-                                    attachment.name + ')'))
+                    raise exceptions.Warning(_('Zip file corrupted (' +
+                                               attachment.name + ')'))
                 except zipfile.LargeZipFile:
-                    raise exceptions.Warning(_('Zip64 is not supported(' + 
-                                    attachment.name + ')'))
+                    raise exceptions.Warning(_('Zip64 is not supported(' +
+                                               attachment.name + ')'))
                 else:
                     list_file = zip_.namelist()
                     # loop over all files in zip
@@ -230,25 +234,24 @@ class ImportMail(models.TransientModel):
         # deletes zip removed from the data
         for f in self.list_zip.split(';'):
             if f not in self.mapped('data.name') and f != '':
-                if os.path.exists(self.path+f):
-                    tmp = removename(self.list_zip,f)
+                if os.path.exists(self.path + f):
+                    tmp = removename(self.list_zip, f)
                     if tmp == -1:
                         raise exceptions.Warning(
                             _("Does not find the file during suppression"))
                     else:
                         self.list_zip = tmp
-                    os.remove(self.path+f)
-        
-        
-    #------------------------ _RUN_ANALYZE -------------------------------------
-        
+                    os.remove(self.path + f)
+
+    # ------------------------ _RUN_ANALYZE ----------------------------------
+
     @api.one
     def button_run_analyze(self):
         """
         Analyze each attachment (decompress zip too) by checking if the file
-        is not done twice (check same name)[, extract zip], use 
+        is not done twice (check same name)[, extract zip], use
         analyze_attachment at the end
-        
+
         """
         # list for checking if a file come twice
         check = []
@@ -257,19 +260,19 @@ class ImportMail(models.TransientModel):
                 check.append(attachment.name)
                 # check for zip
                 if check_file(attachment.name) == 2:
-                    zip_ = zipfile.ZipFile(self.path+attachment.name,'r')
+                    zip_ = zipfile.ZipFile(self.path + attachment.name, 'r')
                     path_zip = self.path + os.path.splitext(
                         str(attachment.name))[0]
                     if not os.path.exists(path_zip):
                         os.makedirs(path_zip)
                     for f in zip_.namelist():
-                        zip_.extract(f,path_zip)
+                        zip_.extract(f, path_zip)
                         absname = path_zip + '/' + f
                         if os.path.isfile(absname):
                             self.analyze_attachment(absname)
                     # delete all the tmp files
                     shutil.rmtree(path_zip)
-                    os.remove(self.path+attachment.name)
+                    os.remove(self.path + attachment.name)
                 # case with normal format (PDF,TIFF)
                 elif check_file(attachment.name) == 1:
                     self.analyze_attachment(
@@ -283,25 +286,25 @@ class ImportMail(models.TransientModel):
             else:
                 raise exceptions.Warning(_('Two files are the same'))
                 return
-        
-    def analyze_attachment(self,file_,data=None):
+
+    def analyze_attachment(self, file_, data=None):
         """
         Analyze attachment (PDF/TIFF) and save everything
-        
+
         :param string file_: Name of the file to analyze
         :param binary data: Image to scan (by default, read it from hdd)
         :returns: Barcode (error code: 1 was not able to read it, 2 format\
                   not accepted)
         :rtype: String (error code: int)
         """
-        if data != None:
-            f = open(file_,'w')
+        if data is not None:
+            f = open(file_, 'w')
             f.write(base64.b64decode(data))
             f.close()
         if isPDF(file_):
-            #convert
+            # convert
             name = os.path.splitext(file_)[0]
-            cmd = ['convert',file_,file_ + '.tif']
+            cmd = ['convert', file_, file_ + '.tif']
             (stdout, stderr) = subprocess.Popen(
                 cmd, stdout=subprocess.PIPE,
                 universal_newlines=True).communicate()
@@ -314,28 +317,33 @@ class ImportMail(models.TransientModel):
             except:
                 raise exceptions.Warning(
                     _("Unable to read file: {}").format(file_))
-            if code == None:
+            if code is None:
                 raise exceptions.Warning(
                     _("Not able to read barcode from: {}").format(file_))
             try:
-                partner,child = code.data.split('XX') 
+                partner, child = code.data.split('XX')
             except:
                 raise exceptions.Warning(
                     _("Barcode not in the required format in file: {}").format(
                         file_))
 
             # NEED REVIEW
-            # 
-            # template_id
-            # supporter_language (method needed for check if partner langage exist in res.lang.compassion / field have to be set required in import.mail.line)
-            # status (local variable with status selection needed, please refer to the status selection field)
             #
-            # Problem: the converted jpeg can be opened on a windows PC or with nano, but odoo can't recognize the file. Tried to give a jpeg
+            # template_id
+            # supporter_language (method needed for check if partner langage
+            # exist in res.lang.compassion / field have to be set required in
+            # import.mail.line)
+            # status (local variable with status selection needed, please
+            # refer to the status selection field)
+            #
+            # Problem: the converted jpeg can be opened on a windows PC or
+            # with nano, but odoo can't recognize the file. Tried
+            # to give a jpeg
             # to odoo from windows -> same problem
 
             f_tmp = os.path.splitext(file_)[0] + ".jpeg"
-           
-            cmd = ['convert',file_,f_tmp]
+
+            cmd = ['convert', file_, f_tmp]
             (stdout, stderr) = subprocess.Popen(
                 cmd, stdout=subprocess.PIPE,
                 universal_newlines=True).communicate()
@@ -350,18 +358,18 @@ class ImportMail(models.TransientModel):
             pdb.set_trace()
             file_jpeg = open(f_tmp, "r")
             document_vals = {'name': 'file_.jpeg',
-                'datas': file_jpeg.read(),
-                'datas_fname': 'file_.jpeg',
-                'res_model': 'import.mail.line',
-                'res_id': import_mail_line.id
-                }
+                             'datas': file_jpeg.read(),
+                             'datas_fname': 'file_.jpeg',
+                             'res_model': 'import.mail.line',
+                             'res_id': import_mail_line.id
+                             }
 
             image = self.env['ir.attachment'].create(document_vals)
             import_mail_line.letter_image_preview = image
             file_jpeg.close()
-            
+
             self.import_mail_line_ids += import_mail_line
-          
+
         else:
-            
+
             raise exceptions.Warning('Format not accepted in {}'.format(file_))
--- sbc_compassion/models/zxing.py
+++ sbc_compassion/models/zxing.py
@@ -12,171 +12,176 @@
 #  original version on github.com/oostendo/python-zxing
 
 __version__ = '0.4'
-import subprocess, re, os
+import subprocess
+import re
+import os
+
 
 class BarCodeTool():
-  """
-  This class needs to use the method decode/encode in order to do something
-  At the initialization, the directories names are set up and the non-changing
-  (between two utilization) part is written
-  """
-  location = ""
-  command = "java"
-  libs = ["javase.jar", "core.jar", "jcommander.jar", "jai-imageio-core.jar"]
-  args = ["-cp", "LIBS"]
-  args_de = ["com.google.zxing.client.j2se.CommandLineRunner"]
-  args_en = ["com.google.zxing.client.j2se.CommandLineEncoder"]
 
-  def __init__(self, loc=""):
-    if not len(loc):
-      if ("HOME" in os.environ):
-        loc = os.environ["HOME"] + "/.libZxing"
-      else:
-        loc = ".."
-
-    self.location = loc
-
-  def decode(self, files, try_harder = False,qr_only=True):
     """
-    Decodes a/some file/s
-    :param string files: Name of the files to decode
-    :param bool try_harder: Spend more time to find a barcode (if needed)
-    :param bool qr_only: Only check for QR code or not
-    :returns: Information about the barcode
-    :rtype: BarCode
-    """    
-    cmd = [self.command]
-    cmd += self.args[:] #copy arg values
-    cmd += self.args_de
-
-
-    # send one file, or multiple files in a list
-    SINGLE_FILE = False
-    if type(files) != type(list()):
-      cmd.append(files)
-      SINGLE_FILE = True
-    else:
-      cmd += files
-
-    if try_harder:
-      cmd.append("--try_harder")
-    if qr_only:
-      cmd.append("--possible_formats")
-      cmd.append("QR_CODE")
-
-    libraries = [self.location + "/" + l for l in self.libs]
-
-    cmd = [ c if c != "LIBS" else os.pathsep.join(libraries) for c in cmd ]
-
-    (stdout, stderr) = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                                        universal_newlines=True).communicate()
-    codes = []
-    file_results = stdout.split("\nfile:")
-    for result in file_results:
-      lines = stdout.split("\n")
-      if re.search("No barcode found", lines[0]):
-        codes.append(None)
-        continue
-
-      codes.append(BarCode(result))
-
-    if SINGLE_FILE:
-      return codes[0]
-    else:
-      return codes
-
-  def encode(self, file_, text, code_format=None, width=None, height=None,
-             errorcorrection=None):
+    This class needs to use the method decode/encode in order to do something
+    At the initialization, the directories names are set up and the
+    non-changing (between two utilization) part is written
     """
-    Create a Barcode (written in PNG format [the library can accepts JPG,GIF 
-    as a paramter to the command line])
-    :param string file_: File name where to write the barcode
-    :param string text: Text to encode
-    :param string code_format: Type of barcode (see github, zxing project)
-    :param int width: Width of the ouput image
-    :param int height: Height of the output image
-    :param int errorcorrection: Correction level (QR code between 0 and 3)
-    """
+    location = ""
+    command = "java"
+    libs = ["javase.jar", "core.jar", "jcommander.jar", "jai-imageio-core.jar"]
+    args = ["-cp", "LIBS"]
+    args_de = ["com.google.zxing.client.j2se.CommandLineRunner"]
+    args_en = ["com.google.zxing.client.j2se.CommandLineEncoder"]
 
-    cmd = [self.command]
-    cmd += self.args[:] #copy arg values
-    cmd += self.args_en
-    libraries = [self.location + "/" + l for l in self.libs]
+    def __init__(self, loc=""):
+        if not len(loc):
+            if ("HOME" in os.environ):
+                loc = os.environ["HOME"] + "/.libZxing"
+            else:
+                loc = ".."
 
-    cmd = [ c if c != "LIBS" else os.pathsep.join(libraries) for c in cmd ]
+        self.location = loc
 
-    cmd.append(text)
-    if width != None:
-      cmd.append("--width")
-      cmd.apppend(width)
-    if height != None:
-      cmd.append("--height")
-      cmd.append(height)
-    if code_format != None:
-      cmd.append("--barcode_format")
-      cmd.append(code_format)
-    if errorcorrection != None:
-      cmd.append("--error_correction_level")
-      cmd.append(errorcorrection)
+    def decode(self, files, try_harder=False, qr_only=True):
+        """
+        Decodes a/some file/s
+        :param string files: Name of the files to decode
+        :param bool try_harder: Spend more time to find a barcode (if needed)
+        :param bool qr_only: Only check for QR code or not
+        :returns: Information about the barcode
+        :rtype: BarCode
+        """
+        cmd = [self.command]
+        cmd += self.args[:]  # copy arg values
+        cmd += self.args_de
 
-    cmd.append("--output")
-    cmd.append(file_)
-    print cmd
-    (stdout, stderr) = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                                        universal_newlines=True).communicate()
+        # send one file, or multiple files in a list
+        SINGLE_FILE = False
+        if type(files) != type(list()):
+            cmd.append(files)
+            SINGLE_FILE = True
+        else:
+            cmd += files
+
+        if try_harder:
+            cmd.append("--try_harder")
+        if qr_only:
+            cmd.append("--possible_formats")
+            cmd.append("QR_CODE")
+
+        libraries = [self.location + "/" + l for l in self.libs]
+
+        cmd = [c if c != "LIBS" else os.pathsep.join(libraries) for c in cmd]
+
+        (stdout, stderr) = subprocess.Popen(
+            cmd, stdout=subprocess.PIPE, universal_newlines=True).communicate()
+        codes = []
+        file_results = stdout.split("\nfile:")
+        for result in file_results:
+            lines = stdout.split("\n")
+            if re.search("No barcode found", lines[0]):
+                codes.append(None)
+                continue
+
+            codes.append(BarCode(result))
+
+        if SINGLE_FILE:
+            return codes[0]
+        else:
+            return codes
+
+    def encode(self, file_, text, code_format=None, width=None, height=None,
+               errorcorrection=None):
+        """
+        Create a Barcode (written in PNG format [the library can accepts JPG,
+        GIF as a paramter to the command line])
+        :param string file_: File name where to write the barcode
+        :param string text: Text to encode
+        :param string code_format: Type of barcode (see github, zxing project)
+        :param int width: Width of the ouput image
+        :param int height: Height of the output image
+        :param int errorcorrection: Correction level (QR code between 0 and 3)
+        """
+
+        cmd = [self.command]
+        cmd += self.args[:]  # copy arg values
+        cmd += self.args_en
+        libraries = [self.location + "/" + l for l in self.libs]
+
+        cmd = [c if c != "LIBS" else os.pathsep.join(libraries) for c in cmd]
+
+        cmd.append(text)
+        if width is not None:
+            cmd.append("--width")
+            cmd.apppend(width)
+        if height is not None:
+            cmd.append("--height")
+            cmd.append(height)
+        if code_format is not None:
+            cmd.append("--barcode_format")
+            cmd.append(code_format)
+        if errorcorrection is not None:
+            cmd.append("--error_correction_level")
+            cmd.append(errorcorrection)
+
+        cmd.append("--output")
+        cmd.append(file_)
+        print cmd
+        (stdout, stderr) = subprocess.Popen(
+            cmd, stdout=subprocess.PIPE, universal_newlines=True).communicate()
+
 
 class BarCode:
-  """
-  Class containing all the information about the codebars
-  """
-  format = ""
-  points = []
-  data = ""
-  raw = ""
 
-  def __init__(self, zxing_output):
-    lines = zxing_output.split("\n")
-    raw_block = False
-    parsed_block = False
-    point_block = False
+    """
+    Class containing all the information about the codebars
+    """
+    format = ""
+    points = []
+    data = ""
+    raw = ""
 
-    self.points = []
-    for l in lines:
-      m = re.search("format:\s([^,]+)", l)
-      if not raw_block and not parsed_block and not point_block and m:
-        self.format = m.group(1)
-        continue
-
-      if (not raw_block and not parsed_block and not point_block and 
-          l == "Raw result:"):
-        raw_block = True
-        continue
-
-      if raw_block and l != "Parsed result:":
-        self.raw += l + "\n"
-        continue
-
-      if raw_block and l == "Parsed result:":
+    def __init__(self, zxing_output):
+        lines = zxing_output.split("\n")
         raw_block = False
-        parsed_block = True
-        continue
-
-      if parsed_block and not re.match("Found\s\d\sresult\spoints", l):
-        self.data += l + "\n"
-        continue
-
-      if parsed_block and re.match("Found\s\d\sresult\spoints", l):
         parsed_block = False
-        point_block = True
-        continue
+        point_block = False
 
-      if point_block:
-        m = re.search("Point\s(\d+):\s\(([\d\.]+),([\d\.]+)\)", l)
-        if (m):
-          self.points.append((float(m.group(2)), float(m.group(3))))
+        self.points = []
+        for l in lines:
+            m = re.search("format:\s([^,]+)", l)
+            if not raw_block and not parsed_block and not point_block and m:
+                self.format = m.group(1)
+                continue
 
-    return
+            if (not raw_block and not parsed_block and not point_block and
+                    l == "Raw result:"):
+                raw_block = True
+                continue
+
+            if raw_block and l != "Parsed result:":
+                self.raw += l + "\n"
+                continue
+
+            if raw_block and l == "Parsed result:":
+                raw_block = False
+                parsed_block = True
+                continue
+
+            if parsed_block and not re.match("Found\s\d\sresult\spoints", l):
+                self.data += l + "\n"
+                continue
+
+            if parsed_block and re.match("Found\s\d\sresult\spoints", l):
+                parsed_block = False
+                point_block = True
+                continue
+
+            if point_block:
+                m = re.search("Point\s(\d+):\s\(([\d\.]+),([\d\.]+)\)", l)
+                if (m):
+                    self.points.append((float(m.group(2)), float(m.group(3))))
+
+        return
 
 
 if __name__ == "__main__":
-  print("ZXing module")
+    print("ZXing module")
