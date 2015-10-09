d92b804c83d3c8f0a9644a2ad05b3580e8baf81c
loikki <loic_hausammann@hotmail.com>
2015-10-09 16:13:24 +0200

    decrease data to download and correct path

--- sbc_compassion/tools/makelink.sh
+++ sbc_compassion/tools/makelink.sh
@@ -1,12 +1,7 @@
-for f in *.so.3.0
+for f in /usr/local/lib/*.so.3.0.0
 do 
-	rm "$f"
-	ln -s `echo $f | sed 's/\(.*\.\)so.3.0/\1so.3.0.0/'` $f
+	ln -s $f `echo $f | sed 's/\(.*\.\)so.3.0.0/\1so.3.0/'`
+	ln -s `echo $f | sed 's/\(.*\.\)so.3.0.0/\1so.3.0/'` `echo $f | sed 's/\(.*\.\)so.3.0.0/\1so/'`
 done
 
-for f in *.so
-do 
-	rm "$f"
-	ln -s `echo $f | sed 's/\(.*\.\)so/\1so.3.0/'`  $f
-done
 
