2713fe98b28bb5d830da7ac1c9b97fb4bbba48bb
loikki <loic_hausammann@hotmail.com>
2015-10-12 09:03:33 +0200

    delete symbolic link before trying to make them

--- sbc_compassion/tools/makelink.sh
+++ sbc_compassion/tools/makelink.sh
@@ -1,7 +1,18 @@
+#! /bin/bash
 for f in /usr/local/lib/*.so.3.0.0
 do 
-	ln -s $f `echo $f | sed 's/\(.*\.\)so.3.0.0/\1so.3.0/'`
-	ln -s `echo $f | sed 's/\(.*\.\)so.3.0.0/\1so.3.0/'` `echo $f | sed 's/\(.*\.\)so.3.0.0/\1so/'`
+    L1=`echo $f | sed 's/\(.*\.\)so.3.0.0/\1so.3.0/'`
+    if [ -h "$L1" ]; then
+	rm $L1
+    fi
+
+    L2=`echo $f | sed 's/\(.*\.\)so.3.0.0/\1so/'`
+    if [ -h "$L2" ]; then
+	rm $L2
+    fi
+   
+    ln -s $f $L1
+    ln -s $L1 $L2
 done
 
 
