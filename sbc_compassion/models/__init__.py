021cde6ba0dab6a07af8356293e5ff84c29c3edc
loikki <loic_hausammann@hotmail.com>
2015-10-22 14:59:16 +0200

    change name

--- sbc_compassion/models/__init__.py
+++ sbc_compassion/models/__init__.py
@@ -12,5 +12,5 @@ from . import partner_compassion
 from . import lang_compassion
 from . import country_compassion
 from . import sponsorship_correspondence
-from . import import_mail
-from . import import_mail_line
+from . import import_letters_history
+from . import import_letters_line
