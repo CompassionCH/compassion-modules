a7b4807e8b3e6e47bd3d2e4282c2f228ce7f25a6
Roman Zoller <rz-comp@users.noreply.github.com>
2015-11-23 16:50:11 +0100

    add a test for QR code reader; add TODOs

--- sbc_compassion/tests/test_import_letter.py
+++ sbc_compassion/tests/test_import_letter.py
@@ -11,10 +11,11 @@
 
 import unittest
 
+import tools
+
 import logging
 import cv2
 import os
-from tools import bluecornerfinder as bcf
 
 logger = logging.getLogger(__name__)
 THIS_DIR = os.path.dirname(__file__)
@@ -29,10 +30,35 @@ class TestImportLetter(unittest.TestCase):
         self.test_document_normal = os.path.join(THIS_DIR,
                                                  'testdata/normal.png')
 
-    def test_blue_corner_finder(self):
-        """TODO doc"""
-        img = cv2.imread(self.test_document_normal)
-        self.assertIsNotNone(img)
-        bluecorner = bcf.BlueCornerFinder(img)
+    def test_blue_corner_finder_should_find(self):
+        """Blue corner should be found at known coordinates."""
+        img = self._read_img(self.test_document_normal)
+        bluecorner = tools.bluecornerfinder.BlueCornerFinder(img)
         bluecorner_position = bluecorner.getIndices()
         self.assertEqual(bluecorner_position, [2438, 76])
+        # TODO add tests with more documents
+
+    def test_blue_corner_finder_should_not_find(self):
+        """Blue corner should not be found."""
+
+    def test_pattern_recognition(self):
+        """TODO doc"""
+        # TODO: need image files or base64 representation of patterns to test
+
+    def test_zxing_read_qr_code(self):
+        """QR code should be read correctly."""
+        bar_code_tool = tools.zxing.BarCodeTool()
+        path = os.path.abspath(self.test_document_normal)
+        qr_code = bar_code_tool.decode(path,
+                                       try_harder=True)
+        self.assertEqual(qr_code.data, "1509540XXGU4920269\n")
+        # TODO add tests with more documents
+
+    def test_zxing_no_qr_code(self):
+        """Should report error for document that does not contain QR code."""
+        # TODO implement
+
+    def _read_img(self, path):
+        img = cv2.imread(path)
+        self.assertIsNotNone(img)
+        return img
