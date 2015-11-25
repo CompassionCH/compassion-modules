de279836eb430dab3bba10ac78ce803f8cea9589
Roman Zoller <rz-comp@users.noreply.github.com>
2015-11-25 14:17:01 +0100

    skip javadoc

--- travis/install_zxing.sh
+++ travis/install_zxing.sh
@@ -22,14 +22,14 @@ git checkout 9910fcfaa9be4e3c81efe7febdb121d59baa07e9
 cd ${ZXING_ROOT}
 # Disable rat plugin to avoid incorrect license header errors; save time by
 # not building and running tests.
-mvn install -Drat.skip=true -Dmaven.test.skip=true
+mvn install -Drat.skip=true -Dmaven.test.skip=true -Dmaven.javadoc.skip=true
 cp zxingorg/target/zxingorg-${ZXING_VERSION}/WEB-INF/lib/jcommander-*.jar jcommander.jar
 cp zxingorg/target/zxingorg-${ZXING_VERSION}/WEB-INF/lib/jai-imageio-core-*.jar jai-imageio-core.jar
 
 cd ${ZXING_ROOT}/core
-mvn install -Drat.skip=true -Dmaven.test.skip=true
+mvn install -Drat.skip=true -Dmaven.test.skip=true -Dmaven.javadoc.skip=true
 cp target/core-${ZXING_VERSION}.jar ../core.jar
 
 cd ${ZXING_ROOT}/javase
-mvn install -Drat.skip=true -Dmaven.test.skip=true
+mvn install -Drat.skip=true -Dmaven.test.skip=true -Dmaven.javadoc.skip=true
 cp target/javase-${ZXING_VERSION}.jar ../javase.jar
