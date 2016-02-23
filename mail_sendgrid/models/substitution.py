e32825713fee1c6e27537b7f4bea557848e5c659
Emanuel Cino <ecino@compassion.ch>
2016-02-23 16:08:11 +0100

    Remove useless metod in sendgrid substitution

--- mail_sendgrid/models/substitution.py
+++ mail_sendgrid/models/substitution.py
@@ -9,13 +9,7 @@
 #
 ##############################################################################
 
-import logging
-
 from openerp import models, fields
-from openerp.tools import ustr
-from openerp.addons.email_template.email_template import mako_template_env
-
-_logger = logging.getLogger(__name__)
 
 
 class Substitution(models.Model):
@@ -32,33 +26,3 @@ class Substitution(models.Model):
     email_id = fields.Many2one(
         'mail.mail', ondelete='cascade')
     value = fields.Char()
-
-    def get_substitutions(self, object):
-        """Render the values and returns a dictionary with the keys and values
-           Replace mako expressions ``${expr}``
-           with the result of evaluating these expressions with
-           an evaluation context containing:
-
-                * ``user``: browse_record of the current user
-                * ``object``: browse_record of the document record this mail is
-                              related to
-
-           :param object: browse record this mail is related to.
-        """
-        result = dict()
-        variables = {
-            'user': self.user,
-            'ctx': self.context,  # context kw would clash with mako internals
-            'object': object
-        }
-        for substitution in self:
-            result[substitution.key] = substitution.value
-            try:
-                template = mako_template_env.from_string(ustr(
-                    substitution.value))
-                result[substitution.key] = template.render(variables)
-            except Exception:
-                _logger.exception("Failed to render substitution %r",
-                                  substitution.value)
-
-        return result
