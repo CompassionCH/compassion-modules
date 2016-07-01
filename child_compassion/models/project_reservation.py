956c584b86c7ec056ae2b194fa69ac64dc4b425f
Maxime Beck <mbcompte@gmail.com>
2016-07-01 12:25:49 +0200

    CO-207 Add support for Create and Delete operations on child reservations

--- child_compassion/models/project_reservation.py
+++ child_compassion/models/project_reservation.py
@@ -9,7 +9,7 @@
 #
 ##############################################################################
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class ProjectReservation(models.Model):
@@ -17,6 +17,8 @@ class ProjectReservation(models.Model):
     _name = 'icp.reservation'
     _description = 'Project Reservation'
 
+    name = fields.Char(required=True)
+    beneficiary_global_id = fields.Char()
     reservation_id = fields.Char()
     channel_name = fields.Char()
     icp_id = fields.Many2one(
@@ -26,7 +28,34 @@ class ProjectReservation(models.Model):
     expiration_date = fields.Date(required=True)
     hold_expiration_date = fields.Datetime(required=True)
     hold_yield_rate = fields.Integer()
-    is_reservation_auto_approved = fields.Boolean(required=True)
+    is_reservation_auto_approved = fields.Boolean(default=True)
     number_of_beneficiaries = fields.Integer(required=True)
     primary_owner = fields.Char(required=True)
     secondary_owner = fields.Char()
+    active = fields.Boolean(default=True, readonly=True)
+
+    @api.model
+    def create(self, vals):
+        res = super(ProjectReservation, self).create(vals)
+        message_obj = self.env['gmc.message.pool']
+        action_id = self.env.ref('child_compassion.create_reservation').id
+
+        message_vals = {
+            'action_id': action_id,
+            'object_id': res.id,
+        }
+        message_obj.with_context(async_mode=False).create(message_vals)
+        return res
+
+    @api.multi
+    def unlink(self):
+        message_obj = self.env['gmc.message.pool']
+        action_id = self.env.ref('child_compassion.cancel_reservation').id
+
+        self.active = False
+        message_vals = {
+            'action_id': action_id,
+            'object_id': self.id
+        }
+        message_obj.with_context(async_mode=False).create(message_vals)
+        return
