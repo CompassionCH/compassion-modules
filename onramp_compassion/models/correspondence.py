eeecfefd5cef9c123eca9a8bb221b12549bf56d2
Emanuel Cino <ecino@compassion.ch>
2016-07-13 16:32:28 +0200

    CO-255 Don't send e-mail to sponsor when wrong composition
    - Fix search view of children

--- onramp_compassion/models/correspondence.py
+++ onramp_compassion/models/correspondence.py
@@ -108,10 +108,12 @@ class SponsorshipCorrespondence(models.Model):
     def process_letter(self):
         """ Method called when new B2S letter is Published. """
         self.download_attach_letter_image(type='original_letter_url')
+        res = True
         for letter in self:
             if letter.original_language_id not in \
                     letter.correspondant_id.spoken_lang_ids:
-                letter.compose_letter_image()
+                res = res and letter.compose_letter_image()
+        return res
 
     @api.multi
     def download_attach_letter_image(self, type='final_letter_url'):
