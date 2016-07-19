3e271db02f4442097616fab59ee07f889b730ef2
philippe89 <heerphilippe@msn.com>
2016-07-19 13:09:28 +0200

    CO-263 Implementation of IPC Lifecycle kit

---
+++ child_compassion/mappings/project_lifecycle_mapping.py
@@ -0,0 +1,63 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2016 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Emanuel Cino <ecino@compassion.ch>
+#             Philippe Heer <heerphilippe@msn.com>
+#
+#    The licence is in the file __openerp__.py
+#
+##############################################################################
+from openerp.addons.message_center_compassion.mappings.base_mapping import \
+    OnrampMapping
+
+
+class ICPMapping(OnrampMapping):
+    ODOO_MODEL = 'compassion.project.ile'
+    MAPPING_NAME = "new_project_lifecyle"
+
+    CONNECT_MAPPING = {
+        "ActionPlan": "action_plan",
+        "ActualSuspensionEndDate": "suspension_end_date",
+        "ActualTransitionDate": "date",
+        "FutureInvolvement": "future_involvement",
+        "ICPImprovementDescription": "icp_improvement_desc",
+        "ICPLifecycleEventType": "type",
+        "IsBeneficiaryInformationUpdatesWithheld":
+            "is_beneficiary_information_updates_withheld",
+        "IsBeneficiaryToSupporterLettersWithheld": "hold_b2s_letters",
+        "IsGiftsWithheld": "hold_gifts",
+        "IsSponsorshipFundsWithheld": "hold_cdsp_funds",
+        "IsSuporterToBeneficiaryLettersWithheld": "hold_s2b_letters",
+        "IsSurvivalFundsWithheld": "hold_csp_funds",
+        "IsSuspensionExtendedAdditionalThirtyDays": "extension_2",
+        "IsSuspensionExtendedThirtyDays": "extension_1",
+        "IsTransitionComplete": "transition_complete",
+        "Name": "name",
+        "ProjectedTransitionDate": "projected_transition_date",
+        "ReactivationDate": "reactivation_date",
+        "ReasonForFirstExtension": "extension_1_reason",
+        "ReasonforSecondExtension": "extension_2_reason",
+        "SuspensionDetail": "suspension_detail",
+        "SuspensionReason": "suspension_reason",
+        "SuspensionStartDate": "suspension_start_date",
+        "TransitionDetails": "details",
+        "TransitionReason": "reason",
+        "TranslationCompletedFields": "translation_completed_fields",
+        "TranslationRequiredFields": "translation_required_fields",
+        "TranslationStatus": "translation_status",
+        "ICPStatus": "project_status",
+        "ICP_ID": ('project_id.icp_id', 'compassion.project'),
+
+        # Not used in Odoo
+        'SourceKitName': None,
+    }
+
+    FIELDS_TO_SUBMIT = {
+        'gpid': None,
+    }
+
+    CONSTANTS = {
+
+    }
--- child_compassion/models/project_lifecycle_event.py
+++ child_compassion/models/project_lifecycle_event.py
@@ -4,13 +4,16 @@
 #    Copyright (C) 2016 Compassion CH (http://www.compassion.ch)
 #    Releasing children from poverty in Jesus' name
 #    @author: Emanuel Cino <ecino@compassion.ch>
+#             Philippe Heer <heerphilippe@msn.com>
 #
 #    The licence is in the file __openerp__.py
 #
 ##############################################################################
 
 
-from openerp import models, fields
+from openerp import models, fields, api
+from openerp.addons.message_center_compassion.mappings \
+    import base_mapping as mapping
 
 
 class ProjectLifecycle(models.Model):
@@ -19,15 +22,15 @@ class ProjectLifecycle(models.Model):
     _order = 'date desc'
 
     project_id = fields.Many2one(
-        'compassion.project', required=True, ondelete='cascade', readonly=True)
-    date = fields.Date(readonly=True)
+        'compassion.project', required=True, ondelete='cascade',
+        readonly=True)
+    date = fields.Date(readonly=True, default=fields.Date.today())
     type = fields.Selection([
         ('Suspension', 'Suspension'),
         ('Reactivation', 'Reactivation'),
         ('Transition', 'Transition'),
     ], readonly=True)
-    reason = fields.Char(readonly=True)
-    details = fields.Text(readonly=True)
+    # reason = fields.Char(readonly=True)
     action_plan = fields.Text(readonly=True)
 
     # Reactivation
@@ -38,6 +41,14 @@ class ProjectLifecycle(models.Model):
     ############
     suspension_start_date = fields.Date(readonly=True)
     suspension_end_date = fields.Date(readonly=True)
+    suspension_detail = fields.Char(readonly=True)
+    suspension_reason = fields.Selection([
+        ('Child Protection Issue', 'Child Protection Issue'),
+        ('Disaster event', 'Disaster event'),
+        ('Does not comply with policies', 'Does not comply with policies'),
+        ('Financial Mismanagement', 'Financial Mismanagement'),
+        ('Other', 'Other'),
+    ], readonly=True)
 
     hold_cdsp_funds = fields.Boolean(readonly=True)
     hold_csp_funds = fields.Boolean(readonly=True)
@@ -45,6 +56,8 @@ class ProjectLifecycle(models.Model):
     hold_s2b_letters = fields.Boolean(readonly=True)
     hold_b2s_letters = fields.Boolean(readonly=True)
     hold_child_updates = fields.Boolean(readonly=True)
+    is_beneficiary_information_updates_withheld = fields.Boolean(
+        readonly=True)
 
     extension_1 = fields.Boolean(
         help='Suspension is extended by 30 days', readonly=True)
@@ -56,8 +69,65 @@ class ProjectLifecycle(models.Model):
 
     # Transition
     ############
-    actual_transition_date = fields.Date(readonly=True)
+    transition_date = fields.Date(readonly=True)
     transition_complete = fields.Boolean(readonly=True)
-    future_involvement = fields.Char(readonly=True)
-    celebration_details = fields.Char(readonly=True)
-    relationship_strengths = fields.Char(readonly=True)
+    details = fields.Text(readonly=True)
+    reason = fields.Selection([
+        ('Child Protection Issue', 'Child Protection Issue'),
+        ('Disaster Event', 'Disaster Event'),
+        ('Does not comply with policies', 'Does not comply with policies'),
+        ('Financial Mismanagement', 'Financial Mismanagement'),
+        ('Other', 'Other'),
+        ('Successful Transition', 'Successful Transition'),
+    ], readonly=True)
+    projected_transition_date = fields.Date(readonly=True)
+    future_involvement = fields.Selection([
+        ('Advocacy', 'Advocacy'),
+        ('Alumni', 'Alumni'),
+        ('Church Mobilization', 'Church Mobilization'),
+        ('Leveraged', 'Leveraged'),
+        ('Mentoring', 'Mentoring'),
+        ('No Future Engagement', 'No Future Engagement'),
+        ('Training', 'Training'),
+    ], readonly=True)
+    # celebration_details = fields.Char(readonly=True)
+    # relationship_strengths = fields.Char(readonly=True)
+
+    # Translation
+    ############
+    translation_completed_fields = fields.Selection([
+        ('Action Plan', 'Action Plan'),
+        ('Suspension Details', 'Suspension Details'),
+        ('Transition Details', 'Transition Details'),
+    ], readonly=True)
+    translation_required_fields = fields.Selection([
+        ('Action Plan', 'Action Plan'),
+        ('Suspension Details', 'Suspension Details'),
+        ('Transition Details', 'Transition Details'),
+    ], readonly=True)
+    translation_status = fields.Selection([
+        ('Draft', 'Draft'),
+        ('Ready For Translation', 'Ready For Translation'),
+        ('Translation Complete', 'Translation Complete'),
+    ], readonly=True)
+
+    name = fields.Char(readonly=True)
+    reactivation_date = fields.Date(readonly=True)
+    project_status = fields.Selection([
+        ('Active', 'Active'),
+        ('Phase Out', 'Phase Out'),
+        ('Suspended', 'Suspended'),
+        ('Transitioned', 'Transitioned'),
+    ], readonly=True)
+
+    @api.model
+    def process_commkit(self, commkit_data):
+        project_mapping = mapping.new_onramp_mapping(
+            self._name,
+            self.env,
+            'new_project_lifecyle')
+
+        vals = project_mapping.get_vals_from_connect(commkit_data)
+        project = self.with_context(no_comm_kit=True).create(vals)
+
+        return [project.id]
