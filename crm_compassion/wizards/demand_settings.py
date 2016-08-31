ba924be5df515e9fb683e3f98f8479e98e245245
Maxime Beck <mbcompte@gmail.com>
2016-08-31 11:01:47 +0200

    CO-416 Add SUB Sponsorships Computing

--- crm_compassion/wizards/demand_settings.py
+++ crm_compassion/wizards/demand_settings.py
@@ -19,6 +19,7 @@ class DemandPlanningSettings(models.TransientModel):
 
     number_children_website = fields.Integer()
     number_children_ambassador = fields.Integer()
+    number_children_sub = fields.Integer()
     days_allocate_before_event = fields.Integer()
 
     @api.multi
@@ -34,6 +35,12 @@ class DemandPlanningSettings(models.TransientModel):
             str(self.number_children_ambassador))
 
     @api.multi
+    def set_number_children_sub(self):
+        self.env['ir.config_parameter'].set_param(
+            'crm_compassion.number_children_sub',
+            str(self.number_children_sub))
+
+    @api.multi
     def set_days_allocate_before_event(self):
         self.env['ir.config_parameter'].set_param(
             'crm_compassion.days_allocate_before_event',
@@ -45,11 +52,14 @@ class DemandPlanningSettings(models.TransientModel):
         web = int(param_obj.get_param('crm_compassion.number_children_web'))
         ambassador = int(param_obj.get_param(
             'crm_compassion.number_children_ambassador'))
+        sub = int(param_obj.get_param(
+            'crm_compassion.number_children_sub'))
         days_event = int(param_obj.get_param(
             'crm_compassion.days_allocate_before_event'))
 
         return {
             'number_children_website': web,
             'number_children_ambassador': ambassador,
+            'number_children_sub': sub,
             'days_allocate_before_event': days_event,
         }
