d0db76e564099d79d7290f158ed18d6a96283b0f
Emanuel Cino <ecino@compassion.ch>
2016-09-21 09:25:30 +0200

    Move Sponsorship Gifts in a new module

---
+++ gift_compassion/__init__.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2016 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Emanuel Cino <ecino@compassion.ch>
+#
+#    The licence is in the file __openerp__.py
+#
+##############################################################################
+
+from . import models
+from . import wizards
+# from . import mappings
