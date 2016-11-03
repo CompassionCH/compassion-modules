6c9d93c5b74e005110edf1ea6a644bc07689aba9
Emmanuel Girardin <manu18989@hotmail.com>
2016-11-03 09:54:16 +0100

    CO-534: Correspondence Templates faster and more reliable: Image are compared at 100DPI. The correspondence is made with ORB (FAST+BRIEF) instead of SIFT.

--- sbc_compassion/tools/patternrecognition.py
+++ sbc_compassion/tools/patternrecognition.py
@@ -18,6 +18,8 @@ import cv2
 import numpy as np
 import base64
 import tempfile
+import time
+import math
 from copy import deepcopy
 
 from openerp import _
@@ -39,6 +41,8 @@ def patternRecognition(image, pattern, crop_area=None,
     :param int threshold: Number of keypoints to find in order \
     to define a match
     :param bool full_result: if True, returns result from pattern too
+    :param float new_dpi: A new dpi smaller than 300 to reduce computation
+    time
 
     :returns: None if not enough keypoints found, position of the keypoints \
     (first index image/pattern)
@@ -68,27 +72,72 @@ def patternRecognition(image, pattern, crop_area=None,
     # cut the part useful for the recognition
     (xmin, ymin), img1 = subsetImage(img1, crop_area)
 
-    # compute the keypoints
-    sift = cv2.xfeatures2d.SIFT_create()
-    kp1, des1 = sift.detectAndCompute(img1, None)
-    kp2, des2 = sift.detectAndCompute(img2, None)
+    tic = time.time()
+    algo = 'ORB'
+    if algo == 'SIFT':
+        # compute the keypoints and descriptors using SIFT
+        sift = cv2.xfeatures2d.SIFT_create()
+        kp1, des1 = sift.detectAndCompute(img1, None)
+        kp2, des2 = sift.detectAndCompute(img2, None)
+        if not full_result:
+            print("\tSIFT keypoints: " + str(time.time() - tic))
+    elif algo == 'ORB':
+        # compute keypoints with ORB (FAST detector and BRIEF descriptor)
+        orb = cv2.ORB_create()
+        # ORB detect the corners (keypoints) with FAST
+        kp1_tmp = orb.detect(img1, None)
+        kp2_tmp = orb.detect(img2, None)
+        # And compute the descriptors with BRIEF
+        kp1, des1 = orb.compute(img1, kp1_tmp)
+        kp2, des2 = orb.compute(img2, kp2_tmp)
+        if not full_result:
+            print("\tORB keypoints: " + str(time.time() - tic))
 
     if des1 is None or des2 is None:
         return None
 
     # find matches between the two pictures
-    good = findMatches(des1, des2)
+    tic = time.time()
+    good12 = findMatches(des1, des2)
+
+    if not full_result:
+        print("\tfindMatches: "+str(len(good12)) +
+              " matches.\t" + str(time.time() - tic))
+    better12 = removeBadMatches(good12, kp1, kp2)
+    if not full_result:
+        print("\tremoveBadMatches: "+str(len(better12)) +
+              " matches.\t" + str(time.time() - tic))
+
+    good21 = findMatches(des2, des1)
+    better21 = removeBadMatches(good21, kp2, kp1)
+
+    best12 = keepReciproqueMatches(better12, better21)
+    if not full_result:
+        print("\tkeepReciproqueMatches: "+str(len(best12)) +
+              " matches.\t" + str(time.time() - tic))
 
     if full_result:
-        return kp1, kp2, good
-    if len(good) >= threshold:
+        return kp1, kp2, best12
+    if len(best12) >= threshold:
+        # list of positions of good keypoints
+        # tic = time.time()
+        posA = np.array([kp1[i[0].queryIdx].pt for i in best12])
+        posB = np.array([kp2[i[0].trainIdx].pt for i in best12])
+        angleA = np.array([kp1[i[0].queryIdx].angle for i in best12])
+        angleB = np.array([kp2[i[0].trainIdx].angle for i in best12])
+        # scaleAB, R, t = scaled_rigid_transform(posA, posB)
+        # print "scaled_rigid_transform: " + str(time.time() - tic)
+
+        disorder_pt, disorder_angle = measure_disorder(
+            posA, posB, angleA, angleB)
+
         # put in a np.array the position of the image's keypoints
-        keypoints = np.array([kp1[i[0].queryIdx].pt for i in good])
+        keypoints = np.array([kp1[i[0].queryIdx].pt for i in best12])
         # compute the position in the original picture
         keypoints = keypoints + np.array((xmin, ymin))
-        return keypoints
+        return keypoints, disorder_pt, disorder_angle
     else:
-        return None
+        return None, None, None
 
 
 def subsetImage(img, crop_area):
@@ -105,15 +154,15 @@ def subsetImage(img, crop_area):
     """
     h, w = img.shape[:2]
     # compute absolute coordinates
-    xmin = round(w * crop_area[0])
-    xmax = round(w * crop_area[1])
-    ymin = round(h * crop_area[2])
-    ymax = round(h * crop_area[3])
+    xmin = int(round(w * crop_area[0]))
+    xmax = int(round(w * crop_area[1]))
+    ymin = int(round(h * crop_area[2]))
+    ymax = int(round(h * crop_area[3]))
     # in opencv first index->height
     return (xmin, ymin), img[ymin:ymax, xmin:xmax]
 
 
-def findMatches(des1, des2, test=0.8):
+def findMatches(desA, desB, test=0.8):
     """
     Look through the descriptor in order to find some matches.
 
@@ -123,7 +172,7 @@ def findMatches(des1, des2, test=0.8):
     :rtype: list[Keypoints]
     """
     bf = cv2.BFMatcher()
-    matches = bf.knnMatch(des1, des2, k=2)
+    matches = bf.knnMatch(desA, desB, k=2)
     # Apply ratio test
     good = []
     for m, n in matches:
@@ -131,6 +180,99 @@ def findMatches(des1, des2, test=0.8):
             good.append([m])
 
     return good
+
+
+def removeBadMatches(goodAB, kpA, kpB, max_angle=45):
+    betterAB = []
+    for match in goodAB:
+        angleA = kpA[match[0].queryIdx].angle
+        angleB = kpB[match[0].trainIdx].angle
+        dAngle = math.fabs(angleA-angleB)
+        dAngle = dAngle if dAngle < 180 else 360-dAngle
+        if dAngle < max_angle:
+            betterAB.append(match)
+    return betterAB
+
+
+def keepReciproqueMatches(betterAB, betterBA):
+    bestAB = []
+    for matchAB in betterAB:
+        idA = matchAB[0].queryIdx
+        idB = matchAB[0].trainIdx
+        for matchBA in betterBA:
+            if idA == matchBA[0].trainIdx and idB == matchBA[0].queryIdx:
+                bestAB.append(matchAB)
+    return bestAB
+
+
+def measure_disorder(posA, posB, angA, angB):
+    """
+    Naive function to check if the matching point clouds looks the same
+    :param posA:
+    :param posB:
+    :param angA:
+    :param angB:
+    :return:
+    """
+
+    # centering positions around their mean
+    posA = posA - posA.mean(axis=0)
+    posB = posB - posB.mean(axis=0)
+
+    # if the the two patterns are similar, with no deformation and with same
+    # scale and orientations, then the sum of absolute differences should
+    # be close to zero
+    SAD = (np.absolute(posA-posB)).sum()
+    # we return the mean of the Sum of Absolute Difference
+    disorder_pt = SAD / len(posA)
+
+    disorder_angle = (np.absolute(angA-angB)).mean()
+
+    return disorder_pt, disorder_angle
+
+
+def scaled_rigid_transform(A, B):
+    """
+    The rigid transform is made of translation and rotations.
+    More explanations on http://nghiaho.com/?page_id=671
+    Here we've modified the rigid transform by adding the scaling term. It is
+    not a rigid transform anymore, but it still should find a nearly optimal
+    transformation made of scaling, rotation and translation.
+    """
+    assert len(A) == len(B)
+
+    centroid_A = np.mean(A, axis=0)
+    centroid_B = np.mean(B, axis=0)
+
+    # centre the points
+    AA = A - centroid_A
+    BB = B - centroid_B
+
+    # compute the scale factor from AA to BB. To do so, we compute the sum
+    # of magnitude A and B. (sum of pythagor)
+    magnitude_A = np.sum(np.sqrt(np.sum((np.square(AA)), axis=1)))
+    magnitude_B = np.sum(np.sqrt(np.sum((np.square(BB)), axis=1)))
+
+    scaleAB = magnitude_B/magnitude_A
+    AA *= scaleAB
+
+    # dot is matrix multiplication for array
+    H = np.transpose(AA) * BB
+
+    # compute the singular value decomposition transformation.
+    U, S, Vt = np.linalg.svd(H)
+
+    R = Vt.T * U.T
+
+    # special reflection case
+    if np.linalg.det(R) < 0:
+        print("Reflection detected")
+        Vt[1, :] *= -1
+        R = Vt.T * U.T
+
+    t = -scaleAB*R * centroid_A.T + centroid_B.T
+
+    return scaleAB, R, t
 
 
 def keyPointCenter(keypoints):
@@ -170,7 +312,8 @@ def keyPointCenter(keypoints):
         return center / N
 
 
-def find_template(img, templates, test=False, threshold=0.8):
+def find_template(img, templates, test=False,
+                  threshold=0.8, resize_ratio=1.0):
     """
     Use pattern recognition to detect which template correponds to img.
 
@@ -184,7 +327,8 @@ def find_template(img, templates, test=False, threshold=0.8):
     :rtype: template, layout, None or np.array
     """
     # number of keypoint related between the picture and the pattern
-    max_ratio_keypoints = 0.0
+    nb_keypoints = 0.0
+    # TODO: min_disorder = sys.float_info.max
     key_img = False
     matching_template = None
     if test:
@@ -196,32 +340,54 @@ def find_template(img, templates, test=False, threshold=0.8):
         # Crop the image to speedup detection and avoid false positives
         crop_area = template.get_pattern_area()
 
+        # resizing the pattern, assuming it had been saved at 300dpi
+        temp_image = []
+        with tempfile.NamedTemporaryFile() as temp:
+            temp.write(base64.b64decode(template.pattern_image))
+            temp.flush()
+            temp_image = cv2.imread(temp.name,
+                                    cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
+        temp_image = cv2.resize(temp_image, None,
+                                fx=resize_ratio, fy=resize_ratio,
+                                interpolation=cv2.INTER_CUBIC)
         if test:
+            # TODO CROP IMAGE BEFOR FUNCTION CALL
             recognition = patternRecognition(
-                img, template.pattern_image, crop_area, full_result=True)
-            with tempfile.NamedTemporaryFile() as temp:
-                temp.write(base64.b64decode(template.pattern_image))
-                temp.flush()
-                img2 = cv2.imread(temp.name)
+                img, temp_image, crop_area, full_result=True)
+
+            # with tempfile.NamedTemporaryFile() as temp:
+            #    temp.write(base64.b64decode(template.pattern_image))
+            #    temp.flush()
+            #    img2 = cv2.imread(temp.name)
             (xmin, ymin), img1 = subsetImage(img, crop_area)
             if recognition is not None:
                 kp1, kp2, good = recognition
-                img3 = cv2.drawMatchesKnn(img1, kp1, img2,
+                img3 = cv2.drawMatchesKnn(img1, kp1, temp_image,
                                           kp2, good, None, flags=2)
             else:
                 img3 = img1
             test_img.append(img3)
 
+        print("\n" + str(template.id) + ") " + template.name + ": ")
         # try to recognize the pattern
-        tmp_key = patternRecognition(
-            img, template.pattern_image, crop_area)
+        res = patternRecognition(img, temp_image, crop_area)
+        if res is None:
+            continue
+
+        tmp_key, tmp_disorder_pt, tmp_disorder_angle = res
+        if tmp_key is None:
+            continue
+
+        print("\tAngle disorder: " + str(tmp_disorder_angle))
+        print("\tPositions disorder: " + str(tmp_disorder_pt))
+
         # check if it is a better result than before
-        if (tmp_key is not None and
-                (threshold < float(len(tmp_key)) / float(
-                    template.nber_keypoints) > max_ratio_keypoints)):
+        if (len(tmp_key) > nb_keypoints):
             # save all the data if it is better
-            max_ratio_keypoints = float(
-                len(tmp_key))/float(template.nber_keypoints)
+            nb_keypoints = len(tmp_key)
             key_img = tmp_key
             matching_template = template
+
+    print("\nDetected template: " + matching_template.name + ' (' +
+          matching_template.layout + ')')
     return matching_template, keyPointCenter(key_img), test_img
