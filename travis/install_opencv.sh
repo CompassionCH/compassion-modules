3c7fe64accc203858ed117d6c316082c6e5a2835
Emanuel Cino <ecino@compassion.ch>
2016-11-22 14:46:33 +0100

    Fix travis for new sbc import letters

--- travis/install_opencv.sh
+++ travis/install_opencv.sh
@@ -12,10 +12,8 @@ set -ex
 
 OPENCV_BUILD_DIR=${OPENCV_ROOT}/build
 OPENCV_INSTALL_DIR=${OPENCV_ROOT}/install
-OPENCV_CONTRIB_DIR=${OPENCV_ROOT}/contrib
 
 git clone https://github.com/Itseez/opencv.git -b 3.1.0 ${OPENCV_ROOT}
-git clone https://github.com/Itseez/opencv_contrib -b 3.1.0 ${OPENCV_CONTRIB_DIR}
 
 mkdir -p ${OPENCV_BUILD_DIR}
 mkdir -p ${OPENCV_INSTALL_DIR}
@@ -24,7 +22,6 @@ cd ${OPENCV_BUILD_DIR}
 
 cmake \
 -DCMAKE_INSTALL_PREFIX=${OPENCV_INSTALL_DIR} \
--DOPENCV_EXTRA_MODULES_PATH=${OPENCV_CONTRIB_DIR}/modules \
 -DBUILD_PERF_TESTS=OFF \
 -DBUILD_TESTS=OFF \
 -DBUILD_opencv_java=OFF \
