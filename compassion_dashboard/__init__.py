699599b5aaf78b0257814ba6b01a197e22cda916
Maxime Beck <mbcompte@gmail.com>
2017-07-24 12:06:32 +0200

    Add Compassion Dashboard Module

---
+++ compassion_dashboard/__init__.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2017 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Maxime Beck, Nathan Flückiger
+#
+#    The licence is in the file __openerp__.py
+#
+##############################################################################
