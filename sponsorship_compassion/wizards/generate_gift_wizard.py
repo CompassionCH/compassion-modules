2b9b7a46bc4ef9545da04828e2a742dc21f3cb48
Emanuel Cino <ecino@compassion.ch>
2018-02-19 16:24:21 +0100

    CO-1459 FIX gift report not using correct partner

--- sponsorship_compassion/wizards/generate_gift_wizard.py
+++ sponsorship_compassion/wizards/generate_gift_wizard.py
@@ -101,8 +101,7 @@ class GenerateGiftWizard(models.TransientModel):
             ('company_id', '=', 1)], limit=1).id
         return {
             'type': 'out_invoice',
-            'partner_id': getattr(contract, contract.send_gifts_to,
-                                  contract.correspondant_id).id,
+            'partner_id': contract.gift_partner_id.id,
             'journal_id': journal_id,
             'date_invoice': invoice_date,
             'payment_mode_id': contract.payment_mode_id.id,
