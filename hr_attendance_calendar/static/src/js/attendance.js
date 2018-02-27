872f36da0b02d44671bdfd8cbfa50498a787058e
Samuel Fringeli <samuel.fringeli@me.com>
2018-02-27 14:38:49 +0100

    CO-1498 : Fix something that produced JS errors on other pages

--- hr_attendance_calendar/static/src/js/attendance.js
+++ hr_attendance_calendar/static/src/js/attendance.js
@@ -28,30 +28,32 @@ odoo.define('hr_switzerland.attendance', function (require) {
 
                     // auto-counter
                     $('#moment_pl').html(Date.now());
-                    setInterval(function() {
-                        var moment_start = moment(new Date(parseInt($('#moment_pl').text())));
-                        var diff_minutes = moment().diff(moment_start, 'minutes');
+                    setInterval(function () {
+                        if ($('#moment_pl').length) {
+                            var moment_start = moment(new Date(parseInt($('#moment_pl').text())));
+                            var diff_minutes = moment().diff(moment_start, 'minutes');
 
-                        ['worked_today', 'balance_today'].forEach(
-                        function(el) {
-                            var matches = $('#'+el+'_pl').text().match(/^-?(\d{2}):(\d{2})$/);
-                            var hours = parseInt(matches[1]);
-                            var minutes = parseInt(matches[2]);
+                            ['worked_today', 'balance_today'].forEach(
+                                function (el) {
+                                    var matches = $('#' + el + '_pl').text().match(/^-?(\d{2}):(\d{2})$/);
+                                    var hours = parseInt(matches[1]);
+                                    var minutes = parseInt(matches[2]);
 
-                            var total_minutes = (hours * 60 + minutes) * ((matches[0].substring(0,1) == '-') ? -1:1);
-                            var negative = (total_minutes + diff_minutes) < 0;
+                                    var total_minutes = (hours * 60 + minutes) * ((matches[0].substring(0, 1) == '-') ? -1 : 1);
+                                    var negative = (total_minutes + diff_minutes) < 0;
 
-                            var new_total = Math.abs(total_minutes + diff_minutes);
-                            var new_hours = ('0' + (new_total/60|0)).slice(-2);
-                            var new_minutes = ('0' + (new_total%60)).slice(-2);
+                                    var new_total = Math.abs(total_minutes + diff_minutes);
+                                    var new_hours = ('0' + (new_total / 60 | 0)).slice(-2);
+                                    var new_minutes = ('0' + (new_total % 60)).slice(-2);
 
-                            $('#'+el).text((negative ? '-':'') + new_hours + ':' + new_minutes);
+                                    $('#' + el).text((negative ? '-' : '') + new_hours + ':' + new_minutes);
 
-                            if (el != 'worked_today') {
-                                $('#'+el).parent().get(0).style.color = negative ? 'red':'green';
-                            }
-                        });
-                    }, 10000);
+                                    if (el != 'worked_today') {
+                                        $('#' + el).parent().get(0).style.color = negative ? 'red' : 'green';
+                                    }
+                                });
+                        }
+                    }, 20000);
                 });
 
             return result;
