3a5466a891db8a394113d826e2b7697f59bde19e
Nicolas Badoux <n.badoux@hotmail.com>
2018-03-20 16:50:17 +0100

    CO-1547 Faire un module survey phone
    Create a survey phone module to provide the capacity to the opertator to
    create survey without sending an email and completing it himself.

---
+++ survey_compassion/README.rst
@@ -0,0 +1,28 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :alt: License: AGPL-3
+
+Advanced Survey
+====================
+
+Use this module to make easy phone survey of the sponsors. Offers the ability to bypass the sending of an email.
+
+Usage
+=====
+
+
+Known issues / Roadmap
+======================
+Nothing is yet implemented.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Nicolas Badoux <n.badoux@hotmail.com>
+
+Maintainer
+----------
+
+This module is maintained by `Compassion Switzerland <https://www.compassion.ch>`.
---
+++ survey_compassion/models/survey_user_input.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2016 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Nicolas Badoux <n.badoux@hotmail.com>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+import logging
+
+from odoo import models, fields
+from odoo.addons.base_phone import fields as phone_fields
+logger = logging.getLogger(__name__)
+
+
+class SurveyUserInput(models.Model):
+    """
+    Add some fields to the answer view for a survey. In particular a filed for
+    both mobile and phone number as well as a clickable URL to the survey page.
+    """
+    _inherit = 'survey.user_input'
+    phone = phone_fields.Phone(related='partner_id.phone')
+    mobile = phone_fields.Phone(related='partner_id.mobile')
+    survey_link = fields.Char("Link to complete the survey",
+                              compute='_compute_survey_link')
+
+    def _compute_survey_link(self):
+        """
+        Recreate the private url to access the survey from the token and public
+        url available in self.
+        :return: Nothing
+        """
+        self.survey_link = self.survey_id.public_url + '/' + self.token
