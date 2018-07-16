e40c3d7258858122fe81d1c856e32eae6ae7a842
Civiliste Compassion <gigon.quentin@gmail.com>
2018-07-16 14:45:12 +0200

    created structure for sms sponsorship

---
+++ sms_sponsorship/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2018 Compassion CH (http://www.compassion.ch)
+#    @author: Quentin Gigon <gigon.quentin@gmail.com>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+{
+    'name': 'Compassion SMS Sponsorships',
+    'version': '10.0.1.0.3',
+    'category': 'Other',
+    'author': 'Compassion CH',
+    'license': 'AGPL-3',
+    'website': 'http://www.compassion.ch',
+    'depends': ['crm_compassion', 'website-cms'],
+    'data': [],
+    'demo': [
+        'demo/demo_data.yml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
