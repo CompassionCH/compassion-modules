d04fa57ec69b164634b4f3273c59f0987b68120e
Emanuel Cino <ecino@compassion.ch>
2018-07-19 10:15:13 +0200

    CO-1916 [ADD] hold children for SMS requests

--- sms_sponsorship/models/compassion_hold.py
+++ sms_sponsorship/models/compassion_hold.py
@@ -1,49 +1,32 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2016 Compassion CH (http://www.compassion.ch)
+#    Copyright (C) 2018 Compassion CH (http://www.compassion.ch)
 #    Releasing children from poverty in Jesus' name
 #    @author: Emanuel Cino <ecino@compassion.ch>
 #
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
-import logging
+from odoo import api, models, fields
 
-from odoo import api, models, fields, _
-from odoo.exceptions import UserError
-import re
 
-logger = logging.getLogger(__name__)
+class AbstractHold(models.AbstractModel):
+    _inherit = 'compassion.abstract.hold'
+
+    channel = fields.Selection(selection_add=[
+        ('sms', 'SMS sponsorship service')
+    ])
 
 
 class CompassionHold(models.Model):
-
     _inherit = 'compassion.hold'
 
-    booked_by_phone_number = fields.Char()
-    booked_by_sms_at = fields.Datetime(required=False)
+    sms_request_id = fields.Many2one('sms.child.request')
 
     @api.multi
-    def book_by_sms(self, phone_number):
-        self.ensure_one()
-        assert re.match(r'\+\d+', phone_number), \
-            "The phone number do not match the international format"
-        assert not self.event_id or self.event_id.accepts_sms_booking, \
-            "SMS booking isn't supported for this event"
-        if self.booked_by_phone_number:
-            raise UserError(_('Child already booked by {}'
-                            .format(self.booked_by_phone_number)))
-        self.booked_by_phone_number = phone_number
-        self.booked_by_sms_at = fields.Datetime.now()
-
-    @api.multi
-    def release_sms_booking(self):
-        self.ensure_one()
-        self.booked_by_phone_number = None
-        self.booked_by_sms_at = None
-
-    @api.multi
-    def generate_url_of_next_step(self):
-        self.ensure_one()
-        return 'sms-sponsorship/{}'.format(self.id)
+    def hold_released(self, vals=None):
+        self.sms_request_id.write({
+            'state': 'expired'
+        })
+        return super(CompassionHold, self).hold_released(vals)
--- sms_sponsorship/models/event_compassion.py
+++ sms_sponsorship/models/event_compassion.py
@@ -3,16 +3,48 @@
 #
 #    Copyright (C) 2018 Compassion CH (http://www.compassion.ch)
 #    Releasing children from poverty in Jesus' name
-#    @author: Nicolas Bornand
+#    @author: Nicolas Bornand, Emanuel Cino
 #
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
-from odoo import models, fields
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, models, fields
+from odoo.addons.child_compassion.models.compassion_hold import HoldType
 
 
 class EventCompassion(models.Model):
 
     _inherit = 'crm.event.compassion'
 
-    accepts_sms_booking = fields.Boolean('Accepts SMS booking', default=False)
+    accepts_sms_booking = fields.Boolean('Accepts SMS booking')
+
+    @api.model
+    def hold_children_for_sms(self):
+        in_one_day = datetime.now() + relativedelta(days=1)
+        for event in self.search([
+            ('accepts_sms_booking', '=', True),
+            ('start_date', '<=', fields.Datetime.to_string(in_one_day))
+        ]):
+            childpool_search = self.env['compassion.childpool.search'].create({
+                # Take 5% of planned sponsorships
+                'take': int(event.planned_sponsorships * 0.05) or 3,
+            })
+            childpool_search.rich_mix()
+            expiration = fields.Datetime.from_string(event.end_date) + \
+                relativedelta(days=1)
+            self.env['child.hold.wizard'].with_context(
+                active_id=childpool_search.id).create({
+                    'type': HoldType.CONSIGNMENT_HOLD.value,
+                    'expiration_date': fields.Datetime.to_string(expiration),
+                    'primary_owner': self.env.uid,
+                    'event_id': event.id,
+                    'campaign_id': event.campaign_id.id,
+                    'ambassador': event.user_id.partner_id.id or self.env.uid,
+                    'channel': 'sms',
+                    'source_code': 'automatic_sms_reservation_for_event',
+                    'return_action': 'view_holds'
+                }).send()
+        return True
