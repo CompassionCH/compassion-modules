7394513b2149303bcdf7a4f8067f2df0574ea0e9
Civiliste Compassion <gigon.quentin@gmail.com>
2018-07-24 12:03:50 +0200

    CO-1912 major_revision method for household is done

--- child_compassion/models/household.py
+++ child_compassion/models/household.py
@@ -227,6 +227,7 @@ class Household(models.Model):
         return res
 
     def major_revision(self, vals):
+        # TODO fix for update on both parents (major_revision_multiple)
         revised_data = []
         household_mapping = HouseHoldMapping(self.env)
         household_data = household_mapping.get_vals_from_connect(
@@ -235,26 +236,32 @@ class Household(models.Model):
             ('household_id', '=', household_data['household_id'])
         ])
 
-        if household_data['father_alive'] != household_obj.father_alive:
+        if household_data['father_alive'] == 'No':
             revised_data.append({
                 'name': 'Is Natural Father Alive?',
                 'old_value': household_obj.father_alive,
                 'household_id': household_obj.id
             })
-        if household_data['mother_alive'] != household_obj.mother_alive:
+        if household_data['mother_alive'] == 'No':
             revised_data.append({
                 'name': 'Is Natural Mother Alive?',
                 'old_value': household_obj.mother_alive,
                 'household_id': household_obj.id
             })
+        revised_data_ids = []
+        for data in revised_data:
+            revised_data_ids.append(
+                self.env['compassion.major.revision'].create(
+                    data).id)
 
-        if revised_data:
+        if revised_data_ids:
             for child in household_obj.child_ids:
-                child._major_revision(
-                    {'revised_value_ids': [
-                        (0, 0, data) for data in revised_data]}
-                )
-        return True
+                child._major_revision({
+                    'revised_value_ids': [(6, _, revised_data_ids)]
+                })
+
+        household_obj.write(household_data)
+        return household_obj.ids
 
 
 class HouseholdMembers(models.Model):
