dfb66035dd7163ce13c0e3aee8ee16d007636d48
Civiliste Compassion <gigon.quentin@gmail.com>
2018-07-27 11:43:01 +0200

    AP-11 route and endpoint for get message created and working. need more info on response shape

---
+++ mobile_app_connector/models/res_partner.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2018 Compassion CH (http://www.compassion.ch)
+#    @author: Quentin Gigon <gigon.quentin@gmail.com>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+from odoo import models, fields, api
+
+
+class GetPartnerMessage(models.Model):
+    _inherit = "res.partner"
+
+    @api.model
+    def mobile_get_message(self, **other_params):
+        values = dict(other_params)
+        partner_id = values['partner_id']
+        nb_sponsorships = len(self.env["recurring.contract"].search([
+            ('partner_id.id', '=', partner_id)
+        ]))
+
+        messages = [{
+            "ActionDestination": "Child selector",
+            "Body": "You're changing " + str(nb_sponsorships) + " lives",
+            "SortOrder": 1001,
+            "SubType": "CH1",
+            "Title": "Thank You",
+            "Type": "Child"
+        }]
+
+        links = {
+            "Base": "<base url here>",
+            "Next": "/mobile-app-api/get-message/" + str(partner_id)
+                    + "?limit=1&start=1",
+            "Self": "<base url here>/mobile-app-api/get-message/" + str(
+                partner_id) + "?limit=1&start=0"
+        }
+
+        response = {
+            "Limit": 1,
+            "Messages": messages,
+            "Size": 1,
+            "Start": 0,
+            "_Links": links
+        }
+        return response
