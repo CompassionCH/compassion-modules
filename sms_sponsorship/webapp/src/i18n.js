46179ec63008e1c284e3dad094ec7eb325004408
quentingigon <gigon.quentin@gmail.com>
2018-08-03 16:05:48 +0200

    CO-1959 finished french and german translations for SMS (#755)

--- sms_sponsorship/webapp/src/i18n.js
+++ sms_sponsorship/webapp/src/i18n.js
@@ -70,10 +70,10 @@ i18n
                 translations: {
                     // App.js
                     "error_noRequestID": "Erreur : requête invalide. <br/>Pour recevoir un nouveau lien valide, merci d'envoyer COMPASSION par SMS au 959.",
+                    "error_noService": "Erreur : le service n'est pas disponible pour le moment. <br/>Veuillez essayer dans quelques minutes. Merci !",
                     "error_sponsorshipAlreadyMade": "Cet enfant est déjà parrainé! <br/><br/> Si vous souhaitez parrainer un autre enfant, merci d'envoyer COMPASSION par SMS au 959.",
-                    "error_noService": "Error : the service is not available at this time. <br/>Please try again in a few minutes. Thank you!",
                     "waitingForChild": "Merci de patienter quelques secondes pendant que nous recherchons un enfant sans parrain...",
-                    "waitingForOtherChild": "Nous recherchons un enfant qui correspond à tes critères, merci de patienter quelques secondes...",
+                    "waitingForOtherChild": "Nous recherchons un enfant qui correspond à vos critères, merci de patienter quelques secondes...",
                     "ageYears": " ans",
                     // ChildCard.js
                     "cardTitle": "Parraine un enfant",
@@ -85,7 +85,7 @@ i18n
                     // ChildDetails.js
                     "more": "Plus d'informations",
                     // SponsorForm.js
-                    "coordinates": "Tes coordonnées",
+                    "coordinates": "Vos coordonnées",
                     "firstname": "Prénom",
                     "lastname": "Nom",
                     "email": "Email",
@@ -94,7 +94,7 @@ i18n
                     "sponsorNow": "Parrainer {{name}}",
                     // ModalForm.js
                     "chooseTitle": "Choisir un autre enfant",
-                    "modalHelp": "Tu peux spécifier ici quelques critères de recherche. Laisse-les simplement vides si tu ne veux pas choisir.",
+                    "modalHelp": "Vous pouvez spécifier ici quelques critères de recherche. Laissez-les simplement vides si vous ne voulez pas choisir.",
                     "genderSelect": "Garçon/Fille",
                     "ageSelect": "Tranche d'âge",
                     "countrySelect": "Pays",
@@ -109,80 +109,80 @@ i18n
                     // SponsorshipPlusTabs.js
                     "basicTab": "Parrainage standard",
                     "basicTitle": "CHF 42.- / mois",
-                    "basicDescription": "Your monthly support provides life-changing opportunities such as " +
-                        "an opportunity to attend or stay in school, " +
-                        "medical care, nourishing food, mentoring, and a safe environment. <br/><br/>" +
-                        "Compassion child sponsorship works! Millions of children have “graduated” from our program " +
-                        "and are now responsible, fulfilled adults.",
+                    "basicDescription": "Votre soutien mensuel offre des opportunités qui changent la vie telles qu'"+
+                         "une opportunité d'assister ou de rester à l'école, " +
+                        "des soins médicaux, des repas nourrissants, du mentorat et un environnement sûr. <br/><br/>" +
+                        "Le parrainage d'enfants de Compassion fonctionne! Des millions d'enfants ont été diplômés de notre programme " +
+                        "et sont maintenant des adultes responsables et accomplis.",
                     "plusTab": "Parrainage plus",
                     "plusTitle": "CHF 50.- / mois",
-                    "plusDescription": "Sponsorship Plus is the basic sponsorship " +
-                        "plus an additional donation of CHF 8.00 per month: <br/><br/>it enables Compassion to finance " +
-                        "projects to change the children's environment. " +
-                        "The funds received under the Sponsorship Plus program contribute to a common fund " +
-                        "and enable Compassion to support several projects each year.",
+                    "plusDescription": "Le Sponsorship Plus représente le parrainage basique" +
+                        "plus un don additionnel de 8 CHF par mois: <br/><br/>cela permet à Compassion de financer " +
+                        "des projets pour changer l'environnement des enfants." +
+                        "Les fonds reçus par le Sponsorship Plus contribuent à un fond commun" +
+                        "et permettent à Compassion de soutenir plusieurs projets par année.",
                     // SuccessMessage.js
                     "successMessage1": "Félicitations ! {{preferred_name}} sera heureux d'apprendre que vous êtes son parrain.",
-                    "successMessage2": "Il reste quelques petite étapes avant de pouvoir débuter le parrainage, mais soyez tranquille : vous recevrez tous les détails nécessaires par e-mail. Vous pourrez ainsi compléter les informations manquantes lorsque vous aurez du temps.",
+                    "successMessage2": "Il reste quelques petites étapes avant de pouvoir débuter le parrainage, mais soyez tranquille : vous recevrez tous les détails nécessaires par e-mail. Vous pourrez ainsi compléter les informations manquantes lorsque vous aurez du temps.",
                 }
             },
             de: {
                 translations: {
                     // App.js
-                    "successMessage_male": "Hooray! {{preferred_name}} will soon receive the good news that you are his sponsor. <br/><br/>There a few more steps required to confirm the sponsorship, but no worries: you will receive all the details per e-mail so that you can complete your registration when you have time.",
-                    "successMessage_female": "Hooray! {{preferred_name}} will soon receive the good news that you are her sponsor. <br/><br/>There a few more steps required to confirm the sponsorship, but no worries: you will receive all the details per e-mail so that you can complete your registration when you have time.",
-                    "error_noRequestID": "Error : this request is invalid. <br/>Please try again to get a valid link by texting COMPASSION to 959. Thank you!",
-                    "error_sponsorshipAlreadyMade": "You already sponsored this child, thank you! <br/><br/> If you want to sponsor another child, please text again COMPASSION to 959.",
-                    "error_noService": "Error : the service is not available at this time. <br/>Please try again in a few minutes. Thank you!",
-                    "waitingForChild": "we are looking for a child waiting for a sponsor, please wait a few seconds...",
-                    "waitingForOtherChild": "we are looking for a child matching your request, please wait a few seconds...",
-                    "ageYears": " years",
+                    "successMessage_male": "Hurra! {{preferred_name}} wird bald die gute Nachricht erhalten, dass Sie sein Sponsor sind. <br/><br/>Es sind noch ein paar Schritte nötig, um das Sponsoring zu bestätigen, aber keine Sorge: Sie werden alle Details per E-Mail erhalten, damit Sie Ihre Registrierung abschließen können, wenn Sie Zeit haben.",
+                    "successMessage_female": "Hooray! {{preferred_name}} wird bald die gute Nachricht erhalten, dass Sie sein Sponsor sind. <br/><br/> Es sind noch ein paar Schritte nötig, um das Sponsoring zu bestätigen, aber keine Sorge: Sie werden alle Details per E-Mail erhalten, damit Sie Ihre Registrierung abschließen können, wenn Sie Zeit haben.",
+                    "error_noRequestID": "Fehler: Diese Anfrage ist ungültig. <br/>Bitte versuchen Sie es erneut, um einen gültigen Link zu erhalten, indem Sie COMPASSION an 959 senden. Danke!",
+                    "error_sponsorshipAlreadyMade": "Du hast dieses Kind bereits gesponsert, danke! <br/><br/> Wenn Sie ein anderes Kind sponsern möchten, geben Sie bitte erneut COMPASSION bis 959 ein.",
+                    "error_noService": "Error : Der Dienst ist zu diesem Zeitpunkt nicht verfügbar. <br/>Bitte versuchen Sie es in ein paar Minuten noch einmal. Vielen Dank!",
+                    "waitingForChild": "Wir suchen ein Kind, das auf einen Sponsor wartet, bitte warten Sie ein paar Sekunden ...",
+                    "waitingForOtherChild": "Wir suchen ein Kind, das Ihrer Anfrage entspricht, bitte warten Sie ein paar Sekunden ...",
+                    "ageYears": " Jahre",
                     // ChildCard.js
-                    "cardTitle": "Sponsor a child",
-                    "age": "Age",
-                    "gender": "Gender",
-                    "country": "Country",
-                    "Male": "Male",
-                    "Female": "Female",
+                    "cardTitle": "Patenschaft für ein Kind",
+                    "age": "Alter",
+                    "gender": "Geschlecht",
+                    "country": "Land",
+                    "Male": "Männlich",
+                    "Female": "Weiblich",
                     // ChildDetails.js
-                    "more": "More details",
+                    "more": "Mehr Details",
                     // SponsorForm.js
-                    "coordinates": "Your coordinates",
-                    "firstname": "Firstname",
-                    "lastname": "Lastname",
+                    "coordinates": "Ihre Koordinaten",
+                    "firstname": "Vorname",
+                    "lastname": "Nachname",
                     "email": "Email",
                     "sponsorshipPlus": "Sponsorship plus",
-                    "otherChild": "Other child",
-                    "sponsorNow": "Sponsor now",
+                    "otherChild": "Anderes Kind",
+                    "sponsorNow": "Sponsor jetzt",
                     // ModalForm.js
-                    "chooseTitle": "Choose another child",
-                    "modalHelp": "Select filters for another child. You can leave the filters empty if you don't want to choose on given criteria.",
-                    "genderSelect": "Choose a boy or a girl",
-                    "ageSelect": "Select age",
-                    "countrySelect": "Choose a country",
-                    "cancel": "Cancel",
-                    "boy": "Boy",
-                    "girl": "Girl",
-                    "ageCat1": "0-3 years",
-                    "ageCat2": "4-6 years",
-                    "ageCat3": "7-10 years",
-                    "ageCat4": "11-14 years",
-                    "ageCat5": "15-20 years",
+                    "chooseTitle": "Wähle ein anderes Kind",
+                    "modalHelp": "Wählen Sie Filter für ein anderes Kind aus. Sie können die Filter leer lassen, wenn Sie nicht nach bestimmten Kriterien auswählen möchten.",
+                    "genderSelect": "Wähle einen Jungen oder ein Mädchen",
+                    "ageSelect": "Wählen Sie das Alter aus",
+                    "countrySelect": "Wähle ein Land",
+                    "cancel": "Abbestellen",
+                    "boy": "Junge",
+                    "girl": "Mädchen",
+                    "ageCat1": "0-3 Jahre",
+                    "ageCat2": "4-6 Jahre",
+                    "ageCat3": "7-10 Jahre",
+                    "ageCat4": "11-14 Jahre",
+                    "ageCat5": "15-20 Jahre",
                     // SponsorshipPlusTabs.js
                     "basicTab": "Sponsorship basic",
-                    "basicTitle": "CHF 42.- / month",
-                    "basicDescription": "Your monthly support provides life-changing opportunities such as " +
-                        "an opportunity to attend or stay in school, " +
-                        "medical care, nourishing food, mentoring, and a safe environment. <br/><br/>" +
-                        "Compassion child sponsorship works! Millions of children have “graduated” from our program " +
-                        "and are now responsible, fulfilled adults.",
+                    "basicTitle": "CHF 42.- / Monat",
+                    "basicDescription": "Ihre monatliche Unterstützung bietet lebensverändernde Möglichkeiten wie " +
+                        "die Möglichkeit, in der Schule zu bleiben oder zu bleiben, " +
+                        "Nahrung zu nähren, Mentoring und eine sichere Umgebung. <br/><br/>" +
+                        "Mitgefühlskindersponsoring funktioniert! Millionen von Kindern haben unser Programm absolviert " +
+                        "und sind nun verantwortliche, erfüllte Erwachsene.",
                     "plusTab": "Sponsorship plus",
-                    "plusTitle": "CHF 50.- / month",
-                    "plusDescription": "Sponsorship Plus is the basic sponsorship " +
-                        "plus an additional donation of CHF 8.00 per month: <br/><br/>it enables Compassion to finance " +
-                        "projects to change the children's environment. " +
-                        "The funds received under the Sponsorship Plus program contribute to a common fund " +
-                        "and enable Compassion to support several projects each year."
+                    "plusTitle": "CHF 50.- / Monat",
+                    "plusDescription": "Sponsorship Plus ist die Grundförderung " +
+                        "plus eine zusätzliche Spende von CHF 8.00 pro Monat: <br/><br/>Es ermöglicht Compassion, Projekte zu finanzieren, " +
+                        "um die Umwelt der Kinder zu verändern. " +
+                        "Die im Rahmen des Programms Sponsorship Plus erhaltenen Mittel tragen zu einem gemeinsamen Fonds bei " +
+                        "und ermöglichen es Compassion, jedes Jahr mehrere Projekte zu unterstützen."
                 }
             },
             it: {
