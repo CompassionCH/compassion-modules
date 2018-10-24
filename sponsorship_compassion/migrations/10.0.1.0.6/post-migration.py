d18655371d39cc3868146da7500753587ca9d43c
Civiliste Compassion <gigon.quentin@gmail.com>
2018-10-24 10:51:52 +0200

    CO-2112 applied change requests

--- sponsorship_compassion/migrations/10.0.1.0.6/post-migration.py
+++ sponsorship_compassion/migrations/10.0.1.0.6/post-migration.py
@@ -7,11 +7,9 @@
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
-from odoo import fields
 from openupgradelib import openupgrade
 
 import random
-import dateutil.parser
 
 
 @openupgrade.migrate(use_env=True)
@@ -19,10 +17,8 @@ def migrate(env, version):
     if not version:
         return
 
-    end_date_limit = dateutil.parser.parse("2016-12-13").date()
-
     old_sponsorships = env['recurring.contract'].search([
-        ('end_date', '<=', fields.Date.to_string(end_date_limit)),
+        ('type', 'like', 'S'),
         ('state', '=', 'terminated'),
         ('global_id', '=', False),
         ('activation_date', '!=', False)
@@ -31,10 +27,10 @@ def migrate(env, version):
     random_global_ids = range(0, 1000000)
     random.shuffle(random_global_ids)
 
-    min_transmition_delay = 5
+    min_days_duration = 10
 
     for sponsorship in old_sponsorships.filtered(
-            lambda s: s.contract_duration > min_transmition_delay):
+            lambda s: s.contract_duration > min_days_duration):
         sponsorship.write({
             'global_id': 'missing-' + str(random_global_ids.pop())
         })
