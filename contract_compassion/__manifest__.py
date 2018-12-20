e1b84cab5299524516780fdb447c924203018300
Emanuel Cino <ecino@compassion.ch>
2018-12-20 15:12:02 +0100

    FIX commitment number

--- contract_compassion/__manifest__.py
+++ contract_compassion/__manifest__.py
@@ -30,7 +30,7 @@
 # pylint: disable=C8101
 {
     'name': 'Compassion Contracts',
-    'version': '10.0.2.1.0',
+    'version': '10.0.2.2.0',
     'category': 'Other',
     'author': 'Compassion CH',
     'license': 'AGPL-3',
