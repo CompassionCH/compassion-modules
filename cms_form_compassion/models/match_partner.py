f796a0d3cf4dce9a758a73e234c7eca93b6c5dec
Christopher Meier <25900023+c-meier@users.noreply.github.com>
2019-02-20 14:35:41 +0100

    Moved the need validation flag to the compassion-switzerland module. Some cosmetic change.

--- cms_form_compassion/models/match_partner.py
+++ cms_form_compassion/models/match_partner.py
@@ -9,7 +9,7 @@
 #
 ##############################################################################
 
-from odoo import api, models, _
+from odoo import api, models
 
 
 class MatchPartner(models.AbstractModel):
@@ -56,6 +56,53 @@ class MatchPartner(models.AbstractModel):
         return partner
 
     @api.model
+    def match_after_match(self, partner, new_partner, infos):
+        """Once a match is found or created, this method allows to change it"""
+        if not new_partner:
+            self.match_update(partner, infos)
+        return partner
+
+    @api.model
+    def match_create(self, partner_obj, infos):
+        """Create a new partner from a selection of the given infos."""
+        create_infos = self.match_process_create_infos(infos)
+        infos.setdefault('lang', self.env.lang)
+        return partner_obj.create(create_infos)
+
+    @api.model
+    def match_process_create_infos(self, infos):
+        """
+        From the info given by the user, select the one that should be used
+        for the creation of the partner.
+        """
+        valid = self._match_get_valid_create_fields()
+        create_infos = {}
+        for key, value in infos.iteritems():
+            if key in valid:
+                create_infos[key] = value
+
+        return create_infos
+
+    @api.model
+    def match_update(self, partner, infos):
+        """Update the matched partner with a selection of the given infos."""
+        update_infos = self.match_process_update_infos(infos)
+        partner.write(update_infos)
+
+    @api.model
+    def match_process_update_infos(self, infos):
+        """
+        From the info given by the user, select the one that should be used
+        for the update of the partner.
+        """
+        valid = self._match_get_valid_update_fields()
+        update_infos = {}
+        for key, value in infos.iteritems():
+            if key in valid:
+                update_infos[key] = value
+        return update_infos
+
+    @api.model
     def _match_get_rules_order(self):
         """
         Get, in order, the name of the methods used to find a matching partner.
@@ -81,61 +128,11 @@ class MatchPartner(models.AbstractModel):
         ])
 
     @api.model
-    def match_after_match(self, partner, new_partner, infos):
-        """Once a match is found or created, this method allows to change it"""
-        if not new_partner:
-            self.match_update(partner, infos)
-        return partner
-
-    @api.model
-    def match_process_create_infos(self, infos):
-        """
-        From the info given by the user, select the one that should be used
-        for the creation of the partner.
-        """
-        valid = self._match_get_valid_create_fields()
-        create_infos = {}
-        for key, value in infos.iteritems():
-            if key in valid:
-                create_infos[key] = value
-
-        # Mark the partner to be validated
-        create_infos['state'] = 'pending'
-
-        return create_infos
-
-    @api.model
-    def match_create(self, partner_obj, infos):
-        """Create a new partner from a selection of the given infos."""
-        create_infos = self.match_process_create_infos(infos)
-        infos.setdefault('lang', self.env.lang)
-        return partner_obj.create(create_infos)
-
-    @api.model
     def _match_get_valid_create_fields(self):
         """Return the fields which can be used at creation."""
         return ['firstname', 'lastname', 'email', 'phone', 'street', 'city',
                 'zip', 'country_id', 'state_id', 'title', 'lang', 'birthdate',
                 'church_unlinked', 'function', 'spoken_lang_ids']
-
-    @api.model
-    def match_process_update_infos(self, infos):
-        """
-        From the info given by the user, select the one that should be used
-        for the update of the partner.
-        """
-        valid = self._match_get_valid_update_fields()
-        update_infos = {}
-        for key, value in infos.iteritems():
-            if key in valid:
-                update_infos[key] = value
-        return update_infos
-
-    @api.model
-    def match_update(self, partner, infos):
-        """Update the matched partner with a selection of the given infos."""
-        update_infos = self.match_process_update_infos(infos)
-        partner.write(update_infos)
 
     @api.model
     def _match_get_valid_update_fields(self):
