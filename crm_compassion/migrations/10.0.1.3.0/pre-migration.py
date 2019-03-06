ee0d83d71428a20d8a853dc5bc75cb3e979aa5c9
Emanuel Cino <ecino@compassion.ch>
2019-03-06 09:14:08 +0100

    CO-2399 Improve performance for interaction_resume
    - Replace SQL view by Transient table

---
+++ crm_compassion/migrations/10.0.1.3.0/pre-migration.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2019 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Emanuel Cino <ecino@compassion.ch>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+
+def migrate(cr, version):
+    if not version:
+        return
+
+    # Remove SQL view
+    try:
+        cr.execute("""
+            DROP VIEW interaction_resume IF EXISTS;
+        """)
+    except:
+        cr.rollback()
