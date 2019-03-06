49b52a580dfb9dbcacb97e5895642c6972b82212
Emanuel Cino <ecino@compassion.ch>
2019-03-06 09:14:08 +0100

    CO-2399 Improve performance for interaction_resume
    - Replace SQL view by Transient table

--- crm_compassion/models/interaction_resume.py
+++ crm_compassion/models/interaction_resume.py
@@ -1,22 +1,19 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2014 Compassion CH (http://www.compassion.ch)
+#    Copyright (C) 2019 Compassion CH (http://www.compassion.ch)
 #    Releasing children from poverty in Jesus' name
 #    @author: Fluckiger Nathan <nathan.fluckiger@hotmail.ch>
 #
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
-from odoo import tools
 from odoo import models, fields, api
 
 
-class InteractionResume(models.Model):
+class InteractionResume(models.TransientModel):
 
     _name = "interaction.resume"
-    _auto = False
-    _table = "interaction_resume"
     _order = "communication_date desc"
 
     partner_id = fields.Many2one("res.partner", "Partner")
@@ -30,94 +27,105 @@ class InteractionResume(models.Model):
     ])
     # Used to display icons in tree view
     state = fields.Selection(related='direction')
-    communication_date = fields.Date()
+    communication_date = fields.Datetime()
     subject = fields.Text()
-    body = fields.Html(compute='_compute_body')
+    body = fields.Html()
     phone_id = fields.Many2one("crm.phonecall", "Phonecall")
     paper_id = fields.Many2one("partner.communication.job", "Communication")
     email_id = fields.Many2one("mail.mail", "Email")
     message_id = fields.Many2one("mail.message", "Email")
 
-    def _compute_body(self):
-        for resume in self:
-            resume.body = resume.paper_id.body_html or resume.phone_id.name or\
-                resume.email_id.body_html or resume.message_id.body
-
-    @api.model_cr
-    def init(self):
-        tools.drop_view_if_exists(self.env.cr, self._table)
+    @api.model
+    def populate_resume(self, partner_id):
+        """
+        Creates the rows for the resume of given partner
+        :param partner_id: the partner
+        :return: True
+        """
+        self.search([('partner_id', '=', partner_id)]).unlink()
         self.env.cr.execute("""
-            CREATE OR REPLACE VIEW %s AS (
-              SELECT ROW_NUMBER()
-                OVER (ORDER BY sub.communication_type, sub.res_id) as id,
-              sub.*
-    -- paper communications
-              FROM (( SELECT
-                pcj.id AS res_id,
+            SELECT
                 'Paper' as communication_type,
                 pcj.sent_date as communication_date,
                 pcj.partner_id,
                 pcj.subject,
+                pcj.body_html AS body,
                 'out' AS direction,
-                NULL::Integer as phone_id,
-                NULL::Integer as email_id,
-                NULL::Integer as message_id,
+                0 as phone_id,
+                0 as email_id,
+                0 as message_id,
                 pcj.id as paper_id
                 FROM "partner_communication_job" as pcj
                 WHERE pcj.state = 'done'
                 AND pcj.send_mode = 'physical'
-                )
+                AND pcj.partner_id = %s
     -- phonecalls
-            UNION ALL (
+            UNION (
               SELECT
-                crmpc.id AS res_id,
                 'Phone' as communication_type,
                 crmpc.date as communication_date,
                 crmpc.partner_id as partner_id,
                 crmpc.name as subject,
+                crmpc.name as body,
                 CASE crmpc.direction
                     WHEN 'inbound' THEN 'in' ELSE 'out'
                     END
                 AS direction,
                 crmpc.id as phone_id,
-                NULL::Integer as email_id,
-                NULL::Integer as message_id,
-                NULL::Integer as paper_id
+                0 as email_id,
+                0 as message_id,
+                0 as paper_id
                 FROM "crm_phonecall" as crmpc
+                WHERE crmpc.partner_id = %s
                 )
     -- outgoing e-mails
-            UNION ALL (
+            UNION (
               SELECT
-                mail.id AS res_id,
                 'Email' as communication_type,
                 m.date as communication_date,
                 rel.res_partner_id as partner_id,
                 m.subject as subject,
+                m.body,
                 'out' AS direction,
-                NULL::Integer as phone_id,
+                0 as phone_id,
                 mail.id as email_id,
-                NULL::Integer as message_id,
-                NULL::Integer as paper_id
+                0 as message_id,
+                0 as paper_id
                 FROM "mail_mail" as mail
                 JOIN mail_message m ON mail.mail_message_id = m.id
                 JOIN mail_mail_res_partner_rel rel
                 ON rel.mail_mail_id = mail.id
-                WHERE mail.state IN ('sent', 'received')
+                WHERE mail.state = ANY (ARRAY ['sent', 'received'])
+                AND rel.res_partner_id = %s
                 )
     -- incoming messages from partners
-            UNION ALL (
+            UNION (
               SELECT
-                m.id AS res_id,
                 'Email' as communication_type,
                 m.date as communication_date,
                 m.author_id as partner_id,
                 m.subject as subject,
+                m.body,
                 'in' AS direction,
-                NULL::Integer as phone_id,
-                NULL::Integer as email_id,
+                0 as phone_id,
+                0 as email_id,
                 m.id as message_id,
-                NULL::Integer as paper_id
+                0 as paper_id
                 FROM "mail_message" as m
-                )) sub )
-        ORDER BY communication_date desc
-                """ % self._table)
+                WHERE m.subject IS NOT NULL
+                AND m.message_type = 'email'
+                AND m.author_id = %s
+                )
+                """, [partner_id] * 4)
+        for row in self.env.cr.dictfetchall():
+            self.create(row)
+        return True
+
+    def open_object(self):
+        """
+        TODO Implement and add icon in tree view
+        Used for opening the related object directly from tree view.
+        :return: action for opening the related object
+        """
+        self.ensure_one()
+        return True
