b25be41b64b393154b471c92b961f6555fa096d1
Joel Vaucher <jvaucher@compassion.ch>
2019-04-02 14:07:54 +0200

    CO-2378 Prévisualisation mail template
    tile Settings -> menu technical -> template -> preview
    
    tile Sponsorships -> menu Communication -> communication rule ->
    watch a revision (eye button) -> preview

---
+++ partner_communication/wizards/email_template_preview_view.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2019 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Joel Vaucher <jvaucher@compassion.ch>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+from odoo import models, api, fields
+
+
+class TemplatePreview(models.TransientModel):
+    _inherit = "email_template.preview"
+
+    res_id = fields.Reference(selection=lambda s: s._reference_models(),
+                              default=lambda s: s._default_model())
+
+    @api.model
+    def _reference_models(self):
+        template = self.env['mail.template'].browse(
+            self._context.get('active_id')
+        )
+        domain = [('model', '=', template.model)]
+        models = self.env['ir.model'].search(domain)
+        return [(model.model, model.name) for model in models]
+
+    @api.model
+    def _default_model(self):
+        recs = self._reference_models()
+        model = recs[0][0]
+        specific_domain = {
+            'res.partner': []
+        }
+        domain = specific_domain.get(model) or [('partner_id', '!=', False)]
+        return model + ',' + str(self.env[model].search(domain, limit=1).id)
+
+    @api.onchange('res_id')
+    @api.multi
+    def on_change_res_id(self):
+        return super(TemplatePreview, self).on_change_res_id()
