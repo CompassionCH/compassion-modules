44a2851cdbe5134240c0ed37b9a208be5d4c9b36
Unknown <eicher31@hotmail.com>
2019-04-05 09:22:08 +0200

    CO-2491 Fix update message linked to request

---
+++ crm_request/migrations/10.0.1.2.0/post-migration.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#    Copyright (C) 2019 Compassion CH
+#    @author: Stephane Eicher <seicher@compassion.ch>
+
+def migrate(cr, version):
+    if not version:
+        return
+
+    # Remove all alias that was created by
+    cr.execute(""" UPDATE crm_claim SET email_origin = email_from; """)
+    cr.execute(""" DELETE FROM res_partner WHERE type = 'email_alias' 
+    and email = 'xivo@xivo2016.sokl.ch'; """)
