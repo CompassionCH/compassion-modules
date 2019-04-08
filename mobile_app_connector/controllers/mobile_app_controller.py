fe932d4ae816cd3c25b12385005e63393cd2fd10
Jonathan Tarabbia <j.tarabbia@gmail.com>
2019-04-08 16:42:06 +0200

    Fix giving tiles

--- mobile_app_connector/controllers/mobile_app_controller.py
+++ mobile_app_connector/controllers/mobile_app_controller.py
@@ -99,6 +99,87 @@ class RestController(http.Controller):
         res = hero_mapping.get_connect_data(hero)
         return [res]
 
+    @http.route('/mobile-app-api/donation_type/', type='json',
+                auth='public', methods=['GET'])
+    def get_donation_type(self, view=None, **parameters):
+        return [
+            {
+                "DisplayOrder": "1",
+                "FundName": "Different Path Appeal",
+                "Id": 45
+            },
+            {
+                "DisplayOrder": "2",
+                "FundName": "Health Response",
+                "Id": 16
+            },
+            {
+                "DisplayOrder": "4",
+                "FundName": "Child Survival Programme",
+                "Id": 31
+            },
+            {
+                "DisplayOrder": "5",
+                "FundName": "Respond",
+                "Id": 15
+            },
+            {
+                "DisplayOrder": "6",
+                "FundName": "General Fund",
+                "Id": 2
+            },
+            {
+                "DisplayOrder": "8",
+                "FundName": "The Disaster Fund",
+                "Id": 3
+            },
+            {
+                "DisplayOrder": "9",
+                "FundName": "Education Response",
+                "Id": 17
+            },
+            {
+                "DisplayOrder": "10",
+                "FundName": "Stability Response",
+                "Id": 18
+            },
+            {
+                "DisplayOrder": "11",
+                "FundName": "Child Protection Response",
+                "Id": 19
+            },
+            {
+                "DisplayOrder": "21",
+                "FundName": "Christmas Appeal",
+                "Id": 26
+            },
+            {
+                "DisplayOrder": "26",
+                "FundName": "Tours & Visits Payment",
+                "Id": 6
+            },
+            {
+                "DisplayOrder": "105",
+                "FundName": "Ethiopia Drought",
+                "Id": 37
+            },
+            {
+                "DisplayOrder": "362",
+                "FundName": "Sponsorship Plus",
+                "Id": 32
+            },
+            {
+                "DisplayOrder": "364",
+                "FundName": "Water and Sanitation",
+                "Id": 38
+            },
+            {
+                "DisplayOrder": "522",
+                "FundName": "Brazil Handbook",
+                "Id": 43
+            }
+        ]
+
     @http.route('/mobile-app-api/correspondence/letter_pdf',
                 type='http', auth='user', methods=['GET'])
     def download_pdf(self, **parameters):
