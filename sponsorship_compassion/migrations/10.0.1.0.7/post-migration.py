9e08a84b0a69c661dd61d180ae559b74b063d93b
Emanuel Cino <ecino@compassion.ch>
2019-04-09 15:00:03 +0200

    FIX flake8
    - fix migration of partners

--- sponsorship_compassion/migrations/10.0.1.0.7/post-migration.py
+++ sponsorship_compassion/migrations/10.0.1.0.7/post-migration.py
@@ -8,7 +8,10 @@
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
+import logging
 from openupgradelib import openupgrade
+
+_logger = logging.getLogger(__name__)
 
 
 @openupgrade.migrate(use_env=True)
@@ -16,9 +19,11 @@ def migrate(env, version):
     if not version:
         return
 
-    partners = env['res.partner'].search(
+    partners = env['res.partner'].search_count(
             [('global_id', '!=', False),
-             ('partner.title.gender', '!=', False),
-             ('partner.title.name', 'not in',
-                 ['Madam', 'Mister', 'Misters', 'Ladies'])])
-    partners.upsert_constituent()
+             ('title.gender', '!=', False),
+             ('title.name', 'not in', [
+                 'Madam', 'Mister', 'Misters', 'Ladies'])])
+    _logger.warning("Found %s partners to migrate gender on GMC. Don't forget "
+                    "to call upsert_constituent method on those.", partners)
+    # partners.upsert_constituent()
