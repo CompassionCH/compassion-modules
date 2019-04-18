96462a4e65d9ea281587a244e7e1771dc22265eb
Jonathan Tarabbia <j.tarabbia@gmail.com>
2019-04-18 09:35:34 +0200

    AP-53 - Fix date format for IOS

--- mobile_app_connector/mappings/compassion_correspondence_mapping.py
+++ mobile_app_connector/mappings/compassion_correspondence_mapping.py
@@ -9,6 +9,7 @@
 ##############################################################################
 
 from odoo import _
+import datetime
 from odoo.addons.message_center_compassion.mappings.base_mapping import \
     OnrampMapping
 
@@ -44,7 +45,16 @@ class FromLetterMapping(OnrampMapping):
         'Message': 'original_text',
     }
 
-    FIELDS_TO_SUBMIT = {k: None for k, v in CONNECT_MAPPING.iteritems()}
+    FIELDS_TO_SUBMIT = {
+        'CorrespondenceID': None,
+        'Date': lambda date: datetime.datetime.strptime(
+            date, '%Y-%m-%d %H:%M:%S').strftime('%d-%m-%Y %H:%M:%S'),
+        'FileID': None,  # Can be a string? int in example
+        'FileName': None,
+        'Message': None,
+    }
+
+    # FIELDS_TO_SUBMIT = {k: None for k, v in CONNECT_MAPPING.iteritems() if v}
 
     def get_connect_data(self, odoo_object, fields_to_submit=None):
         mapped = super(FromLetterMapping, self) \
