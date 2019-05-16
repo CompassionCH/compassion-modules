9d9819ba6d61a70726d2a0306e57abb9b1f584ec
Nicolas Badoux <n.badoux@hotmail.com>
2019-05-16 14:59:36 +0200

    AP-73 Firebase Connector

---
+++ firebase_connector/__manifest__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#       ______ Releasing children from poverty      _
+#      / ____/___  ____ ___  ____  ____ ___________(_)___  ____
+#     / /   / __ \/ __ `__ \/ __ \/ __ `/ ___/ ___/ / __ \/ __ \
+#    / /___/ /_/ / / / / / / /_/ / /_/ (__  |__  ) / /_/ / / / /
+#    \____/\____/_/ /_/ /_/ .___/\__,_/____/____/_/\____/_/ /_/
+#                        /_/
+#                            in Jesus' name
+#
+#    Copyright (C) 2019 Compassion CH (http://www.compassion.ch)
+#    @author: Nicolas Badoux <n.badoux@hotmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+# pylint: disable=C8101
+{
+    'name': 'Compassion EU Firebase Connector',
+    'version': '10.0.0.0.0',
+    'category': 'Other',
+    'author': 'Compassion CH',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CompassionCH/compassion-modules/tree/10.0',
+    'depends': [
+    ],
+    'external_dependencies': {
+        'python': ['firebase_admin'],
+    },
+    'data': [
+        'views/firebase_registration.xml',
+    ],
+    'demo': [
+    ],
+    'development_status': 'Beta',
+    'installable': True,
+    'auto_install': False,
+}
---
+++ firebase_connector/models/firebase_registration.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2019 Compassion CH (http://www.compassion.ch)
+#    @author: Nicolas Badoux <n.badoux@hotmail.com>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+import logging
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import messaging
+
+from odoo import api, models, fields
+from odoo.tools import config
+
+
+_logger = logging.getLogger(__name__)
+
+
+class FirebaseRegistration(models.Model):
+    """
+    The represent a device that we can send push notification to. It is
+    characterized by the Firebase token and an optional partner.
+    It provides function to send messages to the device.
+    """
+
+    _name = 'firebase.registration'
+    _description = 'Device registered with Firebase Cloud Messaging'
+
+    registration_id = fields.Char(required=True,
+                                  string="Firebase Registration ID")
+    partner_id = fields.Many2one('res.partner', string="Partner")
+    partner_name = fields.Char(related='partner_id.name', readonly=True)
+    firebase_credentials =\
+        credentials.Certificate(config.get('firebase_api_key'))
+    firebase_app = firebase_admin.initialize_app(
+        credential=firebase_credentials)
+
+    _sql_constraints = [
+                     ('firebase_id_unique',
+                      'UNIQUE(registration_id)',
+                      'Firebase registration ID should be unique')
+    ]
+
+    def name_get(self):
+        res = []
+        for record in self:
+            res.append((record.id, record.registration_id))
+        return res
+
+    def send_message_from_interface(self, context):
+        """
+        Allows to send a notification from Odoo web interface.
+        Button is in the form view but generally hidden.
+        :param context: Context from the view
+        :return: None
+        """
+
+        self.send_message(context.get('message_title'), context.get('message_body'))
+
+    @api.multi
+    def send_message(self, message_title, message_body, data=None):
+        """
+        Send a notification to the device registered with this firebase id.
+        If the firebase id is not in use anymore, we remove the registration
+        from the database.
+        :param message_title: Title of the notification
+        :param message_body: Content of the notification
+        :param data: Data segment of a Firebase message (see the docs)
+        :return: None
+        """
+
+        notif = messaging.Notification(title=message_title, body=message_body)
+
+        for firebase_id in self:
+            message = messaging.Message(notification=notif,
+                                        data=data,
+                                        token=firebase_id.registration_id)
+            try:
+                messaging.send(message=message)
+            except messaging.ApiCallError as e:
+                logging.debug(
+                    "A device is not reachable from Firebase, unlinking."
+                    "Firebase ID: %s" % firebase_id)
+                if e.code == 'registration-token-not-registered':
+                    # app uninstalled or token renewed
+                    firebase_id.unlink()
+                else:
+                    raise e
