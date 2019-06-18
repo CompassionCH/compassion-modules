c8ff7a1257e2922a4f5a5be6ff8993b35205cc55
Theyiot <pprizee@gmail.com>
2019-06-18 08:53:54 +0200

    Quick fix: 'are' were displayed in a mail subject even if it refered to only 1 sms

--- sms_sponsorship/models/sms_child_request.py
+++ sms_sponsorship/models/sms_child_request.py
@@ -361,8 +361,8 @@ class SmsChildRequest(models.Model):
             self.message_post(
                 body=_("{} partner(s) have ongoing SMS Sponsorship").format(
                     nb_sms_requests),
-                subject=_("{} SMS Sponsorship are ongoing").format(
-                    nb_sms_requests),
+                subject=_("{} SMS Sponsorship {} ongoing").format(
+                    nb_sms_requests, 'is' if nb_sms_requests <= 1 else 'are'),
                 partner_ids=notify_ids,
                 type='comment',
                 subtype='mail.mt_comment',
