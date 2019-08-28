8cb5834e970c5d985c635243432e168175aa15f0
Simon <hayoz.simon@bluewin.ch>
2019-08-28 16:23:42 +0200

    AP-253 Added route for privacy statement

--- mobile_app_connector/controllers/mobile_app_controller.py
+++ mobile_app_connector/controllers/mobile_app_controller.py
@@ -80,6 +80,18 @@ class RestController(http.Controller):
         return request.env.get('frequently.asked.questions').mobile_get_faq(
             **parameters)
 
+    @http.route(
+        '/mobile-app-api/privacy.statement.agreement/get_privacy_notice',
+                type='json', auth='public', methods=['GET'])
+    def mobile_app_get_privacy_statement(self, **parameters):
+        """
+        Privacy statement entry point for not logged in users
+        :param parameters: all other optional parameters sent by the request
+        :return: json data for mobile app
+        """
+        return request.env.get('privacy.statement.agreement').\
+            mobile_get_privacy_notice(**parameters)
+
     @http.route(['/mobile-app-api/<string:model>/<string:method>',
                  '/mobile-app-api/<string:model>/<string:method>/'
                  '<request_code>'],
