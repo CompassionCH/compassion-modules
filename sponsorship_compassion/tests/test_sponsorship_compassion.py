011bdb42e9b52aaa2474fba308b1a0623d3945e0
nbornand <nicolas74@gmail.com>
2019-10-04 16:20:53 +0200

    Add a default thankyou.config for sponsorship_compassion test.

--- sponsorship_compassion/tests/test_sponsorship_compassion.py
+++ sponsorship_compassion/tests/test_sponsorship_compassion.py
@@ -40,6 +40,10 @@ class BaseSponsorshipTest(BaseContractCompassionTest):
         self.product = self.env['product.product'].search([
             ('name', '=', 'Sponsorship')
         ], limit=1)
+        self.env['thankyou.config'].create({
+            'min_donation_amount': 50.0,
+            'send_mode': 'auto_digital_only',
+        })
 
     @mock.patch(mock_update_hold)
     @mock.patch(mock_get_infos)
