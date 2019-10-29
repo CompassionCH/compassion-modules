b512bdbb45182753afc3bccb804b0e0e1679d770
Quentin Gigon <gigon.quentin@gmail.com>
2019-10-29 13:56:53 +0100

    working on search_filter_ids

--- intervention_compassion/__init__.py
+++ intervention_compassion/__init__.py
@@ -8,6 +8,22 @@
 #
 ##############################################################################
 
-from message_center_compassion import load_mappings
 from . import models
 from . import wizards
+from odoo.addons.message_center_compassion.tools.load_mappings import \
+    load_mapping_files
+
+
+def load_mappings(cr, registry):
+    path = 'intervention_compassion/static/mappings/'
+    files = [
+        'commitment_mapping.json',
+        'global_intervention_mapping.json',
+        'global_intervention_search_mapping.json',
+        'hold_create_mapping.json',
+        'hold_cancel_mapping.json',
+        'hold_create_mapping.json',
+        'intervention_mapping.json',
+        'intervention_search_mapping.json']
+
+    load_mapping_files(cr, path, files)
