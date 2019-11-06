d4b559080081a8de709b439b74061e4391616a45
Emanuel Cino <ecino@compassion.ch>
2019-11-06 17:06:01 +0100

    CO-2807 Small fixes

--- thankyou_letters/models/partner_communication.py
+++ thankyou_letters/models/partner_communication.py
@@ -33,16 +33,19 @@ class PartnerCommunication(models.Model):
         domain=[('type', '=', 'sentence')])
     success_sentence = fields.Text(related='success_sentence_id.body_text')
     add_success_story = fields.Boolean(related='config_id.add_success_story')
-    amount = fields.Float(string='Amount', compute='_compute_donation_amount',
-                          store=True)
+    amount = fields.Float(compute='_compute_donation_amount', store=True)
 
     @api.multi
     @api.depends('object_ids')
     def _compute_donation_amount(self):
         for communication in self:
-            model = communication.config_id.model
-            if model == 'account.invoice.line':
-                invoice_lines = communication.get_objects()
+            if communication.model == 'account.invoice.line':
+                try:
+                    invoice_lines = communication.get_objects().exists()
+                    if not invoice_lines:
+                        continue
+                except ValueError:
+                    continue
                 communication.amount = sum(invoice_lines
                                            .mapped('price_subtotal'))
 
