87a33201f28f5f3de14af8c961fb41db3ee10e9e
josueBulle <cyril.favre@epfl.ch>
2019-11-11 12:56:03 +0100

    AP-283 orders letters in hub

--- mobile_app_connector/models/compassion_correspondence.py
+++ mobile_app_connector/models/compassion_correspondence.py
@@ -38,15 +38,16 @@ class CompassionCorrespondence(models.Model):
             data = []
             for letter in self:
                 data.append(mapping.get_connect_data(letter))
-        unread_recently = not(self.email_read and self.email_read <
-                              fields.Date.to_string(
-                                  date.today() + timedelta(days=3)))
+
+        order_date = self.sent_date or self.status_date
+        if self.direction == 'Supporter to beneficiary':
+            order_date = self.scanned_date
+
         return {
             'Child': child.get_app_json_no_wrap(),
             wrapper: data,
-            'OrderDate': self.sent_date or self.status_date,
-            'UnReadRecently': unread_recently,
-            }
+            'OrderDate': order_date
+        }
 
     @api.model
     def mobile_post_letter(self, json_data, **parameters):
