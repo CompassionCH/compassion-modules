076ae8b63857a011ea1926b451a3478380075a25
Cyril Favre <17401810+josueBulle@users.noreply.github.com>
2019-12-06 09:39:12 +0100

    CO-2848 sms_sponsorship migration v11

--- sms_sponsorship/models/staff_notification_settings.py
+++ sms_sponsorship/models/staff_notification_settings.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Copyright (C) 2018 Compassion CH (http://www.compassion.ch)
@@ -13,7 +12,7 @@ from odoo import api, models, fields
 
 class StaffNotificationSettings(models.TransientModel):
     """ Settings configuration for any Notifications."""
-    _inherit = 'staff.notification.settings'
+    _inherit = 'res.config.settings'
 
     # Users to notify when new partner make a sponsorship by SMS
     new_partner_notify_ids = fields.Many2many(
@@ -30,11 +29,10 @@ class StaffNotificationSettings(models.TransientModel):
     def get_default_values(self, _fields):
 
         param_obj = self.env['ir.config_parameter']
-        res = super(StaffNotificationSettings, self).get_default_values(
-            _fields)
+        res = super().get_default_values(_fields)
         res['new_partner_notify_ids'] = False
         partners = param_obj.get_param(
             'sms_sponsorship.new_partner_notify_ids', False)
         if partners:
-            res['new_partner_notify_ids'] = map(int, partners.split(','))
+            res['new_partner_notify_ids'] = list(map(int, partners.split(',')))
         return res
