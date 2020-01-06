8530b914286a3dcff84edfe50e01a19033c80151
qgigon <gigon.quentin@gmail.com>
2020-01-06 16:42:38 +0100

    redone the task in sql

--- crm_compassion/models/interaction_resume.py
+++ crm_compassion/models/interaction_resume.py
@@ -62,108 +62,117 @@ class InteractionResume(models.TransientModel):
             ('id', '=', partner_id)
         ])
         email_address = original_partner.email
-        partners_with_same_email = self.env['res.partner'].search([
+        partners_with_same_email_ids = self.env['res.partner'].search([
             ('email', '=', email_address)
-        ])
-        self.search([('partner_id', '=', partner_id)]).unlink()
+        ]).mapped("id")
+        s = []
+        for partner_id in partners_with_same_email_ids:
+            s.append(str(partner_id))
+        l = str(s)
+        # s = s[:-1]
+        count = len(partners_with_same_email_ids)
+        self.env.cr.execute("""
+                SELECT
+                        'Paper' as communication_type,
+                        pcj.sent_date as communication_date,
+                        COALESCE(p.contact_id, pcj.partner_id) AS partner_id,
+                        NULL AS email,
+                        COALESCE(c.name, pcj.subject) AS subject,
+                        REGEXP_REPLACE(pcj.body_html, '<img[^>]*>', '') AS body,
+                        'out' AS direction,
+                        0 as phone_id,
+                        0 as email_id,
+                        0 as message_id,
+                        pcj.id as paper_id,
+                        NULL as tracking_status
+                        FROM "partner_communication_job" as pcj
+                        JOIN res_partner p ON pcj.email_to = p.email
+                        FULL OUTER JOIN partner_communication_config c
+                            ON pcj.config_id = c.id
+                            AND c.name != 'Default communication'
+                        WHERE pcj.state = 'done'
+                        AND pcj.send_mode = 'physical'
+                        AND (p.contact_id in ({}) OR p.id in ({}))
+            -- phonecalls
+                    UNION (
+                      SELECT
+                        'Phone' as communication_type,
+                        crmpc.date as communication_date,
+                        COALESCE(p.contact_id, p.id) AS partner_id,
+                        NULL AS email,
+                        crmpc.name as subject,
+                        crmpc.name as body,
+                        CASE crmpc.direction
+                            WHEN 'inbound' THEN 'in' ELSE 'out'
+                            END
+                        AS direction,
+                        crmpc.id as phone_id,
+                        0 as email_id,
+                        0 as message_id,
+                        0 as paper_id,
+                        NULL as tracking_status
+                        FROM "crm_phonecall" as crmpc
+                        JOIN res_partner p ON crmpc.email_from = p.email
+                        WHERE (p.contact_id in ({}) OR p.id in ({}))
+                        )
+            -- outgoing e-mails
+                    UNION (
+                      SELECT
+                        'Email' as communication_type,
+                        m.date as communication_date,
+                        COALESCE(p.contact_id, p.id) AS partner_id,
+                        mt.recipient_address as email,
+                        COALESCE(config.name, m.subject) as subject,
+                        REGEXP_REPLACE(m.body, '<img[^>]*>', '') AS body,
+                        'out' AS direction,
+                        0 as phone_id,
+                        mail.id as email_id,
+                        0 as message_id,
+                        job.id as paper_id,
+                        COALESCE(mt.state, 'error') as tracking_status
+                        FROM "mail_mail" as mail
+                        JOIN mail_message m ON mail.mail_message_id = m.id
+                        JOIN mail_mail_res_partner_rel rel
+                        ON rel.mail_mail_id = mail.id
+                        FULL OUTER JOIN mail_tracking_email mt ON mail.id = mt.mail_id
+                        JOIN res_partner p ON mail.email_to = p.email
+                        FULL OUTER JOIN partner_communication_job job
+                            ON job.email_id = mail.id
+                        FULL OUTER JOIN partner_communication_config config
+                            ON job.config_id = config.id
+                            AND config.name != 'Default communication'
+                        WHERE mail.state = ANY (ARRAY ['sent', 'received'])
+                        AND (p.contact_id in ({}) OR p.id in ({}))
+                        )
+            -- incoming messages from partners
+                    UNION (
+                      SELECT
+                        'Email' as communication_type,
+                        m.date as communication_date,
+                        COALESCE(p.contact_id, p.id) AS partner_id,
+                        m.email_from as email,
+                        m.subject as subject,
+                        REGEXP_REPLACE(m.body, '<img[^>]*>', '') AS body,
+                        'in' AS direction,
+                        0 as phone_id,
+                        0 as email_id,
+                        m.id as message_id,
+                        0 as paper_id,
+                        NULL as tracking_status
+                        FROM "mail_message" as m
+                        JOIN res_partner p ON m.email_from = p.email
+                        WHERE m.subject IS NOT NULL
+                        AND m.message_type = 'email'
+                        AND (p.contact_id in ({}) OR p.id in ({}))
+                        )
 
-        default_communication_config = self.env['partner.communication.config'].search([
-            ('name', '=', "Default communication")
-        ])
+                        """.format(l.strip('[]'), l.strip('[]'), l.strip('[]'), l.strip('[]'),
+                                   l.strip('[]'), l.strip('[]'), l.strip('[]'), l.strip('[]')))
 
-        # Paper communications
-        paper_communications = self.env['partner.communication.job'].search([
-            ('state', '=', 'done'),
-            ('send_mode', '=', 'physical'),
-            ('config_id', '=', default_communication_config.id),
-            ('partner_id', 'in', partners_with_same_email.ids)
-        ])
-        for paper_communication in paper_communications:
-            self.create_interaction_resume(
-                "Phone",
-                paper_communication.sent_date,
-                paper_communication.partner_id.id,
-                None,
-                paper_communication.subject,
-                paper_communication.body_html,
-                "out",
-                0,
-                0,
-                0,
-                paper_communication.id,
-                None)
-
-        # Phone communications
-        phone_communications = self.env['crm.phonecall'].search([
-            ('partner_id', 'in', partners_with_same_email.ids)
-        ])
-        for phone_communication in phone_communications:
-            direction = 'out'
-            if phone_communication.direction == "inbound":
-                direction = 'in'
-            self.create_interaction_resume(
-                "Phone",
-                phone_communication.date,
-                phone_communication.partner_id.id,
-                None,
-                phone_communication.name,
-                phone_communication.name,
-                direction,
-                phone_communication.id,
-                0,
-                0,
-                0,
-                None)
-
-        # Outgoing email communications
-        outgoing_messages = self.env['mail.message'].search([
-            ('author_id', 'in', partners_with_same_email.ids),
-            ('message_type', '=', 'email')
-        ])
-        outgoing_emails = self.env['mail.mail'].search([
-            ('mail_message_id', 'in', outgoing_messages.ids)
-        ])
-        for email in outgoing_emails:
-            mt = self.env['mail.tracking.email'].search([
-                ('mail_id', '=', email.id)
-            ])
-            job = self.env['partner.communication.job'].search([
-                ('email_id', '=', email.id)
-            ])
-            self.create_interaction_resume(
-                "Email",
-                email.date,
-                email.mail_message_id.author_id.id,
-                mt.recipient_address,
-                email.subject,
-                email.body,
-                "out",
-                0,
-                email.id,
-                0,
-                job.id,
-                mt.state or "error")
-
-        # Incoming email communications
-        incoming_messages = self.env['mail.message'].search([
-            ('author_id', 'in', partners_with_same_email.ids),
-            ('subject', '!=', None),
-            ('message_type', '=', 'email')
-        ])
-        for message in incoming_messages:
-            self.create_interaction_resume(
-                "Email",
-                message.date,
-                message.author_id.id,
-                message.author_id.email,
-                message.subject,
-                message.body,
-                "in",
-                0,
-                0,
-                message.id,
-                0,
-                None)
+        for row in self.env.cr.dictfetchall():
+            # ensure that "Example <ex@exmaple.com>" is correctly printed
+            row['email'] = html_sanitize(row['email'])
+            self.create(row)
 
         return True
 
@@ -218,99 +227,3 @@ class InteractionResume(models.TransientModel):
 
 
 
-
-# self.env.cr.execute("""
-#         SELECT
-#                 'Paper' as communication_type,
-#                 pcj.sent_date as communication_date,
-#                 COALESCE(p.contact_id, pcj.partner_id) AS partner_id,
-#                 NULL AS email,
-#                 COALESCE(c.name, pcj.subject) AS subject,
-#                 REGEXP_REPLACE(pcj.body_html, '<img[^>]*>', '') AS body,
-#                 'out' AS direction,
-#                 0 as phone_id,
-#                 0 as email_id,
-#                 0 as message_id,
-#                 pcj.id as paper_id,
-#                 NULL as tracking_status
-#                 FROM "partner_communication_job" as pcj
-#                 JOIN res_partner p ON pcj.partner_id = p.id
-#                 FULL OUTER JOIN partner_communication_config c
-#                     ON pcj.config_id = c.id
-#                     AND c.name != 'Default communication'
-#                 WHERE pcj.state = 'done'
-#                 AND pcj.send_mode = 'physical'
-#                 AND (p.contact_id = %s OR p.id = %s)
-#     -- phonecalls
-#             UNION (
-#               SELECT
-#                 'Phone' as communication_type,
-#                 crmpc.date as communication_date,
-#                 COALESCE(p.contact_id, p.id) AS partner_id,
-#                 NULL AS email,
-#                 crmpc.name as subject,
-#                 crmpc.name as body,
-#                 CASE crmpc.direction
-#                     WHEN 'inbound' THEN 'in' ELSE 'out'
-#                     END
-#                 AS direction,
-#                 crmpc.id as phone_id,
-#                 0 as email_id,
-#                 0 as message_id,
-#                 0 as paper_id,
-#                 NULL as tracking_status
-#                 FROM "crm_phonecall" as crmpc
-#                 JOIN res_partner p ON crmpc.partner_id = p.id
-#                 WHERE (p.contact_id = %s OR p.id = %s)
-#                 )
-#     -- outgoing e-mails
-#             UNION (
-#               SELECT
-#                 'Email' as communication_type,
-#                 m.date as communication_date,
-#                 COALESCE(p.contact_id, p.id) AS partner_id,
-#                 mt.recipient_address as email,
-#                 COALESCE(config.name, m.subject) as subject,
-#                 REGEXP_REPLACE(m.body, '<img[^>]*>', '') AS body,
-#                 'out' AS direction,
-#                 0 as phone_id,
-#                 mail.id as email_id,
-#                 0 as message_id,
-#                 job.id as paper_id,
-#                 COALESCE(mt.state, 'error') as tracking_status
-#                 FROM "mail_mail" as mail
-#                 JOIN mail_message m ON mail.mail_message_id = m.id
-#                 JOIN mail_mail_res_partner_rel rel
-#                 ON rel.mail_mail_id = mail.id
-#                 FULL OUTER JOIN mail_tracking_email mt ON mail.id = mt.mail_id
-#                 JOIN res_partner p ON rel.res_partner_id = p.id
-#                 FULL OUTER JOIN partner_communication_job job
-#                     ON job.email_id = mail.id
-#                 FULL OUTER JOIN partner_communication_config config
-#                     ON job.config_id = config.id
-#                     AND config.name != 'Default communication'
-#                 WHERE mail.state = ANY (ARRAY ['sent', 'received'])
-#                 AND (p.contact_id = %s OR p.id = %s)
-#                 )
-#     -- incoming messages from partners
-#             UNION (
-#               SELECT
-#                 'Email' as communication_type,
-#                 m.date as communication_date,
-#                 COALESCE(p.contact_id, p.id) AS partner_id,
-#                 m.email_from as email,
-#                 m.subject as subject,
-#                 REGEXP_REPLACE(m.body, '<img[^>]*>', '') AS body,
-#                 'in' AS direction,
-#                 0 as phone_id,
-#                 0 as email_id,
-#                 m.id as message_id,
-#                 0 as paper_id,
-#                 NULL as tracking_status
-#                 FROM "mail_message" as m
-#                 JOIN res_partner p ON m.author_id = p.id
-#                 WHERE m.subject IS NOT NULL
-#                 AND m.message_type = 'email'
-#                 AND (p.contact_id = %s OR p.id = %s)
-#                 )
-#                 """, [partner_id] * 8)
