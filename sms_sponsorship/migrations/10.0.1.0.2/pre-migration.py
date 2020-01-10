a484b52c5b7785245d51cb4537499d223cb47089
Nooka10 <mails@benoitschopfer.ch>
2020-01-10 12:06:41 +0100

    CO-2950: SMS request badge now count only the reuqests made from SMS (not those made from the app)

---
+++ sms_sponsorship/migrations/10.0.1.0.2/pre-migration.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2018 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Emanuel Cino <ecino@compassion.ch>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+
+def migrate(cr, version):
+    if not version:
+        return
+
+    # Update CRON function
+    cr.execute(
+        """
+        UPDATE sms_child_request
+        SET source = 'SMS'
+        WHERE source IS NULL;
+        """
+    )
