6b995c2eeea13158cb607426b12aa77b9ffef3ee
qgigon <gigon.quentin@gmail.com>
2020-01-22 08:56:38 +0100

    CO-2854 field email_from is now the email of current user or assigned user

--- partner_communication/models/communication_job.py
+++ partner_communication/models/communication_job.py
@@ -666,7 +666,8 @@ class CommunicationJob(models.Model):
                 'attachment_ids': [(6, 0, self.ir_attachment_ids.ids)],
                 'auto_delete': False,
                 'reply_to': (self.email_template_id.reply_to or
-                             self.user_id.email)
+                             self.user_id.email),
+                'email_from': self.env['res.users'].browse(self.env.uid).email
             }
             if self.email_to:
                 # Replace partner e-mail by specified address
