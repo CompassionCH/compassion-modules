663f8b9f3aa2249a69e7474386274b6ece0d1afc
qgigon <gigon.quentin@gmail.com>
2020-02-10 14:40:35 +0100

    fixed config settings

--- mobile_app_connector/wizards/app_settings.py
+++ mobile_app_connector/wizards/app_settings.py
@@ -12,7 +12,6 @@ from odoo import api, models, fields
 
 
 class DemandPlanningSettings(models.TransientModel):
-    _name = 'mobile.app.settings'
     _inherit = 'res.config.settings'
 
     default_s2b_template_id = fields.Many2one(
@@ -24,16 +23,23 @@ class DemandPlanningSettings(models.TransientModel):
             'mobile_app_connector.s2b_template',
             str(self.default_s2b_template_id.id))
 
+    @api.multi
+    def set_values(self):
+        super().set_values()
+        self.env['ir.config_parameter'].set_param(
+            'mobile_app_connector.s2b_template',
+            str(self.default_s2b_template_id.id))
+
     @api.model
-    def get_default_values(self, _fields):
+    def get_values(self):
+        res = super().get_values()
         param_obj = self.env['ir.config_parameter']
         s2b_template_id = int(param_obj.get_param(
             'mobile_app_connector.s2b_template', '0'))
-        all_values = {
+        res.update({
             'default_s2b_template_id': s2b_template_id,
-        }
-
-        return {key: all_values.get(key, 0) for key in _fields}
+        })
+        return res
 
     @api.model
     def get_param(self, param):
