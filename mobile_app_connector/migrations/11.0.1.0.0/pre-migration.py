96850a0c71936d93fc28fbece684e072d9693025
qgigon <gigon.quentin@gmail.com>
2020-02-11 09:02:34 +0100

    fix for pre-migration script

--- mobile_app_connector/migrations/11.0.1.0.0/pre-migration.py
+++ mobile_app_connector/migrations/11.0.1.0.0/pre-migration.py
@@ -1,12 +1,13 @@
 ##############################################################################
 #
-#    Copyright (C) 2018 Compassion CH (http://www.compassion.ch)
-#    @author: Nathan Flückiger <nathan.fluckiger@hotmail.ch>
+#    Copyright (C) 2020 Compassion CH (http://www.compassion.ch)
+#    @author: Quentin Gigon <gigon.quentin@gmail.com>
 #
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
 from openupgradelib import openupgrade
+from message_center_compassion.tools import load_mappings
 
 
 @openupgrade.migrate(use_env=True)
@@ -14,4 +15,17 @@ def migrate(env, version):
     if not version:
         return
 
-    env['import.json.mapping'].python_install_mapping()
+    path = 'mobile_app_connector/static/mappings/'
+    files = [
+        'app_banner_mapping.json',
+        'compassion_child_mapping.json',
+        'compassion_donation_mapping.json',
+        'compassion_login_mapping.json',
+        'compassion_mobile_correspondence_mapping.json',
+        'compassion_project_mapping.json',
+        'from_letter_mapping.json',
+        'mobile_app_tile_mapping.json',
+        'mobile_child_picture_mapping.json',
+        'wp_post_mapping.json',
+    ]
+    load_mappings.load_mapping_files(env.cr, path, files)
