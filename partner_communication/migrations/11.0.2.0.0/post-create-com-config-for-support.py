4e3812190dc3af7b535b1fff8e28ed83acb576c3
Sylvain Losey <sylvainlosey@gmail.com>
2020-02-18 14:36:18 +0100

    Black

--- partner_communication/migrations/11.0.2.0.0/post-create-com-config-for-support.py
+++ partner_communication/migrations/11.0.2.0.0/post-create-com-config-for-support.py
@@ -7,15 +7,17 @@ def migrate(env, version):
         return
 
     # Get all support mail.templates
-    support_templates = env['mail.template'].search([('model', '=', 'crm.claim')])
+    support_templates = env["mail.template"].search([("model", "=", "crm.claim")])
 
-    crm_claim_model = env['ir.model'].search([('model', '=', 'crm.claim')]).id
+    crm_claim_model = env["ir.model"].search([("model", "=", "crm.claim")]).id
 
     # Create a communication rule for each of them
     for template in support_templates:
-        env['partner.communication.config'].create({
-            'name': template['name'],
-            'send_mode': 'digital_only',
-            'model_id': crm_claim_model,
-            'email_template_id': template.id
-        })
+        env["partner.communication.config"].create(
+            {
+                "name": template["name"],
+                "send_mode": "digital_only",
+                "model_id": crm_claim_model,
+                "email_template_id": template.id,
+            }
+        )
