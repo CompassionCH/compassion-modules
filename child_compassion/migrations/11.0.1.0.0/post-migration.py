62c6805778e5285a010e383142ad88f91f440a7f
Emanuel Cino <ecino@compassion.ch>
2020-02-25 16:11:30 +0100

    Further fixes for migration scripts

--- child_compassion/migrations/11.0.1.0.0/post-migration.py
+++ child_compassion/migrations/11.0.1.0.0/post-migration.py
@@ -8,10 +8,38 @@
 ##############################################################################
 from openupgradelib import openupgrade
 
+from odoo.addons.message_center_compassion.tools.load_mappings import \
+    load_mapping_files
+
 
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
     if not version:
         return
 
-    env['import.json.mapping'].python_install_mapping()
+    path = 'child_compassion/static/mappings/'
+    files = [
+        'household_member.json',
+        'household.json',
+        'child.json',
+        'child_cdpr.json',
+        'child_lifecycle.json',
+        'child_note.json',
+        'childpool_advanced_search.json',
+        'childpool_rich_mix.json',
+        'childpool_simple_search.json',
+        'childpool_search_response.json',
+        'childpool_search_fields.json',
+        'demand_planning.json',
+        'disaster_alert.json',
+        'fcp.json',
+        'fcp_lifecycle.json',
+        'field_office.json',
+        'global_child.json',
+        'hold_create.json',
+        'hold_release.json',
+        'reservation_cancel.json',
+        'reservation_child.json',
+        'reservation_project.json',
+    ]
+    load_mapping_files(env.cr, path, files)
