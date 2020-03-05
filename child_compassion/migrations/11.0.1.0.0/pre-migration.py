a1e64ccb0bf2990343cc86f27fc2b315c1020642
Emanuel Cino <ecino@compassion.ch>
2020-03-05 10:26:27 +0100

    FIX migration of settings and worflows
    - FIX sds followers settings

--- child_compassion/migrations/11.0.1.0.0/pre-migration.py
+++ child_compassion/migrations/11.0.1.0.0/pre-migration.py
@@ -6,6 +6,7 @@
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
+from openupgradelib import openupgrade
 
 
 def migrate(cr, version):
@@ -17,4 +18,26 @@ def migrate(cr, version):
         UPDATE ir_model_data
         SET name = REGEXP_REPLACE(name, '(household_duty)(.)(_.*)', '\1\3')
         WHERE name like 'household_duty%';
+    """)
+
+    # Delete workflows
+    openupgrade.delete_model_workflow(cr, 'compassion.child', True)
+
+    # Remove foreign key of old settings tables
+    openupgrade.remove_tables_fks(cr, [
+        'staff_notification_settings',
+        'mobile_app_settings',
+        'demand_planning_settings',
+        'gifth_threshold_settings',
+        'sbc_settings',
+        'sds_follower_settings',
+        'availability_management_settings',
+    ])
+    # Drop relation tables of old settings tables
+    cr.execute("""
+        DROP TABLE res_partner_staff_notification_settings_rel;
+        DROP TABLE staff_disaster_notification_ids;
+        DROP TABLE staff_gift_notification_ids;
+        DROP TABLE invalid_mail_staff_notify_rel;
+        DROP TABLE staff_sms_notification_settings;
     """)
