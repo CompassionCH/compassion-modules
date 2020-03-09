c5b3ef9227e1874044fc281e1c620feadcaf0e3d
Emanuel Cino <ecino@compassion.ch>
2020-03-09 17:12:38 +0100

    Restore missing wizards

---
+++ crm_request/wizards/__init__.py
@@ -0,0 +1,1 @@
+from . import assign_request_wizard
--- partner_communication_revision/wizards/__init__.py
+++ partner_communication_revision/wizards/__init__.py
@@ -7,3 +7,10 @@
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
+
+from . import revision_preview
+from . import install_revisions
+from . import new_proposition_wizard
+from . import validate_revision_wizard
+from . import cancel_revision_wizard
+from . import submit_revision_wizard
