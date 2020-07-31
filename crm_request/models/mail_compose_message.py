16cee9ef53722cc0cfde29d0810de95b7d31f99c
Sylvain Losey <sylvainlosey@gmail.com>
2020-07-31 13:08:04 +0200

    CO-3195 / CO-3196 Add gift payment slips to support replies

--- crm_request/models/mail_compose_message.py
+++ crm_request/models/mail_compose_message.py
@@ -1,9 +1,11 @@
 # Copyright (C) 2018 Compassion CH
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
-from odoo.tools import append_content_to_html
+import base64
 
+from odoo import api, models, _
+from odoo.tools import append_content_to_html
+from odoo.addons.sponsorship_compassion.models.product_names import GIFT_REF
 
 class MailComposer(models.TransientModel):
     """
@@ -38,14 +40,61 @@ class MailComposer(models.TransientModel):
     @api.multi
     def onchange_template_id(self, template_id, composition_mode, model, res_id):
         """
-        Append the quote of previous e-mail to the body of the message.
+        - Append the quote of previous e-mail to the body of the message.
+        - Add gift payment slips for "Gift/Payment slip" and "Christmas Gift Fund"
         """
         result = super().onchange_template_id(
             template_id, composition_mode, model, res_id
         )
+
+        # Add reply qyote
         reply_quote = self.env.context.get("reply_quote")
         if reply_quote:
             result["value"]["body"] = append_content_to_html(
                 result["value"]["body"], "<br/><br/>" + reply_quote, plaintext=False
             )
+
+        # Clear previous attachments
+        self.attachment_ids = None
+
+        # Add gift payment slips
+        template = self.env["mail.template"].browse(template_id)
+        template_name = template.with_context(lang="en_US").name
+        partner = self.partner_ids[0]
+        sponsorships = partner.sponsorship_ids
+
+        if template_name == "Gift/Payment slip" and sponsorships:
+            refs = [GIFT_REF[0], GIFT_REF[1], GIFT_REF[2]]
+            report = self.env.ref("report_compassion.report_3bvr_gift_sponsorship")
+            payment_slips = self.get_payment_slips(refs, report, sponsorships.ids)
+            result["value"]["attachment_ids"] = payment_slips.ids
+
+        if template_name == "Christmas Gift Fund":
+            refs = ["gen_christmas"]
+            report = self.env.ref("report_compassion.report_bvr_fund")
+            payment_slips = self.get_payment_slips(refs, report, partner.id)
+            result["value"]["attachment_ids"] = payment_slips.ids
+
         return result
+
+    def get_payment_slips(self, refs, report, doc_ids):
+        """
+        Returns the pdf of selected gift payment slips
+        :return: "ir.attachment" instance
+        """
+        product = self.env["product.product"].search([("default_code", "in", refs)])
+        data = {
+            "product_ids": product.ids,
+            "product_id": product.ids,
+            "background": True,
+        }
+        pdf, _ = report.with_context(must_skip_send_to_printer=True).render_qweb_pdf(
+            doc_ids, data=data
+        )
+        file_name = _("gift payment slips") + ".pdf"
+
+        return self.env['ir.attachment'].create({
+            'name': file_name,
+            'datas_fname': file_name,
+            'datas': base64.encodebytes(pdf),
+        })
