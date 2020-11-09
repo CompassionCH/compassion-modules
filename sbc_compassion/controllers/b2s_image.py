645e8e09b067de042014b2eb1776717f6a8dc609
Theyiot <pprizee@gmail.com>
2020-11-09 15:05:57 +0100

    CO-3483 Refactors in route '/b2s_image' to provide more flexibility

--- sbc_compassion/controllers/b2s_image.py
+++ sbc_compassion/controllers/b2s_image.py
@@ -24,7 +24,7 @@ class RestController(http.Controller):
     @http.route("/b2s_image", type="http", auth="public", methods=["GET"])
     # We don't want to rename parameter id because it's used by our sponsors
     # pylint: disable=redefined-builtin
-    def handler_b2s_image(self, id=None, **parameters):
+    def handler_b2s_image(self, id=None, disposition=None, file_type=None, **parameters):
         """ Handler for `/b2s_image` url for json data.
 
         It accepts only Communication Kit Notifications.
@@ -38,16 +38,22 @@ class RestController(http.Controller):
         correspondence = correspondence_obj.search([("uuid", "=", id)])
         if not correspondence:
             raise NotFound()
-        data = correspondence.get_image()
         correspondence.email_read = datetime.now()
         headers = Headers()
+        disposition = disposition if disposition else "attachment"
         if correspondence.letter_format == "zip":
-            fname = fields.Date.today().strftime("%d-%m-%Y") + " letters.zip"
-            headers.add("Content-Disposition", "attachment", filename=fname)
+            if file_type == "pdf":
+                data = correspondence.generate_original_pdf()
+                fname = correspondence.file_name
+            else:
+                data = correspondence.get_image()
+                fname = fields.Date.today().strftime("%d-%m-%Y") + " letters.zip"
+            headers.add("Content-Disposition", disposition, filename=fname)
             response = Response(data, content_type="application/zip", headers=headers)
         else:
+            data = correspondence.get_image()
             headers.add(
-                "Content-Disposition", "attachment", filename=correspondence.file_name
+                "Content-Disposition", disposition, filename=correspondence.file_name
             )
             response = Response(data, content_type="application/pdf", headers=headers)
         return response
