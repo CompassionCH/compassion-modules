d3796126b8411f4ad7d8da1d76ebb163a84d5d32
Jonathan Guerne <jonathan.guerne@gmail.com>
2021-04-22 10:23:57 +0200

    CO-3582 explicitly specify state for child after release (#1503)
    * CO-3582 explicitly specify state for child after release
    
    * CO-3582 add state check on child release to keep hold if necessary

--- child_compassion/models/child_compassion.py
+++ child_compassion/models/child_compassion.py
@@ -671,7 +671,14 @@ class CompassionChild(models.Model):
     @api.multi
     def child_released(self, state="R"):
         """ Is called when a child is released to the global childpool. """
-        self.write({"sponsor_id": False, "state": state, "hold_id": False})
+        # if the state is consigned we want to keep to link to the hold.
+        remove_link_to_hold = state not in ["N"]
+
+        update_dic = {"sponsor_id": False, "state": state}
+        if remove_link_to_hold:
+            update_dic.update({"hold_id": False})
+
+        self.write(update_dic)
         # Check if it was a depart and retrieve lifecycle event
         self.get_lifecycle_event()
 
