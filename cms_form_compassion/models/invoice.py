eaa5a2c63982220a32245b8ca7bef92212faebfb
coup1234 <robin.berguerand@gmail.com>
2021-04-22 13:31:16 +0200

    CO-3531: move logic to cms_form_compassion

---
+++ cms_form_compassion/models/invoice.py
@@ -0,0 +1,15 @@
+##############################################################################
+#
+#    Copyright (C) 2021 Compassion CH (http://www.compassion.ch)
+#    @author: Robin Berguerand <robin.berguerand@gmail.com>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+from odoo import models, fields
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+    auto_cancel_no_transaction = fields.Boolean(default=False, help='If true, cancel the invoice if the linked payment '
+                                                        'transaction is cancelled',oldname='cancel_invoice')
