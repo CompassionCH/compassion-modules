59fe5e7f80a090339c4ff67fd37a6fa11b813ab7
Emanuel Cino <ecino@compassion.ch>
2021-08-23 08:42:19 +0200

    Don't sanitize html in generate communication wizard

--- partner_communication/wizards/generate_communication_wizard.py
+++ partner_communication/wizards/generate_communication_wizard.py
@@ -48,7 +48,7 @@ class GenerateCommunicationWizard(models.TransientModel):
     send_mode = fields.Selection("_send_mode_select", default="physical")
     customize_template = fields.Boolean()
     subject = fields.Char()
-    body_html = fields.Html()
+    body_html = fields.Html(sanitize=False)
     report_id = fields.Many2one(
         "ir.actions.report",
         "Letter template",
