559f405c11db1e41b985783526f1170a205c3f4b
Emanuel Cino <ecino@compassion.ch>
2021-08-31 12:16:31 +0200

    CO-3746 FIX again leave computation
    - Use the working rate to make better computations

--- hr_attendance_management/models/hr_holidays.py
+++ hr_attendance_management/models/hr_holidays.py
@@ -48,13 +48,12 @@ class HrLeave(models.Model):
         if employee_id:
             employee = self.env['hr.employee'].browse(employee_id)
             res = employee.get_work_days_data(date_from, date_to)
-            # we extract the hour instead of the day. dividing the total leave hours
-            # by default hours per day allows us to take into account employees
-            # that work part time (instead of subtracting whole days)
             hours_per_day = employee.contract_id.resource_calendar_id.hours_per_day
-            if not hours_per_day:
+            full_time_per_day = employee.company_id.resource_calendar_id.hours_per_day
+            if not hours_per_day or not full_time_per_day:
                 raise UserError(_(
                     "Working contract should define working hours per day"))
-            return res['hours'] / hours_per_day
+            working_rate = hours_per_day / full_time_per_day
+            return res['days'] * working_rate
 
         return super()._get_number_of_days(date_from, date_to, employee_id)
