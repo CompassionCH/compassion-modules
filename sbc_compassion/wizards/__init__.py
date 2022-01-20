b4920032345056b968011436208e0ce2d2433df1
Raphaël Margueron <raphael.margueron@gmail.com>
2022-01-20 12:06:22 +0100

    Remove crosscheck view

--- sbc_compassion/wizards/__init__.py
+++ sbc_compassion/wizards/__init__.py
@@ -7,7 +7,6 @@
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
-from . import correspondence_template_crosscheck
 from . import import_review
 from . import download_letters
 from . import get_letter_image_wizard
