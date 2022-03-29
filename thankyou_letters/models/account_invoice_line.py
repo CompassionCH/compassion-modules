bc9a7148f0e57739e5e37f2a547482142fcca96c
Emanuel Cino <ecino@compassion.ch>
2022-03-29 16:40:24 +0200

    CO-3922 Also use donation conversion for thankyou letters

--- thankyou_letters/models/account_invoice_line.py
+++ thankyou_letters/models/account_invoice_line.py
@@ -27,7 +27,7 @@ class AccountInvoiceLine(models.Model):
         :return: (total_donation_amount, product_name)
         """
         res_name = False
-        total = sum(self.mapped("price_subtotal"))
+        total = sum(self.mapped("price_subtotal_signed"))
         total_string = f"{int(total):,}".replace(",", "'")
 
         product_names = self.mapped("product_id.thanks_name")
