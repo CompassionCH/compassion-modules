bdca9d73e05d2ed862039b49eed333aebdae77fb
Emanuel Cino <ecino@compassion.ch>
2022-05-05 16:13:54 +0200

    CS-757 CS-767 CS-488
    Add ability to modify interaction resume

--- crm_compassion/migrations/12.0.1.0.2/post-migration.py
+++
@@ -1,7 +0,0 @@
-from openupgradelib import openupgrade
-
-
-def migrate(cr, installed_version):
-    if not installed_version:
-        return
-    openupgrade.load_xml(cr, 'crm_compassion', 'data/demand_planning.xml')
