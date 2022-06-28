0a9df45d8c97848e9e8ca78ce26cbdb1fff1a7f1
Emanuel Cino <ecino@compassion.ch>
2022-06-28 13:56:57 +0200

    CP-75 [ADD] few API endpoints
    - get_translation_issue_list
    - raise_translation_issue
    - reply_to_comments
    - mark_comments_read
    - approve_translation

--- sbc_compassion/migrations/12.0.2.0.0/post-migration.py
+++ sbc_compassion/migrations/12.0.2.0.0/post-migration.py
@@ -23,4 +23,7 @@ def migrate(env, version):
     env.cr.execute("""
         UPDATE correspondence SET translate_done = translate_date
         WHERE translate_date IS NOT NULL;
+        UPDATE correspondence SET translate_date = write_date
+        WHERE state = 'Global Partner translation queue'
+        AND translate_date IS NULL;
     """)
