4a667b91faa4aa8f6592a8e77949a6e4f4ac3e61
sebastien <s.jeanfavre@compassion.ch>
2022-07-20 15:52:06 +0200

    CP-94 text and comments displayed in reply_to_comments_view

--- sbc_translation/wizards/reply_to_comments.py
+++ sbc_translation/wizards/reply_to_comments.py
@@ -4,14 +4,11 @@ from odoo import models, fields, api
 class ReplyToComments(models.TransientModel):
     _name = "reply.to.comments"
 
-    comments = fields.Char(string="Comments")
-    answer = fields.Char(string="Answer")
+    paragraph_ids = fields.Many2many("correspondence.paragraph", string="Paragraphs", readonly=True)
+    answer = fields.Text(string="Answer")
 
-
-    # @api.multi
-    # def _get_comments(self):
-    #     return self.env["correspondence"].browse(self.env.context.get("active_id")).comments
-    #
-
+    @api.multi
     def send_reply(self):
-        self.ensure_one()
+        corr = self.env['correspondence'].search([('id', '=', self._context['active_id'])])
+        return corr.reply_to_comments(self.answer)
+
