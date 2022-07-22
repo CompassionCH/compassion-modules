c0ae205daf903703aa30a469944385b037b6993a
sebastien <s.jeanfavre@compassion.ch>
2022-07-22 11:09:00 +0200

    CP-94 to do: debug the sending of mail

--- sbc_translation/wizards/reply_to_comments.py
+++ sbc_translation/wizards/reply_to_comments.py
@@ -4,13 +4,11 @@ from odoo import models, fields, api
 class ReplyToComments(models.TransientModel):
     _name = "reply.to.comments"
 
-    paragraph_ids = fields.Many2many(
-        "correspondence.paragraph", string="Paragraphs", readonly=True)
-    answer = fields.Char(string="Answer")
+    paragraph_ids = fields.Many2many("correspondence.paragraph", string="Paragraphs", readonly=True)
+    answer = fields.Text(string="Answer")
 
+    @api.multi
     def send_reply(self):
-        self.ensure_one()
-        print("Reply SENDED")
-        return True
-
+        corr = self.env['correspondence'].search([('id', '=', self._context['active_id'])])
+        return corr.reply_to_comments(self.answer)
 
