47558d901379609127b483cb2cda43f0712d2ba7
Emanuel Cino <ecino@compassion.ch>
2022-10-11 11:01:05 +0200

    CP-156 [ADD] module for accepting REST-JSON API endpoints

---
+++ rest_json_api/http.py
@@ -0,0 +1,39 @@
+import json
+from werkzeug.exceptions import BadRequest
+
+from odoo.http import JsonRequest
+from odoo.tools import date_utils
+
+
+class RestJSONRequest(JsonRequest):
+    """
+    Special RestJson Handler to accept empty JSON GET messages for
+    mobile-app-api and send back results in clean JSON format
+    (remove wrapper made by Odoo)
+    """
+
+    def __init__(self, *args):
+        try:
+            # The following statement seems to have no effect but it is not the
+            # case. It prevents the *super* of emptying the stream containing
+            # the post data. Without it, we loose access to the post data.
+            # pylint: disable=pointless-statement
+            args[0].values
+
+            super().__init__(*args)
+            self.params = {key: val for key, val in self.httprequest.args.items()}
+        except BadRequest as error:
+            # Put simply an empty JSON data
+            if "Invalid JSON data" in error.description:
+                self.jsonrequest = {}
+                # PUT The GET parameters as the parameters for the controller
+                self.params = {key: val for key, val in self.httprequest.values.items()}
+                self.context = dict(self.session.context)
+            else:
+                raise
+
+    def _json_response(self, result=None, error=None):
+        odoo_result = super()._json_response(result, error)
+        if result is not None and error is None:
+            odoo_result.data = json.dumps(result, default=date_utils.json_default)
+        return odoo_result
