a81ed4781274d0ecc9d77117974e6070e44015a3
Emanuel Cino <ecino@compassion.ch>
2022-11-07 08:19:07 +0100

    Revert "Update contracts.py"
    This reverts commit f614929ede71330d9e606c3a1a34bedf856629de.

--- sponsorship_compassion/models/contracts.py
+++ sponsorship_compassion/models/contracts.py
@@ -507,9 +507,8 @@ class SponsorshipContract(models.Model):
         if "correspondent_id" in vals:
             previous_states.extend([r.state for r in self])
             updated_correspondents = self.filtered("global_id")
-            if self.env.context.get("no_upsert"):
-                for record in updated_correspondents:
-                    record._remove_correspondent()
+            for record in updated_correspondents:
+                record._remove_correspondent()
 
             for child in self.mapped("child_id"):
                 child.child_sponsored(vals["correspondent_id"])
