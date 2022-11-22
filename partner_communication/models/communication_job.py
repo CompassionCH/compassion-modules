6617ebe43bfabcc210fd9a51eb249e0efcdd20f2
Emanuel Cino <ecino@compassion.ch>
2022-11-22 09:26:22 +0100

    FIX reply-to field in communications

--- partner_communication/models/communication_job.py
+++ partner_communication/models/communication_job.py
@@ -622,7 +622,6 @@ class CommunicationJob(models.Model):
                 "subject": self.subject,
                 "attachment_ids": [(6, 0, self.ir_attachment_ids.ids)],
                 "auto_delete": False,
-                "reply_to": (self.email_template_id.reply_to or self.user_id.email),
             }
             if "default_email_vals" in self.env.context:
                 email_vals.update(self.env.context["default_email_vals"])
