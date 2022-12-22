6afb4f913a2ef3ea2072e217482f26bc6a679a1a
Simon Gonzalez <simon.gonzalez@bluewin.ch>
2022-12-22 13:49:11 +0100

    CP-239 refactor

---
+++ res_partner_compassin/__init__.py
@@ -0,0 +1,9 @@
+##############################################################################
+#
+#    Copyright (C) 2014 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Simon Gonzalez <simon.gonzalez@bluewin.ch>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
