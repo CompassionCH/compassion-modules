80e880577b75ee7cade1cc5a5703262132eaf411
Simon Gonzalez <simon.gonzalez@bluewin.ch>
2023-01-23 09:15:03 +0100

    CP-339 planed regression don't return phone call

--- partner_communication/models/__init__.py
+++ partner_communication/models/__init__.py
@@ -16,6 +16,5 @@ from . import email
 from . import ir_attachment
 from . import mail_template
 from . import report_with_omr
-from . import phone_common
 from . import ir_actions
 from . import queue_job
