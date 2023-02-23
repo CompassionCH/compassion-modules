2469c284c2c9cec60cff4d379ddce0c966f5065f
Emanuel Cino <ecino@compassion.ch>
2023-02-23 14:06:32 +0100

    FIX (2) migration of Christmas contracts
    - The correct value will be imported back from Nav, we only cancel all contracts.

--- sponsorship_compassion/migrations/14.0.1.1.6/post-migration.py
+++ sponsorship_compassion/migrations/14.0.1.1.6/post-migration.py
@@ -7,13 +7,8 @@ _logger = logging.getLogger(__name__)
 @openupgrade.migrate()
 def migrate(env, version):
     christmas = env["product.product"].search([("default_code", "=", "Christmas")])
-    sponsors = env["res.partner"].search([("number_sponsorships", ">", 0)])
-    _logger.info("Migrating christmas contracts for %s sponsors...", len(sponsors))
-    for i, partner in enumerate(sponsors):
-        _logger.info("... doing sponsor n° %s/%s", i+1, len(sponsors))
-        contracts = partner.other_contract_ids.filtered(lambda c: c.state not in ["cancelled", "terminated"])
-        christmas_lines = contracts.mapped("contract_line_ids").filtered(lambda l: l.product_id == christmas)
-        if not christmas_lines:
-            continue
-        partner.with_delay().terminate_christmas_contracts(christmas_lines)
-    _logger.info("Migration done!")
+    clines = env["recurring.contract.line"].search(
+        [("product_id", "=", christmas.id), ("contract_id.state", "not in", ["terminated", "cancelled"])])
+    for partner in clines.mapped("contract_id.partner_id"):
+        partner.with_delay().terminate_christmas_contracts(
+            partner.other_contract_ids.mapped("contract_line_ids") & clines)
