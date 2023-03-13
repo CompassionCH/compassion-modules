5d46330c9523484efe0b4e96f7edf0990d066bad
NoeBerdoz <berdoznoe@gmail.com>
2023-03-13 16:27:29 +0100

    FIX: check existence of skip param

--- child_compassion/wizards/global_child_search.py
+++ child_compassion/wizards/global_child_search.py
@@ -568,7 +568,7 @@ class GlobalChildSearch(models.TransientModel):
 
             # When the skip param default value is higher than the available beneficiaries
             # make a second request with a computed skip param to still get an available beneficiary when possible
-            if self.nb_found and self.nb_found <= params['skip']:
+            if self.nb_found and 'skip' in params and self.nb_found <= params['skip']:
                 # Set the 'skip' parameter to retrieve only middle urgent beneficiaries
                 params['skip'] = self.nb_found // 2 if self.nb_found >= 2 else 0
                 result = onramp.send_message(service_name, method, None, params)
