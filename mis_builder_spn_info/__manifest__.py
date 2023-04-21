dad5217cd2c236d0acda98f0764456acd04fac59
davidwul <david@compassion.ch>
2023-04-21 12:05:33 +0200

    Update mis_builder_spn_info/__manifest__.py
    Co-authored-by: NoeBerdoz <44669187+NoeBerdoz@users.noreply.github.com>

--- mis_builder_spn_info/__manifest__.py
+++ mis_builder_spn_info/__manifest__.py
@@ -11,7 +11,7 @@
     """,
 
     'author': "Compassion Suisse",
-    'website': "http://www.compassion.ch",
+    'website': "https://www.compassion.ch",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
