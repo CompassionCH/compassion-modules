4af0cd574f0493aed28bf95d91abd7134d082663
NoeBerdoz <44669187+NoeBerdoz@users.noreply.github.com>
2023-04-21 15:22:59 +0200

    REFACTOR: style and indentation corrections

--- mis_builder_spn_info/models/spn_info.sql
+++ mis_builder_spn_info/models/spn_info.sql
@@ -1,16 +1,61 @@
 CREATE OR REPLACE VIEW mis_spn_info AS (
-select row_number() OVER (ORDER BY a.date) as id,a.*
-from (
-    select activation_date::date as date, aa.id  as account_id,correspondent_id ,rc.partner_id ,0 as debit, 1 as credit, rc.id as contract_id,18 as currency_id,
-    	child_id ,rc.company_id, rco.analytic_id  , pricelist_id ,medium_id ,source_id ,campaign_id ,sponsorship_line_id ,sub_sponsorship_id, parent_id,end_reason_id
-    from recurring_contract rc left join recurring_contract_origin rco on rco.id =rc.origin_id
-    left join account_account aa on aa.company_id =rc.company_id and aa."name" ='Sponsorship count'
-    where activation_date is not null and child_id is not null
-    union
-    select end_date::date as date ,aa.id  as account_id, correspondent_id ,rc.partner_id ,1 as debit, 0 as credit ,rc.id as contract_id,18 as currency_id,
-    	child_id ,rc.company_id, rco.analytic_id , pricelist_id ,medium_id ,source_id ,campaign_id ,sponsorship_line_id ,sub_sponsorship_id, parent_id,end_reason_id
-    from recurring_contract rc left join recurring_contract_origin rco on rco.id =rc.origin_id
-    left join account_account aa on aa.company_id =rc.company_id and aa."name" ='Sponsorship count'
-    where activation_date is not null and end_date is not null and child_id is not null
-    ) a
+    SELECT row_number() OVER (ORDER BY a.date) AS id, a.*
+    FROM (
+        SELECT 
+            activation_date::date AS date,
+            aa.id AS account_id,
+            correspondent_id,
+            rc.partner_id,
+            0 AS debit,
+            1 AS credit,
+            rc.id AS contract_id,
+            18 AS currency_id,
+            child_id,
+            rc.company_id,
+            rco.analytic_id,
+            pricelist_id,
+            medium_id,
+            source_id,
+            campaign_id,
+            sponsorship_line_id,
+            sub_sponsorship_id,
+            parent_id,
+            end_reason_id
+        FROM 
+            recurring_contract rc
+            LEFT JOIN recurring_contract_origin rco ON rco.id = rc.origin_id
+            LEFT JOIN account_account aa ON aa.company_id = rc.company_id AND aa."name" = 'Sponsorship count'
+        WHERE 
+            activation_date IS NOT NULL 
+            AND child_id IS NOT NULL
+        UNION
+        SELECT 
+            end_date::date AS date,
+            aa.id AS account_id,
+            correspondent_id,
+            rc.partner_id,
+            1 AS debit,
+            0 AS credit,
+            rc.id AS contract_id,
+            18 AS currency_id,
+            child_id,
+            rc.company_id,
+            rco.analytic_id,
+            pricelist_id,
+            medium_id,
+            source_id,
+            campaign_id,
+            sponsorship_line_id,
+            sub_sponsorship_id,
+            parent_id,
+            end_reason_id
+        FROM 
+            recurring_contract rc
+            LEFT JOIN recurring_contract_origin rco ON rco.id = rc.origin_id
+            LEFT JOIN account_account aa ON aa.company_id = rc.company_id AND aa."name" = 'Sponsorship count'
+        WHERE 
+            activation_date IS NOT NULL
+            AND end_date IS NOT NULL
+            AND child_id IS NOT NULL
+        ) a
 )
