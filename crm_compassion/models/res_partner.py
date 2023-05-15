c1370c98c73cad47a8e99d0913c5f1a678cfd93b
Simon Gonzalez <45272875+simgonzalez@users.noreply.github.com>
2023-05-15 13:48:30 +0200

    Update res_partner.py

--- crm_compassion/models/res_partner.py
+++ crm_compassion/models/res_partner.py
@@ -67,40 +67,16 @@ class Partner(models.Model):
                     ("active", "=", False),
                 ]
             )
-
-    def open_interaction(self):
+    def open_interaction(self, full=False):
         """
         Populates data for interaction resume and open the view
         :return: action opening the view
         """
         self.ensure_one()
-        self.env["interaction.resume"].populate_resume(self.id)
+        self.env["interaction.resume"].populate_resume(self.id, full)
         partners_with_same_email_ids = (
             self.env["res.partner"]
-            .search([("email", "!=", False), ("email", "=", self.email)])
-            .ids
-        )
-        return {
-            "name": _("Interaction resume"),
-            "type": "ir.actions.act_window",
-            "res_model": "interaction.resume",
-            "view_mode": "tree,form",
-            "domain": [("partner_id", "in",
-                        self.ids + partners_with_same_email_ids +
-                        self.other_contact_ids.ids)],
-            "target": "current",
-        }
-
-    def open_interaction_full(self):
-        """
-        Populates data for interaction resume and open the view
-        :return: action opening the view
-        """
-        self.ensure_one()
-        self.env["interaction.resume"].populate_resume(self.id, full=True)
-        partners_with_same_email_ids = (
-            self.env["res.partner"]
-                .search([("email", "!=", False), ("email", "=", self.email)])
+                .search([("email", "!=", False), ("email", "=", self.email), ('id', 'not in', self.ids)])
                 .ids
         )
         return {
@@ -111,9 +87,12 @@ class Partner(models.Model):
             "view_mode": "tree,form",
             "domain": [("partner_id", "in",
                         self.ids + partners_with_same_email_ids +
-                        self.other_contact_ids.ids)],
+                        self.mapped("other_contact_ids").ids)],
             "target": "current",
         }
+
+    def open_interaction_full(self):
+        return self.open_interaction(full=True)
 
     def log_call(self):
         """Prepare crm.phonecall creation."""
