671458b322215f5e586de3fcded3945476eb70ae
NoeBerdoz <berdoznoe@gmail.com>
2023-06-02 16:13:40 +0200

    [REFACTOR] Module renamed from sponsorship_tracking to sponsorship_sub_management

--- sponsorship_sub_management/__manifest__.py
+++ sponsorship_sub_management/__manifest__.py
@@ -28,7 +28,7 @@
 
 # pylint: disable=C8101
 {
-    "name": "Compassion Sponsorships Tracking",
+    "name": "Compassion SUB Sponsorships Management",
     "version": "14.0.1.0.0",
     "category": "Compassion",
     "author": "Compassion CH",
