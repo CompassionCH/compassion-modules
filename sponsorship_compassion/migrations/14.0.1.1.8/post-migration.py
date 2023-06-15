b3f9c170098d1a27a74a813bbc6d4180667df324
Emanuel Cino <ecino@compassion.ch>
2023-06-15 10:14:27 +0200

    [FIX] 571 - Don't update gmc_commitment_id at migration
    - The value is already set locally and don't need to be updated. Instead, we log an error if we observe a difference from the data GMC sends us.

--- sponsorship_compassion/migrations/14.0.1.1.8/post-migration.py
+++ sponsorship_compassion/migrations/14.0.1.1.8/post-migration.py
@@ -1,5 +1,6 @@
 import logging
 from openupgradelib import openupgrade
+
 from odoo.addons.message_center_compassion.tools.onramp_connector import OnrampConnector
 
 _logger = logging.getLogger(__name__)
@@ -14,7 +15,7 @@ def migrate(env, version):
         ("type", "ilike", "S"), ("state", "=", "active"), ("gmc_correspondent_commitment_id", "=", False)])
     for contract in contracts:
         global_id = contract.child_id.global_id
-        _logger.info(f"Migrating {global_id} child on {contract.gmc_commitment_id} contract")
+        _logger.info(f"Migrating {global_id} child on {contract.gmc_commitment_id} contract.")
         answer = onramp.send_message(f"beneficiaries/{global_id}/summary", "GET")
         commitments = answer.get("content").get("Commitments") if answer.get("code") == 200 else False
         if commitments:
@@ -24,17 +25,13 @@ def migrate(env, version):
                     continue
                 if commitment.get("CommitmentType") == "Sponsor":
                     new_gmc_id = commitment.get('CommitmentID')
-                    if new_gmc_id:
-                        _logger.info(f"Commitment used to assign gmc_commitment_id \n"
-                                     f"current :{contract.gmc_commitment_id}\n"
-                                     f"new :{new_gmc_id}")
-                        contract.gmc_commitment_id = new_gmc_id
+                    if new_gmc_id and new_gmc_id != contract.gmc_commitment_id:
+                        err_message = f"Sponsorship {contract.display_name} has a different commitment_id " \
+                                      f"from the one received by GMC ({contract.gmc_commitment_id} vs {new_gmc_id})"
+                        contract.message_post(body=err_message)
+                        _logger.error(err_message)
                     partner = env['res.partner'].search_read([
                         ('global_id', '=', commitment.get("SupporterID"))], ['id'])
                     contract.gmc_payer_partner_id = partner[0].get("id") if partner else False
                 elif commitment.get("CommitmentType") == "Correspondent":
-                    _logger.info(f"Commitment used to assign gmc_commitment_id \n"
-                                 f"current :{contract.gmc_correspondent_commitment_id}\n"
-                                 f"new :{commitment.get('CommitmentID')}")
                     contract.gmc_correspondent_commitment_id = commitment.get("CommitmentID")
-    _logger.info("End of commitment migration")
