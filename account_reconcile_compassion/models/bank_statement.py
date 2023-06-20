b674129477bc7f2242bfdc20e8adedeb335c8f7b
Simon Gonzalez <45272875+simgonzalez@users.noreply.github.com>
2023-06-20 10:58:51 +0200

    T0246 line reconciliation should be made in separated jobs. (#1779)
    * T0246 line reconciliation should be made in separated jobs.
    
    * T0246 remove useless comments

--- account_reconcile_compassion/models/bank_statement.py
+++ account_reconcile_compassion/models/bank_statement.py
@@ -101,26 +101,15 @@ class AccountStatement(models.Model):
             matching_amls = reconcile_model._apply_rules(bank_statement.line_ids)
 
             for line_id, result in matching_amls.items():
-                if result["aml_ids"]:
-                    line = bank_statement.line_ids.browse(line_id)
-                    move_lines = self.env["account.move.line"].browse(result["aml_ids"])
-                    # Check that line wasn't already reconciled
+               self.with_delay()._reconcile_single_line(line_id, result, bank_statement, reconcile_model)
 
-                    move_lines = move_lines.filtered(lambda a: not a.reconciled)
-                    reconcile = reconcile_model._prepare_reconciliation(
-                        line, move_lines.ids)
-
-                    # An open balance is needed but no partner has been found.
-                    # if reconcile['open_balance_dict'] is False:
-                    #     continue
-
-                    # new_aml_dicts = reconcile['new_aml_dicts']
-                    # if reconcile['open_balance_dict']:
-                    #     new_aml_dicts.append(reconcile['open_balance_dict'])
-                    #
-                    line.with_context({"default_journal_id": None}).reconcile(reconcile)
-                    # line.process_reconciliation(
-                    #     counterpart_aml_dicts=reconcile['counterpart_aml_dicts'],
-                    #     payment_aml_rec=reconcile['payment_aml_rec'],
-                    #     new_aml_dicts=new_aml_dicts,
-                    # )
+    def _reconcile_single_line(self, line_id, result, bank_statement, reconcile_model):
+        """Reconcile method to run it as a job"""
+        if result["aml_ids"]:
+            line = bank_statement.line_ids.browse(line_id)
+            move_lines = self.env["account.move.line"].browse(result["aml_ids"])
+            # Check that line wasn't already reconciled
+            move_lines = move_lines.filtered(lambda a: not a.reconciled)
+            reconcile = reconcile_model._prepare_reconciliation(
+                line, move_lines.ids)
+            line.with_context({"default_journal_id": None}).reconcile(reconcile)
