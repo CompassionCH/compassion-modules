4dd738411f5e28d3c0b132df3711b438e54c9441
Jacques Rodrigues <zenom.1996@gmail.com>
2023-10-02 13:51:58 +0200

    T0554 Added store attribute to child_code in order to be able to sort alphabetically.

--- sponsorship_compassion/models/contracts.py
+++ sponsorship_compassion/models/contracts.py
@@ -30,7 +30,6 @@ SPONSORSHIP_TYPE_LIST = ["S", "SC", "SWP"]
 class SponsorshipContract(models.Model):
     _inherit = ["recurring.contract", "compassion.mapped.model"]
     _name = "recurring.contract"
-    # _order = "child_id"  # , id desc"
 
     ##########################################################################
     #                                 FIELDS                                 #
