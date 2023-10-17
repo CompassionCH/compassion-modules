08af27082d1661be2084ee81fc8ddb45bb577a1c
Jacques Rodrigues <zenom.1996@gmail.com>
2023-10-17 12:34:04 +0200

    some problems by being restrictive or not

--- mis_builder_spn_info/models/spn_info.sql
+++ mis_builder_spn_info/models/spn_info.sql
@@ -99,10 +99,43 @@ CREATE OR REPLACE VIEW mis_spn_info AS (
         FROM
             recurring_contract rc
             LEFT JOIN recurring_contract_origin rco ON rco.id = rc.origin_id
-            LEFT JOIN account_account aa ON aa.company_id = rc.company_id AND aa."name" = 'Child Sponsored'
+            LEFT JOIN account_account aa ON aa.company_id = rc.company_id AND aa."name" = 'Contract Created'
         WHERE
-            (activation_date IS NULL OR
-            date_trunc('month', activation_date) > date_trunc('month', start_date))
+--            (activation_date IS NULL OR
+--            date_trunc('month', activation_date) > date_trunc('month', start_date))
+--            AND
+            child_id IS NOT NULL
+
+        UNION
+
+        SELECT
+            activation_date::date AS date,
+            aa.id AS account_id,
+            correspondent_id,
+            rc.partner_id,
+            1 AS debit,
+            0 AS credit,
+            rc.id AS contract_id,
+            18 AS currency_id,
+            child_id,
+            rc.company_id,
+            rco.analytic_id,
+            pricelist_id,
+            medium_id,
+            source_id,
+            campaign_id,
+            sponsorship_line_id,
+            sub_sponsorship_id,
+            parent_id,
+            end_reason_id,
+            activation_date,
+            TRUE as waiting
+        FROM
+            recurring_contract rc
+            LEFT JOIN recurring_contract_origin rco ON rco.id = rc.origin_id
+            LEFT JOIN account_account aa ON aa.company_id = rc.company_id AND aa."name" = 'Contract Created'
+        WHERE
+            activation_date IS NOT NULL
             AND child_id IS NOT NULL
         ) a
 )
