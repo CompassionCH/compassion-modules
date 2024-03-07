66cba42b194b07ebc107ec0a22adac899f14b38c
Jérémie Lang <jlang@compassion.ch>
2024-03-07 14:32:01 +0100

    T1096 - Churches/CRM 56 - Fix 'opportunity' term automatically added

--- crm_compassion/models/crm_lead.py
+++ crm_compassion/models/crm_lead.py
@@ -81,3 +81,9 @@ class CrmLead(models.Model):
     def merge_opportunity(self, user_id=False, team_id=False):
         CRM_LEAD_FIELDS_TO_MERGE.extend(["phonecall_ids", "meeting_ids"])
         return super().merge_opportunity(user_id, team_id)
+
+    @api.depends('partner_id')
+    def _compute_name(self):
+        for lead in self:
+            if not lead.name and lead.partner_id and lead.partner_id.name:
+                lead.name = lead.partner_id.name
