dbe9d4f3c242a36a373ef9a0c242ca41d1380a3f
luca <luca.delauren@gmail.com>
2024-04-03 13:35:04 +0200

    T1043: removed the redundant if, as the super method is only called for contracts of type "O".

--- sponsorship_compassion/models/contracts.py
+++ sponsorship_compassion/models/contracts.py
@@ -834,8 +834,7 @@ class SponsorshipContract(models.Model):
 
     def contract_waiting(self):
         contracts = self.filtered(lambda c: c.type == "O")
-        if contracts:
-            super(SponsorshipContract, contracts).contract_waiting()
+        super(SponsorshipContract, contracts).contract_waiting()
         for contract in self - contracts:
             if not contract.start_date:
                 contract.start_date = fields.Datetime.now()
