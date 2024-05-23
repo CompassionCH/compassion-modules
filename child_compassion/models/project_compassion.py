326fcbdcc399c65683bdd10df2041be915fc489f
Emanuel Cino <ecino@compassion.ch>
2024-05-23 14:49:22 +0200

    Run pre-commit

--- child_compassion/models/project_compassion.py
+++ child_compassion/models/project_compassion.py
@@ -405,7 +405,10 @@ class CompassionProject(models.Model):
     ###########################
 
     tpl_item_ids = fields.Many2many(
-        "communication.snippet", "name","snippet_text",string="Caption to use for pictures, or prayer shared by fcp, or else"
+        "communication.snippet",
+        "name",
+        "snippet_text",
+        string="Caption to use for pictures, or prayer shared by fcp, or else",
     )
 
     @api.model
