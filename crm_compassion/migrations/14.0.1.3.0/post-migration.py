eb90b911a56da6ac1c494c6b20a0fce15814479c
Nicolas Praz <praznicolas@gmail.com>
2024-08-06 11:40:16 +0200

    line too long...

--- crm_compassion/migrations/14.0.1.3.0/post-migration.py
+++ crm_compassion/migrations/14.0.1.3.0/post-migration.py
@@ -11,6 +11,8 @@ def migrate(env, version):
         WHERE stage_id IN (SELECT id FROM crm_stage WHERE is_lost = TRUE);
         UPDATE crm_lead
         SET stage_id = 211
-        WHERE active = FALSE AND probability = 0 AND stage_id NOT IN (SELECT id FROM crm_stage WHERE is_lost = TRUE);
+        WHERE active = FALSE AND probability = 0 AND stage_id NOT IN (
+            SELECT id FROM crm_stage WHERE is_lost = TRUE
+        );
         """,
     )
