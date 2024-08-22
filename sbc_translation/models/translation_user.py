37bb7c48155846ee59e2614e6f4ce14f524cad64
Emanuel Cino <ecino@compassion.ch>
2024-08-22 15:59:43 +0200

    T1741 FIX translator assignment

--- sbc_translation/models/translation_user.py
+++ sbc_translation/models/translation_user.py
@@ -67,7 +67,8 @@ class TranslationUser(models.Model):
         for translator in self:
             translator.nb_translated_letters_last_year = len(
                 translator.translated_letter_ids.filtered(
-                    lambda it: it.translate_date.year == fields.Datetime.now().year - 1
+                    lambda it: it.translate_date and
+                    it.translate_date.year == fields.Datetime.now().year - 1
                 )
             )
 
