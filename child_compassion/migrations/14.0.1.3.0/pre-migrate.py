8236ad6a16592f78fb1c29a735807651f1712b0b
Nicolas Praz <praznicolas@gmail.com>
2024-09-04 09:58:17 +0200

    added migration to remove dupplicate child pictures, still need to test it

---
+++ child_compassion/migrations/14.0.1.3.0/pre-migrate.py
@@ -0,0 +1,13 @@
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        DELETE FROM compassion_child_pictures ccp_a
+        USING compassion_child_pictures cpp_b
+        WHERE ccp_a.image_url == ccp_b.image_url AND ccp_a.child_id == ccp_b.child_id AND ccp_a.date >= cpp_b.date;
+        """
+    )
