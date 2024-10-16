761d6bc31079a5b99ee66f60ea4b4666a56082e5
Niels Lachat <nlachat@compassion.ch>
2024-10-16 08:40:35 +0200

    Fix pre-commit line length

--- partner_communication/models/communication_job.py
+++ partner_communication/models/communication_job.py
@@ -638,7 +638,8 @@ class CommunicationJob(models.Model):
                 try:
                     fields = template_id.with_context(
                         template_preview_lang=lang,
-                        # Allow arbitrary data to be passed to the email template from the context
+                        # Allow arbitrary data to be passed to the email
+                        # template from the context
                         extra_email_data=self.env.context.get("extra_email_data"),
                     ).generate_email(job.ids, ["body_html", "subject"])
                     job.write(
