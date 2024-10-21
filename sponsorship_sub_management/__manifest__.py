5d9a54ae0fe1be991591aa20464880e1156fd328
robinrolle <robinrolle1748@gmail.com>
2024-10-21 13:39:09 +0200

    [MIG] T1880 sponsorship_sub_management: migration to 17.0

--- sponsorship_sub_management/__manifest__.py
+++ sponsorship_sub_management/__manifest__.py
@@ -29,7 +29,7 @@
 # pylint: disable=C8101
 {
     "name": "Compassion SUB Sponsorships Management",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Compassion",
     "author": "Compassion CH",
     "maintainers": ["ecino", "NoeBerdoz"],
