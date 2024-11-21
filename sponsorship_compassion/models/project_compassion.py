3a2f61f1d45d071432b009759c2271d38153f184
Emanuel Cino <ecino@compassion.ch>
2024-11-21 11:55:31 +0100

    T1715 Use jobs for FCPs updates
    - This leverage time usage and priority for resource consumption

--- sponsorship_compassion/models/project_compassion.py
+++ sponsorship_compassion/models/project_compassion.py
@@ -9,7 +9,6 @@
 ##############################################################################
 
 import logging
-import time
 
 from odoo import api, fields, models
 from odoo.tools import relativedelta
@@ -73,10 +72,10 @@ class ProjectCompassion(models.Model):
         contracts.reactivate_gifts()
 
     def sync_projects_from_gmc(
-        self, requests_throttle_seconds=1.0, log_period=100, max_projects_to_sync=None
+        self, requests_throttle_seconds=1.0, max_projects_to_sync=None
     ):
         """
-        Synchronises the informations and lifecycle events for all the projects with
+        Synchronises the information and lifecycle events for all the projects with
         active sponsorships from the GMC. This should be called from a cron job and can
         take a long time to execute (a few hours). The reason for this is that the
         requests to the GMC server are delayed in order to avoid overwhelming their
@@ -86,8 +85,6 @@ class ProjectCompassion(models.Model):
             requests_throttle_seconds (float, optional): Time to wait between requests,
                 in seconds. This prevents the GMC server from being overwhelmed.
                 Defaults to 1.0.
-            log_period (int, optional): Write to the log every log_period requests.
-                Defaults to 100.
             max_projects_to_sync (int, optional): Maximum number of projects to sync.
                 Used for testing. Defaults to None, which means: sync everything.
         """
@@ -104,21 +101,19 @@ class ProjectCompassion(models.Model):
 
         _logger.info(
             f"Starting projects sync from GMC. {projects_to_sync=}, "
-            f"{requests_throttle_seconds=}, {log_period=}. Estimated duration: "
+            f"Estimated duration: "
             f"{requests_throttle_seconds * nb_projects_to_sync} seconds."
         )
         for i, p in enumerate(projects_to_sync):
             # Only synchronise projects for which we have sponsorships to speedup
             # execution and decrease remote server load
-            p.with_context(async_mode=True).update_informations()
-            p.get_lifecycle_event()
+            p.with_delay(
+                eta=requests_throttle_seconds * i,
+                priority=100,
+                channel="root.child_compassion",
+            )._sync_from_gmc()
 
-            # Throttle requests to avoid overwhelming GMC server
-            time.sleep(requests_throttle_seconds)
-
-            if i > 0 and i % log_period == 0:
-                _logger.info(
-                    f"Projects sync from GMC in progress: "
-                    f"{i+1}/{nb_projects_to_sync}"
-                )
-        _logger.info("Finished projects sync from GMC. ")
+    def _sync_from_gmc(self):
+        self.ensure_one()
+        self.with_context(async_mode=False).update_informations()
+        self.get_lifecycle_event()
