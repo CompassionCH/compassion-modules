ab1b1edad9d3240bbd07188ecb1a5113fc22132e
jordyBSK <dylan.bossoku@jobtrek.ch>
2024-11-22 09:15:43 +0100

    feat: add the google maps button at the top of the form view

--- child_compassion/models/project_compassion.py
+++ child_compassion/models/project_compassion.py
@@ -535,10 +535,17 @@ class CompassionProject(models.Model):
     def _compute_google_link(self):
         for project in self:
             if project.gps_latitude and project.gps_longitude:
-                google_url = f"https://www.google.com/maps/search/?api=1&query={project.gps_latitude},{project.gps_longitude}"
-                project.google_link = (
-                    f'<a href="{google_url}" target="_blank">' "show in Google maps</a>"
-                )
+                project.google_link = f"https://www.google.com/maps/search/?api=1&query={project.gps_latitude},{project.gps_longitude}"
+            else:
+                project.google_link = False
+
+    def open_google_maps(self):
+        self.ensure_one()
+        return {
+            "type": "ir.actions.act_url",
+            "url": self.google_link,
+            "target": "new",
+        }
 
     def _compute_usd(self):
         usd = self.env.ref("base.USD")
