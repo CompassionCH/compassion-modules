0aa1103a161204095a2ec4ed0cf65734781e70b3
jordyBSK <dylan.bossoku@jobtrek.ch>
2024-12-20 12:51:46 +0100

    feat: set the value of zip_id to false during the anonymize method

--- sponsorship_compassion/models/res_partner.py
+++ sponsorship_compassion/models/res_partner.py
@@ -431,6 +431,7 @@ class ResPartner(models.Model):
                 "title": False,
                 "country_id": False,
                 "uuid": False,
+                "zip_id": False,
             }
         )
         partner.env["mail.mail"].search([("recipient_ids", "=", partner.id)]).unlink()
