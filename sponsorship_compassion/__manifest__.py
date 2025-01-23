4a90c8360cf3648721ef5e89a4b21c86a45a63b7
Emanuel Cino <ecino@compassion.ch>
2025-01-23 11:42:09 +0100

    BUMP modules version to ensure update on odoo SH

--- sponsorship_compassion/__manifest__.py
+++ sponsorship_compassion/__manifest__.py
@@ -29,7 +29,7 @@
 # pylint: disable=C8101
 {
     "name": "Compassion Sponsorships",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "category": "Compassion",
     "author": "Compassion CH",
     "license": "AGPL-3",
