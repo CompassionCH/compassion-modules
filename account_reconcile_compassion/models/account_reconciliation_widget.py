23b1842e5a83449c65e9d68ca40a58f316edccbf
Emanuel Cino <ecino@compassion.ch>
2025-02-04 11:11:14 +0100

    T2164 ADD channels for reconcile jobs

--- account_reconcile_compassion/models/account_reconciliation_widget.py
+++ account_reconcile_compassion/models/account_reconciliation_widget.py
@@ -72,7 +72,9 @@ class AccountReconciliationWidget(models.AbstractModel):
     @api.model
     def process_bank_statement_line(self, st_line_ids, data):
         # Execute a job to speedup the interface. The returned data is not used anyway.
-        self.with_delay()._process_bank_statement_line(st_line_ids, data)
+        self.with_delay(
+            channel="root.reconcile_compassion"
+        )._process_bank_statement_line(st_line_ids, data)
         return {}
 
     @api.model
