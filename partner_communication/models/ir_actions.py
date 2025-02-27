d60a6188a0c136372f4391809fdb7e02fd080586
Emanuel Cino <ecino@compassion.ch>
2025-02-27 11:25:52 +0100

    [IMP] Enhance server actions handling by using identity key for communication jobs

--- partner_communication/models/ir_actions.py
+++ partner_communication/models/ir_actions.py
@@ -53,14 +53,13 @@ class IrActionsServer(models.Model):
                     ]
                 )
                 # Use same job if possible to group communications for one partner
+                identity_key = f"create_communication.{self.config_id.id}.{partner.id}"
                 existing_job = self.env["queue.job"].search(
                     [
                         ("state", "=", "pending"),
-                        ("method_name", "=", "create_communication_job"),
-                        ("model_name", "=", self._name),
-                        ("func_string", "like", f"'partner_id': {partner.id}"),
-                        ("func_string", "like", f"'config_id': {self.config_id.id}"),
-                    ]
+                        ("identity_key", "=", identity_key),
+                    ],
+                    limit=1,
                 )
                 if existing_job:
                     vals = existing_job.args[0]
@@ -77,7 +76,9 @@ class IrActionsServer(models.Model):
                 if self.auto_send:
                     vals["auto_send"] = self.auto_send
                 delay = datetime.now() + timedelta(minutes=3)
-                self.with_delay(eta=delay).create_communication_job(vals)
+                self.with_delay(
+                    identity_key=identity_key, eta=delay
+                ).create_communication_job(vals)
         return {}
 
     def create_communication_job(self, vals):
