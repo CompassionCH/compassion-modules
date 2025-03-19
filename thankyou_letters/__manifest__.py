27f0ed3118920186a0efcee81a77782f16f70bf5
AndiR27 <andi.ramiqi@hesge.ch>
2025-03-19 16:14:45 +0100

    T1704 Generate communication added in Invoices

--- thankyou_letters/__manifest__.py
+++ thankyou_letters/__manifest__.py
@@ -35,7 +35,7 @@
     "license": "AGPL-3",
     "website": "https://github.com/CompassionCH/compassion-modules",
     "depends": [
-        "partner_communication",
+        "partner_communication_compassion",
         "partner_salutation",
         "account",
     ],
