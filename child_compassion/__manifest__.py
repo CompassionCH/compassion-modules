1bf8b67e867569bc4656d553f234f9bc49efd73d
Emanuel Cino <ecino@compassion.ch>
2025-03-24 16:01:48 +0100

    T2247 Reverse child name on child picture

--- child_compassion/__manifest__.py
+++ child_compassion/__manifest__.py
@@ -29,7 +29,7 @@
 # pylint: disable=C8101
 {
     "name": "Compassion Children",
-    "version": "17.0.1.0.3",
+    "version": "17.0.1.0.4",
     "category": "Compassion",
     "author": "Compassion CH",
     "license": "AGPL-3",
