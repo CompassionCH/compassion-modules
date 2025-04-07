09a25fb27868168ab615f36edb5748c5973d3fff
Emanuel Cino <ecino@compassion.ch>
2025-04-07 09:28:40 +0200

    T2341 FIX reply-to not formatted in communications

--- partner_communication/models/communication_job.py
+++ partner_communication/models/communication_job.py
@@ -882,7 +882,7 @@ class CommunicationJob(models.Model):
     ):
         res = dict.fromkeys(self.ids)
         for job in self:
-            res[job.id] = job.reply_to
+            res.update(job.email_template_id._render_field("reply_to", job.ids))
         return res
 
     def _job_sent(self, send_mode):
