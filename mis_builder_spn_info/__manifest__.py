e6fcfc8c287b2319e11a8464fa3d54eac7d8a805
Emanuel Cino <ecino@compassion.ch>
2025-05-08 07:23:27 +0200

    [MIG] mis_builder_spn_info: Migration to 16.0

--- mis_builder_spn_info/__manifest__.py
+++ mis_builder_spn_info/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Compassion Switzerland",
     "website": "https://github.com/CompassionCH/compassion-modules",
     "category": "Uncategorized",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     # any module necessary for this one to work correctly
     "depends": [
