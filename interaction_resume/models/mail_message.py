8a8e8e87c3f5cabd2c92db6be051be09110fba73
Emanuel Cino <ecino@compassion.ch>
2025-05-16 12:15:23 +0200

    T2396 ADD function to convert messages to interaction

---
+++ interaction_resume/models/mail_message.py
@@ -0,0 +1,51 @@
+from odoo import models
+
+
+class MailMessage(models.Model):
+    _inherit = "mail.message"
+
+    def convert_as_other_interaction(self):
+        """Convert the message to an interaction resume"""
+        interactions = self.env["partner.log.other.interaction"]
+        for message in self:
+            # Create the interaction resume
+            partner_id = False
+            if message.model == "res.partner":
+                partner_id = message.res_id
+            elif message.partner_ids:
+                partner_id = message.partner_ids[0].id
+            else:
+                res_model = self.env[message.model]
+                if hasattr(res_model, "partner_id"):
+                    partner_id = res_model.browse(message.res_id).partner_id.id
+            if not partner_id:
+                continue
+            interactions += interactions.create(
+                {
+                    "create_uid": message.author_id.id or message.create_uid.id,
+                    "partner_id": partner_id,
+                    "date": message.date,
+                    "communication_type": "Email",
+                    "subject": message.subject,
+                    "body": message.body,
+                    "other_type": "Direct message converted to interaction",
+                    "direction": "out",
+                }
+            )
+            message.unlink()
+        action = {
+            "type": "ir.actions.act_window",
+            "name": "Converted Interactions",
+            "res_model": "partner.log.other.interaction",
+            "view_mode": "tree,form",
+            "domain": [("id", "in", interactions.ids)],
+            "target": "current",
+        }
+        if len(interactions) == 1:
+            action.update(
+                {
+                    "res_id": interactions.id,
+                    "view_mode": "form",
+                }
+            )
+        return action
