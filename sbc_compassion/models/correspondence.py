c54ac06858257ea859b027da19df7a560e31c3d1
Emanuel Cino <ecino@compassion.ch>
2025-06-23 10:49:30 +0200

    T2358 FIX translation check notification only for S2B letters

--- sbc_compassion/models/correspondence.py
+++ sbc_compassion/models/correspondence.py
@@ -544,7 +544,8 @@ class Correspondence(models.Model):
                     "letter_responsible"
                 )
                 if responsible:
-                    for c in self:
+                    for c in self.filtered(
+                            lambda c: c.direction == "Supporter To Beneficiary"):
                         c._make_activity(vals["state"], responsible)
 
             elif "state" in vals:
