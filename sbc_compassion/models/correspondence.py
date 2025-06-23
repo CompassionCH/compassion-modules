bd4f779eee50a245b151042c77d30d8860adc74d
Emanuel Cino <ecino@compassion.ch>
2025-06-23 11:40:53 +0200

    T2356 Improve Disaster Alerts
    - Delay details get, as it seems GMC is sending back errors when we fetch the details directly
    - Notify staff only if impact is set

--- sbc_compassion/models/correspondence.py
+++ sbc_compassion/models/correspondence.py
@@ -606,7 +606,8 @@ class Correspondence(models.Model):
                 )
                 if responsible:
                     for c in self.filtered(
-                            lambda c: c.direction == "Supporter To Beneficiary"):
+                        lambda c: c.direction == "Supporter To Beneficiary"
+                    ):
                         c._make_activity(vals["state"], responsible)
 
             elif "state" in vals:
