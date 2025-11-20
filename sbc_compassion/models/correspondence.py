9e38e13f2a8240f5660c5890eb98ce4e0de57f85
Emanuel Cino <ecino@compassion.ch>
2025-11-20 13:32:45 +0100

    Be more permissive for resubmitting letters to GMC

--- sbc_compassion/models/correspondence.py
+++ sbc_compassion/models/correspondence.py
@@ -946,11 +946,6 @@ class Correspondence(models.Model):
 
     def resubmit_letter(self):
         for letter in self:
-            if letter.state != "Translation check unsuccessful":
-                raise UserError(
-                    _("Letter must be in state 'Translation check unsuccessful'")
-                )
-
             letter.write(
                 {
                     "kit_identifier": False,
@@ -958,7 +953,7 @@ class Correspondence(models.Model):
                     "state": "Received in the system",
                 }
             )
-            letter.create_commkit()
+        self.create_commkit().process_messages()
 
     def quality_check_failed(self):
         return self.write(
