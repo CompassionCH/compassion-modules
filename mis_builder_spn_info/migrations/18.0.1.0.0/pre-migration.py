8c5a7e7ace0225f7c8a1b85f7890f11c046bf779
Emanuel Cino <ecino@compassion.ch>
2025-11-21 20:05:30 +0100

    [MIG] FIX query removing mis_builder settings views

--- mis_builder_spn_info/migrations/18.0.1.0.0/pre-migration.py
+++ mis_builder_spn_info/migrations/18.0.1.0.0/pre-migration.py
@@ -1,5 +1,15 @@
 def migrate(cr, version):
     cr.execute("""
-    DELETE FROM ir_ui_view WHERE model = 'res.config.settings'
-    AND arch_db LIKE '%mis_%';
+  WITH RECURSIVE to_delete AS (
+      SELECT id
+      FROM ir_ui_view
+      WHERE model = 'res.config.settings'
+        AND arch_db->>'en_US' LIKE '%mis_%'
+      UNION ALL
+      SELECT v.id
+      FROM ir_ui_view v
+      JOIN to_delete td ON v.inherit_id = td.id
+    )
+    DELETE FROM ir_ui_view
+    WHERE id IN (SELECT id FROM to_delete);
     """)
