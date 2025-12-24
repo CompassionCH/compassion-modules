61c0efbaa8b687606e7cb0a55b65472aa820c016
SamBachmann <samuel.bachmann02@gmail.com>
2025-12-24 11:28:33 +0100

    [MIG] T2718 partner_communication_omr : import from 14.0

---
+++ partner_communication_omr/__init__.py
@@ -0,0 +1,11 @@
+##############################################################################
+#
+#    Copyright (C) 2022 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Emanuel Cino <ecino@compassion.ch>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+from . import models
---
+++ partner_communication_omr/models/__init__.py
@@ -0,0 +1,16 @@
+##############################################################################
+#
+#    Copyright (C) 2022 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Emanuel Cino <ecino@compassion.ch>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+from . import (
+    communication_attachment,
+    communication_config,
+    communication_job,
+    report_with_omr,
+)
