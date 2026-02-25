c43f22ac689c8fddd0d4fbfcac1e60a46fd4cb92
Emanuel Cino <ecino@compassion.ch>
2026-02-25 15:06:28 +0100

    IMP partner_communication_compassion migration performance

--- partner_communication_compassion/migrations/17.0.1.0.0/pre-migration.py
+++ partner_communication_compassion/migrations/17.0.1.0.0/pre-migration.py
@@ -1,3 +1,20 @@
+from openupgradelib import openupgrade
+
+
 def migrate(cr, version):
     # Remove parameters from report paperformat (no longer used)
-    cr.execute("DELETE FROM report_paperformat_parameter;")
+    openupgrade.logged_query(cr, "DELETE FROM report_paperformat_parameter;")
+    if not openupgrade.column_exists(cr, "correspondence", "has_valid_language"):
+        openupgrade.logged_query(
+            cr,
+            """
+            ALTER TABLE correspondence
+            ADD COLUMN has_valid_language boolean DEFAULT true;
+            """,
+        )
+        openupgrade.logged_query(
+            cr,
+            """ALTER TABLE correspondence
+        ALTER COLUMN has_valid_language
+        SET DEFAULT false;""",
+        )
