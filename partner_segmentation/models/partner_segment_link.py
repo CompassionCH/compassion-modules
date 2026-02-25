81009d16c65b76a7820f6f5d9efebefc07200a9f
Daniel Gergely <dgergely@compassion.ch>
2026-02-25 16:07:41 +0100

    [T3022] REFACTOR: new partner_segment_link model to replace hard coded personalized_links
    - REFACTOR: segmentation data now uses the new model-links
    - CHORE: added partner_segment_link to imports
    - REFACTOR: removed personalized links and added link_ids
    - FEAT: created ResPartnerSegmentLink model
    - REFACTOR: added access rules to new model
    - REFACTOR: updated partner_segmentation_view to display new links

---
+++ partner_segmentation/models/partner_segment_link.py
@@ -0,0 +1,43 @@
+##############################################################################
+#
+#    Copyright (C) 2026 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Daniel Gergely <dgergely@compassion.ch>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+from odoo import fields, models
+
+class ResPartnerSegmentLink(models.Model):
+    """
+    Model to store the
+    - URL
+    - Label
+    - Language
+    - Category
+    of a partner segmentation link, which then can be used to
+    provide useful links to the partners based on their segmentation
+    category and language.
+    """
+    _name = "res.partner.segment.link"
+    _description = "Partner Segmentation Link"
+
+    url = fields.Char(
+        required=True,
+        help="URL of the link to be provided to the partner"
+    )
+    label = fields.Char(
+        required=True,
+        help="Label of the link to be provided to the partner"
+    )
+    language_id = fields.Many2one(
+        "res.lang",
+        string="Language",
+        help="Language of the link, used to provide the link to the partner based on their language"
+    )
+    category_id = fields.Many2one(
+        "res.partner.segment",
+        string="Category",
+        help="Segmentation category of the link, used to provide the link to the partner based on their segmentation category"
+    )
