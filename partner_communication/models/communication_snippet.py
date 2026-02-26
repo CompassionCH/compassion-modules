983a11779632b0fa7e54e9237a1811450ef1dcbb
avuff <aurelienclerc@icloud.com>
2026-02-26 15:27:01 +0100

    T2840 add category and description code snippet (#2070)
    * [FEAT][T2840] add category and description code snippet
    
    * [FEAT][T2840] add category and description code snippet
    
    * T2840 ADD search view
    
    ---------
    
    Co-authored-by: Aurelien Vuffray <zivi1@compassion.ch>
    Co-authored-by: Emanuel Cino <ecino@compassion.ch>

--- partner_communication/models/communication_snippet.py
+++ partner_communication/models/communication_snippet.py
@@ -1,12 +1,34 @@
 from odoo import fields, models
 
 
+class CommunicationSnippetCategory(models.Model):
+    _name = "communication.snippet.category"
+    _description = "Communication Snippet Category"
+
+    name = fields.Char(string="Category Name", required=True)
+
+    _sql_constraints = [
+        (
+            "name_unique",
+            "unique(name)",
+            "The name of the category must be unique.",
+        )
+    ]
+
+
 class CommunicationSnippet(models.Model):
     _name = "communication.snippet"
     _description = "Communication Snippet"
 
     name = fields.Char(required=True, index=True)
     snippet_text = fields.Html(required=True, translate=True, sanitize=False)
+    description = fields.Text(string="Description")
+
+    category_id = fields.Many2one(
+        "communication.snippet.category",
+        string="Category",
+        help="Category of the communication snippet",
+    )
 
     def action_edit_snippet(self):
         self.ensure_one()
