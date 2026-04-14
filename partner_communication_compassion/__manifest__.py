63bd6b3178d08fc28c30e087d6a76b8c7184fb66
Emanuel Cino <ecino@compassion.ch>
2026-04-14 14:22:48 +0200

    T3096 Refactor letters display
    - ADD new field for tracking date of Exit communication sent to sponsor
    - FIX is_final_letter field

--- partner_communication_compassion/__manifest__.py
+++ partner_communication_compassion/__manifest__.py
@@ -29,7 +29,7 @@
 # pylint: disable=C8101
 {
     "name": "Compassion Partner Communications",
-    "version": "14.0.1.1.1",
+    "version": "14.0.1.2.0",
     "category": "Other",
     "author": "Compassion CH",
     "license": "AGPL-3",
