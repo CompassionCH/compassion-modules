8eb8dba68df5616d7b3abd52343c8f688c7b9f33
Emanuel Cino <ecino@compassion.ch>
2026-06-03 15:53:28 +0200

    FIX intervention migration

--- intervention_compassion/migrations/14.0.1.1.0/pre-migration.py
+++ intervention_compassion/migrations/14.0.1.1.0/pre-migration.py
@@ -18,6 +18,23 @@ def migrate(cr, version):
             "gmc.action.connect",
             res_id[0],
         )
+    cr.execute(
+        """
+        SELECT id
+        FROM gmc_action_connect
+        WHERE connect_schema =
+              'http://schemas.ci.org/ci/messaging/availability/2025/02/ReservationExpiredNotification'
+        """
+    )
+    res_id = cr.fetchone()
+    if res_id:
+        openupgrade.add_xmlid(
+            cr,
+            "intervention_compassion",
+            "expiration_reservation_connect",
+            "gmc.action.connect",
+            res_id[0],
+        )
     # Remove duplicate mappings
     openupgrade.logged_query(
         cr,
